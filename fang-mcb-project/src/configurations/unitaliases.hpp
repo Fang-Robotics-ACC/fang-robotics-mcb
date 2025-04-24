580b4e4112407c48ede24f742483ec3b6394a566
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-24 12:42:36 -0500

    Make unit alises reflect v3.0 formatting with float tempalte

--- fang-mcb-project/src/configurations/unitaliases.hpp
+++ fang-mcb-project/src/configurations/unitaliases.hpp
@@ -1,8 +1,8 @@
 #ifndef UNIT_ALIASES_HPP_SDLDDFLKJSKFJELKFJAS
 #define UNIT_ALIASES_HPP_SDLDDFLKJSKFJELKFJAS
 #include "units.h"
-using NewtonMeters = units::torque::newton_meter_t;
-using Watts = units::power::watt_t;
-using Meters = units::length::meter_t;
-using MetersPerSecond = units::velocity::meters_per_second_t;
+using NewtonMeters = units::torque::newton_meters<float>;
+using Watts = units::power::watts<float>;
+using Meters = units::length::meters<float>;
+using MetersPerSecond = units::velocity::meters_per_second<float>;
 #endif
