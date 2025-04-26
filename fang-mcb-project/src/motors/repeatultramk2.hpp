6c689688882d57c6d92985b240657c34da9d882d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-26 16:50:49 -0500

    Make RepeatUltraMk2 only a ISpeedMotor
    It is a brushles motor where you cannot have accurate torque info without an external sensor

--- fang-mcb-project/src/motors/repeatultramk2.hpp
+++ fang-mcb-project/src/motors/repeatultramk2.hpp
@@ -8,7 +8,7 @@
 
 namespace motors
 {
-    class RepeatUltraMk2 : virtual public rail::IDCMotor<MetersPerSecond, NewtonMeters, Watts>, virtual public rail::IWattBudgeter<Watts>
+    class RepeatUltraMk2 : virtual public rail::ISpeedMotor<MetersPerSecond, Watts>, virtual public rail::IWattBudgeter<Watts>
     {
     public:
         void setWattBudget(const Watts& wattage) override;
@@ -22,13 +22,6 @@ namespace motors
 		void setMinSpeed(const MetersPerSecond& minSpeed) override;
 		const MetersPerSecond& getMinSpeed() const override;
 
-		void setTorque(const NewtonMeters& torque) override;
-		const NewtonMeters& getTorque() const override;
-
-		void setMaxTorque(const NewtonMeters& maxTorque) override;
-		const NewtonMeters& getMaxTorque() const override;
-		void setMinTorque(const NewtonMeters& minTorque) override;
-		const NewtonMeters& getMinTorque() const override;
     private:
         Watts m_wattBudget{0};
         Watts m_maxWattBudget{0};
@@ -37,10 +30,6 @@ namespace motors
         MetersPerSecond m_speed{0};
         MetersPerSecond m_maxSpeed{0};
         MetersPerSecond m_minSpeed{0};
-
-        NewtonMeters m_torque{0};
-        NewtonMeters m_maxTorque{0};
-        NewtonMeters m_minTorque{0};
    };
 }
 #endif 
