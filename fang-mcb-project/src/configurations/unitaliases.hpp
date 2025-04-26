f19eb771a104d5db998f6a9e7f7391e2a263d194
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-26 17:17:08 -0500

    Add RPM type alias

--- fang-mcb-project/src/configurations/unitaliases.hpp
+++ fang-mcb-project/src/configurations/unitaliases.hpp
@@ -6,4 +6,5 @@ using NewtonMeters = units::torque::newton_meter_t;
 using Watts = units::power::watt_t;
 using Meters = units::length::meter_t;
 using MetersPerSecond = units::velocity::meters_per_second_t;
+using RPM = units::angular_velocity::revolutions_per_minute_t;
 #endif
