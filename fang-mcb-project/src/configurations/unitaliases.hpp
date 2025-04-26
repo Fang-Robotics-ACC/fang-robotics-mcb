ba5ac2387c5a326834adb3a519a2d2fac479d3ee
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-26 17:45:00 -0500

    Add RPM/Volt unit alias
    Used in brushless kv constants

--- fang-mcb-project/src/configurations/unitaliases.hpp
+++ fang-mcb-project/src/configurations/unitaliases.hpp
@@ -8,4 +8,5 @@ using Meters = units::length::meter_t;
 using MetersPerSecond = units::velocity::meters_per_second_t;
 using RPM = units::angular_velocity::revolutions_per_minute_t;
 using Volts = units::voltage::volt_t;
+using RPMPerVolt = units::compound_unit<RPM, units::inverse<Volts>>; //Used in Motor Constants
 #endif
