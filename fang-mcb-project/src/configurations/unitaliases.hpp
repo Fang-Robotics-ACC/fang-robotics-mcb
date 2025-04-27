e0be9111ffe6604255a5bbef2444882e7cbde6e8
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-27 08:00:02 -0500

    Fix proper compound unit use

--- fang-mcb-project/src/configurations/unitaliases.hpp
+++ fang-mcb-project/src/configurations/unitaliases.hpp
@@ -1,12 +1,15 @@
 #ifndef UNIT_ALIASES_HPP_SDLDDFLKJSKFJELKFJAS
 #define UNIT_ALIASES_HPP_SDLDDFLKJSKFJELKFJAS
-#define UNIT_LIB_DEFAULT_TYPE float
 #include "units.h"
+
 using NewtonMeters = units::torque::newton_meter_t;
 using Watts = units::power::watt_t;
 using Meters = units::length::meter_t;
 using MetersPerSecond = units::velocity::meters_per_second_t;
 using RPM = units::angular_velocity::revolutions_per_minute_t;
 using Volts = units::voltage::volt_t;
-using RPMPerVolt = units::compound_unit<RPM, units::inverse<Volts>>; //Used in Motor Constants
+using RawRPM = units::angular_velocity::revolutions_per_minute;
+using RawVolts = units::voltage::volts;
+using RawRPMPerVolt = units::compound_unit<RawRPM, units::inverse<RawVolts>>; //Used in Motor Constants
+using RPMPerVolt = units::unit_t<RawRPMPerVolt>;
 #endif
