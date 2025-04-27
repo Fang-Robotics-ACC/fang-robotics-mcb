60844fdba5d0b44d9d7ada64d2e055f44ae1ee58
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-27 08:33:04 -0500

    Remove the returning by reference
    Seg fault ahhh

--- fang-mcb-project/src/motors/brushlessutils.cpp
+++ fang-mcb-project/src/motors/brushlessutils.cpp
@@ -6,12 +6,12 @@ namespace motors
     {
         namespace brushless 
         {
-            const RPM& controllerVoltageToSpeed(const Volts& volts, const RPMPerVolt& kv)
+            RPM controllerVoltageToSpeed(const Volts& volts, const RPMPerVolt& kv)
             {
                 return volts * kv; //The kv constant multiplied by the volts = the rpm
             }
 
-            const Volts& speedToControllerVoltage(const RPM& speed, const RPMPerVolt& kv)
+            Volts speedToControllerVoltage(const RPM& speed, const RPMPerVolt& kv)
             {
                 return speed / kv; //The kv constant multiplied by the volts = the rpm
             }
--- fang-mcb-project/src/motors/brushlessutils.hpp
+++ fang-mcb-project/src/motors/brushlessutils.hpp
@@ -11,12 +11,12 @@ namespace motors
             /*!
             This provides the speed that the motor will output to achieve the
             given output voltage from the controller.*/
-            const RPM& controllerVoltageToSpeed(const Volts& volts, const RPMPerVolt& kv);
+            RPM controllerVoltageToSpeed(const Volts& volts, const RPMPerVolt& kv);
 
             /*!
             This provides the voltage that the controller will output to achieve the
             given speed on the motor.*/
-            const Volts& speedToControllerVoltage(const RPM& speed, const RPMPerVolt& kv);
+            Volts speedToControllerVoltage(const RPM& speed, const RPMPerVolt& kv);
         }
     }
 }
