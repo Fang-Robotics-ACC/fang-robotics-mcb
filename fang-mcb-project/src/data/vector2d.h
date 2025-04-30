4422a59159b379c63920157848118d73e4c0cf5a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 10:21:42 -0500

    Add vector2D data type
    This will be redundant to Translation2D, so some refactoring will be done later

---
+++ fang-mcb-project/src/data/vector2d.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR_2D_H_DSLFKJESFLK_YEET
+#define VECTOR_2D_H_DSLFKJESFLK_YEET
+namespace data
+{
+    namespace math
+    {
+        template <typename Unit>
+        struct Vector2D
+        {
+        public:
+            Unit x;
+            Unit y; 
+        };
+    }
+}
+#endif
