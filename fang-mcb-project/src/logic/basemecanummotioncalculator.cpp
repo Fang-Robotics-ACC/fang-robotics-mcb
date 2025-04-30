c98ab188dfe9dca48a276f8a627a63a8036d3597
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 12:10:11 -0500

    Fix translation calculation glitch

--- fang-mcb-project/src/logic/basemecanummotioncalculator.cpp
+++ fang-mcb-project/src/logic/basemecanummotioncalculator.cpp
@@ -6,7 +6,6 @@ namespace logic
 {
     namespace chassis
     {
-
         void BaseMecanumMotionCalculator::setQuadDriveData(const QuadDriveData& quadDriveData)
         {
             m_quadDriveData = quadDriveData;
@@ -22,7 +21,7 @@ namespace logic
             //Refer to equation 23 in https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
             const double xSpeedSum{-m_quadDriveData.frontLeft + m_quadDriveData.frontRight
                             + m_quadDriveData.rearLeft  - m_quadDriveData.rearRight};
-            const double x{ySpeedSum*(0.25)}; //The formula multiplies the sum by 1/4 of the radius
+            const double x{xSpeedSum*(0.25)}; //The formula multiplies the sum by 1/4 of the radius
 
             return Translation2D{x, y};
         }
