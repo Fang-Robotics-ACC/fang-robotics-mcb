ab0a394a05e49c6e3992f9f976b9abea6dfb1116
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 12:19:25 -0500

    Add transational test for mecanum calculator

--- fang-mcb-project/test/mecanumcalculatortest.cpp
+++ fang-mcb-project/test/mecanumcalculatortest.cpp
@@ -4,17 +4,27 @@
 #include "chassislogicaliases.h"
 #include <iostream>
 
-TEST(mecanumCalculator, mecanumLogic)
+using Calculator = logic::chassis::BaseMecanumMotionCalculator;
+Calculator calc{};
+using QuadDriveData = data::chassis::QuadDriveData<double>;
+using Translation2D = logic::chassis::Translation2D;
+
+TEST(forwardTest, mecanumCalculator )
 {
-    using Calculator = logic::chassis::BaseMecanumMotionCalculator;
-    Calculator calc{};
-    using QuadDriveData = data::chassis::QuadDriveData<double>;
-    using Translation2D = logic::chassis::Translation2D;
 
     const QuadDriveData forwardTest{1.0,1.0,1.0,1.0};
     const Translation2D forwardTestExpectedMotion{0.0, 1.0};
-    calc.setQuadDriveData(forwardTest);
     std::cout << calc.getTranslation().x;
     EXPECT_DOUBLE_EQ(calc.getTranslation().x, forwardTestExpectedMotion.x);
     EXPECT_DOUBLE_EQ(calc.getTranslation().y, forwardTestExpectedMotion.y);
+}
+
+TEST(leftTest, mecanumCalculator )
+{
+
+    const QuadDriveData test{1.0,-1.0,-1.0,1.0};
+    const Translation2D expectedMotion{1.0, 0.0};
+    calc.setQuadDriveData(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x, expectedMotion.x);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y, expectedMotion.y);
 }
