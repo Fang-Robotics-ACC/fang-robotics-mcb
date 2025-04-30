7f16e4f75cf78f9e66ae3a121b4e282711b18ab9
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 10:11:10 -0500

    Add motion2d struct
    This will pass around translational information

---
+++ fang-mcb-project/src/data/motion2d.h
@@ -0,0 +1,16 @@
+#ifndef MOTION2D_H_SDLFSLEKLFJSEKF_YEET_YEET
+#define MOTION2D_H_SDLFSLEKLFJSEKF_YEET_YEET
+namespace data
+{
+    namespace physics
+    {
+        template <typename Velocity>
+        struct Motion2d
+        {
+        public:
+            Velocity x;
+            Velocity y; 
+        };
+    }
+}
+#endif
