7394d8680f824b42ba7fd0507e45bb0faadf4356
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 12:30:04 -0500

    Fix translational quad drive calculator

--- fang-mcb-project/src/logic/basemecanummotioncalculator.cpp
+++ fang-mcb-project/src/logic/basemecanummotioncalculator.cpp
@@ -19,8 +19,9 @@ namespace logic
             const double y{ySpeedSum*(0.25)}; //The formula multiplies the sum by 1/4 of the radius
 
             //Refer to equation 23 in https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
-            const double xSpeedSum{-m_quadDriveData.frontLeft + m_quadDriveData.frontRight
-                            + m_quadDriveData.rearLeft  - m_quadDriveData.rearRight};
+            //Adapted so right direciton is positive
+            const double xSpeedSum{m_quadDriveData.frontLeft - m_quadDriveData.frontRight
+                            - m_quadDriveData.rearLeft  + m_quadDriveData.rearRight};
             const double x{xSpeedSum*(0.25)}; //The formula multiplies the sum by 1/4 of the radius
 
             return Translation2D{x, y};
