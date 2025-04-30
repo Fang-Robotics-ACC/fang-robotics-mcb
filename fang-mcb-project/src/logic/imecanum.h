5a3f99c26d76fed8e712d83c46d8d5304393db68
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 12:48:25 -0500

    Initialize likely unused interface
    I'll keep it around just in case there is a reason.

---
+++ fang-mcb-project/src/logic/imecanum.h
@@ -0,0 +1,16 @@
+#ifndef IMECANUM_H_SDFLKSKLJFLAWFE_YEET_YEET
+#define IMECANUM_H_SDFLKSKLJFLAWFE_YEET_YEET
+namespace logic
+{
+
+    namespace chassis
+    {
+        template <typename MotorUnit>
+        class IMecanum
+        {
+        public:
+            virtual MotorUnit 
+        };
+    }
+}
+#endif
