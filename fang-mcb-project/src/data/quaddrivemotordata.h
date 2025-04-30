352806ece74e95cc0f4bfa154be3225013ad818e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-04-30 10:01:43 -0500

    Initialize QuadDriveMotorData
    This provides a convenient method of passing information about the entire drivetrain (for 4 motor drives)

---
+++ fang-mcb-project/src/data/quaddrivemotordata.h
@@ -0,0 +1,30 @@
+#ifndef QUAD_DRIVE_INFO_H_SLDFJLSLDDJDJF_YEET_YEET
+#define QUAD_DRIVE_INFO_H_SLDFJLSLDDJDJF_YEET_YEET
+namespace data
+{
+    namespace chassis
+    {
+        /**
+         * Generally, this class is meant to represent 4 wheels which are facing the same direction.
+         * Although you can use different conventions, the recommended convention is that positive means
+         * that the given wheel will rotate in a manner that would move the chassis forwawrd
+         * (If it's a mecanum wheel, assume that it's a regular wheel - the convention is just
+         * to establish the rotational direction.)
+         */
+        template <typename MotorUnit>
+        struct QuadDriveData
+        {
+        public:
+            QuadDriveData(const MotorUnit& frontLeftData, const MotorUnit& frontRightData,
+                          const MotorUnit& rearLeftData,  const MotorUnit& rearRightData):
+                          frontLeft{frontLeftData}, frontRight{frontRightData},
+                          rearLeft{rearLeftData},   rearRight{rearRightData}
+                          {}
+            MotorUnit frontLeft;
+            MotorUnit frontRight;
+            MotorUnit rearLeft;
+            MotorUnit rearRight;
+        };
+    }
+}
+#endif
