e7844500bfbe6ad6689b2f044b725026dd0ce6c7
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-01 23:42:23 -0500

    Implement adaptedDutyCycle calculation

---
+++ fang-mcb-project/src/logic/vortex80aesclogic.cpp
@@ -0,0 +1,24 @@
+#include "vortex80aesclogic.hpp"
+#include <cmath>
+#include "modm/math/interpolation/linear.hpp"
+#include "modm/container/pair.hpp"
+
+namespace logic 
+{
+    namespace motors
+    {
+        double Vortex80AEscLogic::adaptedDutyCycle(double rangePercentage, Hertz pinFrequency)
+        {
+            // rawDutyCycle will range between 0 and 1
+            // 0.50 will correspond to a 1ms pulse (minimum) assuming the pin is set to a period of 2ms
+            // 1 will correspond to a 2ms pulse.
+            // By dividing the total by 2. It results in the maximum being 0.5
+            // Adding the result to 0.5f will shift the minimum to 0.5 and the maximum to 1
+            const Milliseconds pinPeriod {1.0 / pinFrequency};
+            const double maxDutyCycle{mk_maxRangePeriod / pinPeriod};
+            const double minDutyCycle{mk_minRangePeriod / pinPeriod};
+
+            return std::lerp(minDutyCycle, maxDutyCycle, rangePercentage);
+        }
+    }
+}
