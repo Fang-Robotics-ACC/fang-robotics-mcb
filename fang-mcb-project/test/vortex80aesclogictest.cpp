681455b190d1da69c827972fcd8f1d55ef4b83f0
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-01 23:42:47 -0500

    Add vortex logic unit tests for adapted duty cycle

---
+++ fang-mcb-project/test/vortex80aesclogictest.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+#include "vortex80aesclogic.hpp"
+#include "unitaliases.hpp"
+
+TEST(zeroTest, Vortex80AEscLogic)
+{
+    //2 ms period
+    const Hertz standardFrequency{500};
+    logic::motors::Vortex80AEscLogic vortex {};
+
+    EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(0.0, standardFrequency), 0.5);
+}
+
+TEST(midRangeTest, Vortex80AEscLogic)
+{
+    //2 ms period
+    const Hertz standardFrequency{500};
+    logic::motors::Vortex80AEscLogic vortex {};
+
+    EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(0.5, standardFrequency), 0.75);
+}
+
+TEST(fullRangeTest, Vortex80AEscLogic)
+{
+    //2 ms period
+    const Hertz standardFrequency{500};
+    logic::motors::Vortex80AEscLogic vortex {};
+
+    EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(1, standardFrequency), 1.0);
+}
