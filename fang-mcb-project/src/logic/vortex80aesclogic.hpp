949afcae361b2df597c2afdf64e6d68f20328260
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-02 09:50:58 -0500

    Add setter and getter functions for storing pin frequency in vortex logic

--- fang-mcb-project/src/logic/vortex80aesclogic.hpp
+++ fang-mcb-project/src/logic/vortex80aesclogic.hpp
@@ -32,12 +32,16 @@ namespace logic
              * have a pulse at least 2ms long.
              */
             double adaptedDutyCycle(double rangePercentage, Hertz pinFrequency);
+            double adaptedDutyCycle(double rangePercentage);
+            void setPinFrequency(const Hertz& pinFrequency);
+            Hertz getpinFrequency() const;
         private:
             // Note that in bidirectional mode, the minimum period would
             // correspond to the motor going in full speed reverse
             // But in unidirectional mode, it would mean a still motor
             const Milliseconds mk_maxRangePeriod {2.0};
             const Milliseconds mk_minRangePeriod {1.0};
+            Hertz m_pinFrequency;
         };
     }
 }
