c7dc50585967c32196c1f2c0c6bb7c9d4df13cf4
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-02 00:01:11 -0500

    Add directionality enum class

---
+++ fang-mcb-project/src/data/directionality.hpp
@@ -0,0 +1,14 @@
+#ifndef DIRECTIONALITY_HPP_SDFEFEF_YEET
+#define DIRECTIONALITY_HPP_SDFEFEF_YEET
+namespace data
+{
+    namespace motors
+    {
+        enum class Directionality
+        {
+            BIDIRECTIONAL,
+            UNIDIRECTIONAL
+        };
+    }
+}
+#endif
