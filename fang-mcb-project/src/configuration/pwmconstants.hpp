bc5452b56e5dd7c4af04a248f26e1053cba97f9e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-02 10:30:00 -0500

    Add pwm constants
    Avoid magic numbers yay

---
+++ fang-mcb-project/src/configuration/pwmconstants.hpp
@@ -0,0 +1,11 @@
+#ifndef FANG_ROBOTIC_MCB_PWM_CONSTANTS
+#define FANG_ROBOTIC_MCB_PWM_CONSTANTS
+namespace config
+{
+    namespace pwm
+    {
+        const double k_pwmDutyMax{0.0};
+        const double k_pwmDutyMin{1.0};
+    }
+}
+#endif
