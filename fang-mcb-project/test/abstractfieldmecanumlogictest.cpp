86491f6d5789a7a0124e6a742431c40e29d5dfe3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-06 10:56:23 -0500

    Add more direcions in unit test

--- fang-mcb-project/test/abstractfieldmecanumlogictest.cpp
+++ fang-mcb-project/test/abstractfieldmecanumlogictest.cpp
@@ -20,8 +20,23 @@ TEST(translationSetterGetterTest, AbstractFieldMecanumLogic)
 
     fieldMecanumLogic.setTranslation(forward);
 
-    const logic::chassis::Translation2D result{fieldMecanumLogic.getTranslation()};
+    logic::chassis::Translation2D result{fieldMecanumLogic.getTranslation()};
 
     EXPECT_DOUBLE_EQ(result.x, forward.x);
     EXPECT_DOUBLE_EQ(result.y, forward.y);
+
+    //Test integrity of translation when changing the angle
+    const Degrees facingRight{-90.0};
+    fieldMecanumLogic.setRobotAngle(facingRight);
+
+    result = fieldMecanumLogic.getTranslation();
+    EXPECT_DOUBLE_EQ(result.x, forward.x);
+    EXPECT_DOUBLE_EQ(result.y, forward.y);
+
+    const logic::chassis::Translation2D right{1.0, 0.0};
+    fieldMecanumLogic.setTranslation(right);
+
+    result = fieldMecanumLogic.getTranslation();
+    EXPECT_DOUBLE_EQ(result.x, right.x);
+    EXPECT_DOUBLE_EQ(result.y, right.y);
 }
