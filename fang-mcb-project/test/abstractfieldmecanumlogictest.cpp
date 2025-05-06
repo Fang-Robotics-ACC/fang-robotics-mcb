c49010c18028fd7c17d31c4ac29b67feb471d41d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-06 14:36:38 -0500

    Add basic unit tests for field mecanum logic

--- fang-mcb-project/test/abstractfieldmecanumlogictest.cpp
+++ fang-mcb-project/test/abstractfieldmecanumlogictest.cpp
@@ -1,5 +1,6 @@
 #include "abstractfieldmecanumlogic.hpp"
 #include "chassislogicaliases.hpp"
+#include "abstractmecanumcalculator.hpp"
 #include "unitaliases.hpp"
 
 #include "gtest/gtest.h"
@@ -97,3 +98,62 @@ TEST(harderTranslationSetterGetterTest, AbstractFieldMecanumLogic)
     EXPECT_DOUBLE_EQ(result.x, movement.x);
     EXPECT_DOUBLE_EQ(result.y, movement.y);
 }
+
+TEST(AbstractFieldMecanumLogic, ZeroIsZero)
+{
+    //0 case
+    logic::chassis::AbstractMecanumCalculator calc{};
+    logic::chassis::AbstractFieldMecanumLogic fieldLogic{};
+    logic::chassis::Translation2D expectedTranslation{0.0, -2.23};
+    double expectedRotationOffset{20.0};
+
+    fieldLogic.setMotion(expectedTranslation, expectedRotationOffset);
+    fieldLogic.setRobotAngle(Degrees{0});
+
+    calc.setWheelSpeeds(fieldLogic.getWheelSpeeds());
+
+
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x, expectedTranslation.x);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y, expectedTranslation.y);
+    EXPECT_DOUBLE_EQ(calc.getRotationOffset(), expectedRotationOffset);
+}
+
+TEST(AbstractFieldMecanumLogic, ForwardCase)
+{
+    logic::chassis::AbstractMecanumCalculator calc{};
+    logic::chassis::AbstractFieldMecanumLogic fieldLogic{};
+    logic::chassis::Translation2D expectedTranslation{0, 12.3};
+    double expectedRotationOffset{0.0};
+
+    fieldLogic.setMotion(expectedTranslation, expectedRotationOffset);
+    fieldLogic.setRobotAngle(Degrees{0});
+
+    calc.setWheelSpeeds(fieldLogic.getWheelSpeeds());
+
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x, expectedTranslation.x);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y, expectedTranslation.y);
+    EXPECT_DOUBLE_EQ(calc.getRotationOffset(), expectedRotationOffset);
+}
+
+TEST(AbstractFieldMecanumLogic, offsetAnglePositive)
+{
+    logic::chassis::AbstractMecanumCalculator calc{};
+    logic::chassis::AbstractFieldMecanumLogic fieldLogic{};
+    logic::chassis::Translation2D fieldTranslation{0, 1.0};
+    double expectedRotationOffset{10.0};
+    const Degrees robotAngle{90};
+
+    fieldLogic.setMotion(fieldTranslation, expectedRotationOffset);
+    fieldLogic.setRobotAngle(robotAngle);
+
+    logic::chassis::Translation2D expectedTranslation{1.0, 0};
+
+    calc.setWheelSpeeds(fieldLogic.getWheelSpeeds());
+
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x, expectedTranslation.x);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y, expectedTranslation.y);
+    EXPECT_DOUBLE_EQ(calc.getRotationOffset(), expectedRotationOffset);
+}
+
+
+
