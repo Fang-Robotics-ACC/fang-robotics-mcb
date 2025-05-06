888e38e9fbd475bf848bc114d26497d9b79b51cd
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-06 15:13:01 -0500

    Add couterclockwise vector rotation tests

---
+++ fang-mcb-project/test/rotatevector2dtest.cpp
@@ -0,0 +1,35 @@
+#include "gtest/gtest.h"
+#include "rotatevector2d.hpp"
+
+#include "unitaliases.hpp"
+#include "mathaliases.hpp"
+
+#include <cmath>
+
+const double k_tolerance{1e-14};
+
+TEST(RotateVector2D, counterClockwise)
+{
+    {
+    mathAliases::AbstractVector2D testVector{0.0, 1.0};
+    //Rotate the unit vector
+    mathAliases::AbstractVector2D expectedVector{-std::cos(Radians{Degrees{45}}.to<double>()), std::cos(Radians{Degrees{45}}.to<double>())};
+    Degrees rotation{45};
+
+    mathAliases::AbstractVector2D outputVector{util::math::rotateVector2D(testVector, rotation)};
+
+    EXPECT_NEAR(outputVector.x, expectedVector.x, k_tolerance);
+    EXPECT_NEAR(outputVector.y, expectedVector.y, k_tolerance);
+    }
+
+    {
+    mathAliases::AbstractVector2D testVector{0.0, 100.0};
+    mathAliases::AbstractVector2D expectedVector{-100.0, 0.0};
+    Degrees rotation{90};
+
+    mathAliases::AbstractVector2D outputVector{util::math::rotateVector2D(testVector, rotation)};
+
+    EXPECT_NEAR(outputVector.x, expectedVector.x, k_tolerance);
+    EXPECT_NEAR(outputVector.y, expectedVector.y, k_tolerance);
+    }
+}
