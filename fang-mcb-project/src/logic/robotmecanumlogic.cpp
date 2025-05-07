71ec9596b89f0378e26a30b70bc72926081605d4
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-07 11:50:27 -0500

    Implement robotMecanumLogic wheel functions

---
+++ fang-mcb-project/src/logic/robotmecanumlogic.cpp
@@ -0,0 +1,60 @@
+#include "robotmecanumlogic.hpp"
+
+
+namespace logic
+{
+    namespace chassis
+    {
+        
+        RPM RobotMecanumLogic::getFrontLeftWheelSpeed() const
+        {
+            //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+            //Equation 20, but v_x is v_y and v_y is v_x
+            //If all of v_x is positive, then the wheels are pushing in the same
+            //direction. It is desired for this to be positive
+            //w_1
+            const MetersPerSecond translationFactor{m_translation.y - m_translation.x};
+            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return (1.0 / mk_wheelRadius) * (translationFactor - rotationFactor);
+        }
+
+        RPM RobotMecanumLogic::getFrontRightWheelSpeed() const
+        {
+            //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+            //Equation 20, but v_x is v_y and v_y is v_x
+            //If all of v_x is positive, then the wheels are pushing in the same
+            //direction. It is desired for this to be positive
+            //w_2
+            const MetersPerSecond translationFactor{m_translation.y + m_translation.x};
+            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return (1.0 / mk_wheelRadius) * (translationFactor + rotationFactor);
+        }
+
+        RPM RobotMecanumLogic::getRearLeftWheelSpeed() const
+        {
+            //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+            //Equation 20, but v_x is v_y and v_y is v_x
+            //If all of v_x is positive, then the wheels are pushing in the same
+            //direction. It is desired for this to be positive
+            //w_3
+            const MetersPerSecond translationFactor{m_translation.y + m_translation.x};
+            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return (1.0 / mk_wheelRadius) * (translationFactor - rotationFactor);
+        }
+
+        RPM RobotMecanumLogic::getRearRightWheelSpeed() const
+        {
+
+            //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+            //Equation 20, but v_x is v_y and v_y is v_x
+            //Equation 20, but v_x is v_y and v_y is v_x
+            //If all of v_x is positive, then the wheels are pushing in the same
+            //direction. It is desired for this to be positive
+            //w_4
+            const MetersPerSecond translationFactor{m_translation.y - m_translation.x};
+            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return (1.0 / mk_wheelRadius) * (translationFactor + rotationFactor);
+        }
+
+    }
+}
