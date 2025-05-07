f044e9b5ca15856af2a674b0782948f4d904ce18
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-07 11:55:56 -0500

    Initialize mecanum calculator

---
+++ fang-mcb-project/src/logic/mecanumcalculator.hpp
@@ -0,0 +1,39 @@
+#ifndef FANG_ROBOTICS_MCB_MECANUM_CALCULATOR_HPP
+#define FANG_ROBOTICS_MCB_MECANUM_CALCULATOR_HPP
+#include "quaddrivedata.hpp"
+#include "translation2d.hpp"
+#include "unitaliases.hpp"
+#include "modm/math/geometry/vector.hpp"
+#include "chassislogicaliases.hpp"
+
+namespace logic
+{
+    namespace chassis
+    {
+        using QuadDriveData = data::chassis::QuadDriveData<RPM>;
+        /**
+         * This uses the equations from this paper
+         * https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+         */
+
+        class MecanumCalculator
+        {
+        public:
+            MecanumCalculator(){}
+            void setWheelSpeeds(const QuadDriveData& quadDriveData);
+
+            /**
+             * Returns the movement of the robot relative to the robot.
+             */
+            Velocity2D getTranslation() const;
+
+            /**
+             * Counterclockwise is positive
+             */
+            RPM getRotation() const;
+        private:
+            QuadDriveData m_quadDriveData{RPM{0}, RPM{0}, RPM{0}, RPM{0}};
+        };
+    }
+}
+#endif
