5f029bcde59014a12041d1e34ab95294c685b86d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-08 09:55:47 -0500

    Fix mecanum logic units
    In the formula, it can be interpreted that meters per second is how many radiuses per second (rad/s). By adding radianmeters per second, it adds dimensional consistency with an intuitive interpretation versus using antiradians and addradians

--- fang-mcb-project/src/logic/robotmecanumlogic.cpp
+++ fang-mcb-project/src/logic/robotmecanumlogic.cpp
@@ -1,5 +1,4 @@
 #include "robotmecanumlogic.hpp"
-
 namespace logic
 {
     namespace chassis
@@ -11,12 +10,12 @@ namespace logic
             //Equation 20, but v_x is v_y and v_y is v_x
             //If all of v_x is positive, then the wheels are pushing in the same
             //direction. It is desired for this to be positive
+            //How many radius's per second? i.e. radians persecond
             //w_1
-            const MetersPerSecond translationFactor{m_translation.y - m_translation.x};
-            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation * k_antiRadians};
-            return (1.0 / mk_wheelRadius) * (translationFactor - rotationFactor);
+            const RadianMetersPerSecond translationFactor{(m_translation.y - m_translation.x).to<double>()};
+            const RadianMetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return ((1.0 / mk_wheelRadius) * (translationFactor - rotationFactor));
         }
-
         RPM RobotMecanumLogic::getFrontRightWheelSpeed() const
         {
             //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
@@ -24,12 +23,11 @@ namespace logic
             //If all of v_x is positive, then the wheels are pushing in the same
             //direction. It is desired for this to be positive
             //w_2
-            const MetersPerSecond translationFactor{m_translation.y + m_translation.x};
-            const auto rotationFactor{mk_wheelDistanceConstant * m_rotation};
-            return (1.0 / mk_wheelRadius) * (translationFactor + rotationFactor);
+            const RadianMetersPerSecond translationFactor{(m_translation.y + m_translation.x).to<double>()};
+            const RadianMetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return ((1.0 / mk_wheelRadius) * (translationFactor + rotationFactor));
         }
 
-        /*
         RPM RobotMecanumLogic::getRearLeftWheelSpeed() const
         {
             //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
@@ -37,9 +35,10 @@ namespace logic
             //If all of v_x is positive, then the wheels are pushing in the same
             //direction. It is desired for this to be positive
             //w_3
-            const MetersPerSecond translationFactor{m_translation.y + m_translation.x};
-            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
-            return RPM{(1.0 / mk_wheelRadius) * (translationFactor - rotationFactor)};
+
+            const RadianMetersPerSecond translationFactor{(m_translation.y + m_translation.x).to<double>()};
+            const RadianMetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return ((1.0 / mk_wheelRadius) * (translationFactor - rotationFactor));
         }
 
         RPM RobotMecanumLogic::getRearRightWheelSpeed() const
@@ -51,11 +50,10 @@ namespace logic
             //If all of v_x is positive, then the wheels are pushing in the same
             //direction. It is desired for this to be positive
             //w_4
-            const MetersPerSecond translationFactor{m_translation.y - m_translation.x};
-            const MetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
-            return RPM{(1.0 / mk_wheelRadius) * (translationFactor + rotationFactor)};
-        }
-        */
 
+            const RadianMetersPerSecond translationFactor{(m_translation.y - m_translation.x).to<double>()};
+            const RadianMetersPerSecond rotationFactor{mk_wheelDistanceConstant * m_rotation};
+            return ((1.0 / mk_wheelRadius) * (translationFactor + rotationFactor));
+        }
     }
 }
