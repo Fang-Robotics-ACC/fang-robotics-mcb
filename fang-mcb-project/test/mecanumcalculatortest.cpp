5c5a7075b996d395df95b7b29206fdfaca6ba25f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-08 11:25:52 -0500

    Add forward motion mecanum test

--- fang-mcb-project/test/mecanumcalculatortest.cpp
+++ fang-mcb-project/test/mecanumcalculatortest.cpp
@@ -1,52 +1,19 @@
 #include "gtest/gtest.h"
-#include "abstractmecanumcalculator.hpp"
+#include "mecanumcalculator.hpp"
+#include "unitaliases.hpp"
 #include "quaddrivedata.hpp"
 #include "chassislogicaliases.hpp"
 #include <iostream>
 
-using Calculator = logic::chassis::AbstractMecanumCalculator;
-using QuadDriveData = data::chassis::QuadDriveData<double>;
-using Translation2D = logic::chassis::Translation2D;
-
-TEST(forwardTest, baseMecanumMotionCalculator )
+TEST(mecanumCalculator, MecanumMotionCalculator )
 {
-    Calculator calc{};
-    const QuadDriveData test{1.0,1.0,1.0,1.0};
-    const Translation2D forwardTestExpectedMotion{0.0, 1.0};
+    using namespace units::literals;
+    logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 1.0_m};
+    const logic::chassis::QuadDriveData test {1.0_rpm, 1.0_rpm, 1.0_rpm, 1.0_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 1.0_mps};
+    const RPM expectedRotation{0.0};
     calc.setWheelSpeeds(test);
-    std::cout << calc.getTranslation().x;
-    EXPECT_DOUBLE_EQ(calc.getTranslation().x, forwardTestExpectedMotion.x);
-    EXPECT_DOUBLE_EQ(calc.getTranslation().y, forwardTestExpectedMotion.y);
-}
-
-TEST(leftTest, baseMecanumMotionCalculator )
-{
-    Calculator calc{};
-    const QuadDriveData test{1.0,-1.0,-1.0,1.0};
-    const Translation2D expectedMotion{1.0, 0.0};
-    calc.setWheelSpeeds(test);
-    EXPECT_DOUBLE_EQ(calc.getTranslation().x, expectedMotion.x);
-    EXPECT_DOUBLE_EQ(calc.getTranslation().y, expectedMotion.y);
-}
-
-TEST(zeroTest, baseMecanumMotionCalculator )
-{
-
-    Calculator calc{};
-
-    const QuadDriveData test{0,0,0,0};
-    const Translation2D expectedMotion{0.0, 0.0};
-    calc.setWheelSpeeds(test);
-    EXPECT_DOUBLE_EQ(calc.getTranslation().x, expectedMotion.x);
-    EXPECT_DOUBLE_EQ(calc.getTranslation().y, expectedMotion.y);
-}
-
-TEST(clockwiseOffset, baseMecanumMotionCalculator )
-{
-    Calculator calc{};
-
-    const QuadDriveData test{-1,1,-1,1};
-    const double expectedOffset{1};
-    calc.setWheelSpeeds(test);
-    EXPECT_DOUBLE_EQ(calc.getRotationOffset(), expectedOffset);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
 }
