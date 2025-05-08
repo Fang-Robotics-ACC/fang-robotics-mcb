eb0fd55d5f8b08a03d76ad0258c68184f2e890a3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-08 11:47:14 -0500

    Add more forward tests

--- fang-mcb-project/test/mecanumcalculatortest.cpp
+++ fang-mcb-project/test/mecanumcalculatortest.cpp
@@ -8,6 +8,7 @@
 TEST(mecanumCalculator, MecanumMotionCalculator )
 {
     using namespace units::literals;
+    {
     logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 1.0_m};
     const logic::chassis::QuadDriveData test {1.0_rpm, 1.0_rpm, 1.0_rpm, 1.0_rpm};
     const logic::chassis::Velocity2D expected{0.0_mps, 1.0_mps};
@@ -16,4 +17,27 @@ TEST(mecanumCalculator, MecanumMotionCalculator )
     EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
     EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
     EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
+
+    {
+    logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 4.0_m};
+    const logic::chassis::QuadDriveData test {1.0_rpm, 1.0_rpm, 1.0_rpm, 1.0_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 4.0_mps};
+    const RPM expectedRotation{0.0};
+    calc.setWheelSpeeds(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
+
+    {
+    logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 1.0_m};
+    const logic::chassis::QuadDriveData test {1.5_rpm, 1.5_rpm, 1.5_rpm, 1.5_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 1.5_mps};
+    const RPM expectedRotation{0.0};
+    calc.setWheelSpeeds(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
 }
