366dd8c2949b561330717802955c1904c9e8130a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-08 19:11:03 -0500

    Add clockwise tests for mecanum calculators

--- fang-mcb-project/test/mecanumcalculatortest.cpp
+++ fang-mcb-project/test/mecanumcalculatortest.cpp
@@ -192,3 +192,42 @@ TEST(counterClockwiseTest, MecanumMotionCalculator)
     EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
     }
 }
+
+TEST(clockwiseTest, MecanumMotionCalculator)
+{
+    using namespace units::literals;
+    {
+    logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 1.0_m};
+    const logic::chassis::QuadDriveData test {1.0_rpm, -1.0_rpm, 1.0_rpm, -1.0_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 0.0_mps};
+    const RPM expectedRotation{-1.0};
+    calc.setWheelSpeeds(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
+
+    //Wheel size test
+    {
+    logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 4.0_m};
+    const logic::chassis::QuadDriveData test {1.0_rpm, -1.0_rpm, 1.0_rpm, -1.0_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 0.0_mps};
+    const RPM expectedRotation{-4.0};
+    calc.setWheelSpeeds(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
+
+    //Different chassis size test
+    {
+    logic::chassis::MecanumCalculator calc{2.0_m, 2.0_m, 1.0_m};
+    const logic::chassis::QuadDriveData test {1.0_rpm, -1.0_rpm, 1.0_rpm, -1.0_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 0.0_mps};
+    const RPM expectedRotation{-0.5};
+    calc.setWheelSpeeds(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
+}
