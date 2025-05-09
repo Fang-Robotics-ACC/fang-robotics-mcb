bff354bc86a6957a12afc2810f0b18ca2df7e5cc
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-08 19:35:20 -0500

    Add zero test for robot mecanum logic

--- fang-mcb-project/test/robotmecanumlogictest.cpp
+++ fang-mcb-project/test/robotmecanumlogictest.cpp
@@ -1,2 +1,33 @@
 #include "gtest/gtest.h"
+#include "unitaliases.hpp"
+#include "chassislogicaliases.hpp"
 #include "robotmecanumlogic.hpp"
+#include "mecanumcalculator.hpp"
+
+TEST(zeroTest, robotMecanumLogic)
+{
+    {
+        using namespace units::literals;
+        const Meters horizontalWheelDistance{1};
+        const Meters verticalWheelDistance{1};
+        const Meters wheelRadius{1};
+
+        const logic::chassis::Velocity2D translation{0.0_mps, 0.0_mps};
+        const RPM rotation{0.0};
+
+        logic::chassis::RobotMecanumLogic mecanumLogic{horizontalWheelDistance, verticalWheelDistance, wheelRadius};
+        logic::chassis::MecanumCalculator mecanumCalc{horizontalWheelDistance, verticalWheelDistance, wheelRadius};
+
+        mecanumLogic.setTranslation(translation);
+        mecanumLogic.setRotation(rotation);
+
+        mecanumCalc.setWheelSpeeds(mecanumLogic.getWheelSpeeds());
+
+        const logic::chassis::Velocity2D outputTranslation{mecanumCalc.getTranslation()};
+        const RPM outputRotation{mecanumCalc.getRotation()};
+
+        EXPECT_DOUBLE_EQ(translation.x.to<double>(), outputTranslation.x.to<double>());
+        EXPECT_DOUBLE_EQ(translation.y.to<double>(), outputTranslation.y.to<double>());
+        EXPECT_DOUBLE_EQ(rotation.to<double>(), outputRotation.to<double>());
+    }
+}
