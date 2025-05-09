84535bf344150e894960e5dc9a810473a74878c9
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-08 19:12:13 -0500

    Add zeroTest for mecanum calculator

--- fang-mcb-project/test/mecanumcalculatortest.cpp
+++ fang-mcb-project/test/mecanumcalculatortest.cpp
@@ -231,3 +231,19 @@ TEST(clockwiseTest, MecanumMotionCalculator)
     EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
     }
 }
+
+TEST(zeroTest, MecanumMotionCalculator)
+{
+    using namespace units::literals;
+    {
+    logic::chassis::MecanumCalculator calc{1.0_m, 1.0_m, 1.0_m};
+    const logic::chassis::QuadDriveData test {0.0_rpm, 0.0_rpm, 0.0_rpm, 0.0_rpm};
+    const logic::chassis::Velocity2D expected{0.0_mps, 0.0_mps};
+    const RPM expectedRotation{0.0};
+    calc.setWheelSpeeds(test);
+    EXPECT_DOUBLE_EQ(calc.getTranslation().x.to<double>(), expected.x.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getTranslation().y.to<double>(), expected.y.to<double>());
+    EXPECT_DOUBLE_EQ(calc.getRotation().to<double>(), expectedRotation.to<double>());
+    }
+}
+
