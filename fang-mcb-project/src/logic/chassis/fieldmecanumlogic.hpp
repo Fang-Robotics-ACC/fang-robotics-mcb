7d9fc0e4dcbb63db7d4de8f2e03a8e56cc2e572d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-09 00:10:20 -0500

    Declare raw angle setter
    This will be used to make the calculations more efficient

--- fang-mcb-project/src/logic/chassis/fieldmecanumlogic.hpp
+++ fang-mcb-project/src/logic/chassis/fieldmecanumlogic.hpp
@@ -58,6 +58,9 @@ namespace logic
         double getRearRightWheelSpeed() const;
 
         private:
+        //This does not update the translation when changed.
+        //It is useful in certain cases to speed stuff up.
+        void setRawRotation(const RPM& rotation);
         Velocity2D fieldToRobotTranslation(const Velocity2D& translation) const;
         Velocity2D robotToFieldTranslation(const Velocity2D& translation) const;
         
