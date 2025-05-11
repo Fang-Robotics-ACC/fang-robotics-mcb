52fbf367ef7e348f3a30b0e15726f44fd0d4c93f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-10 23:42:53 -0500

    Initialize 1st mock test

---
+++ fang-mcb-project/test/gearboxrepeatultramk2test.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "gearboxrepeatultramk2.hpp"
+#include "directionality.hpp"
+#include "unitaliases.hpp"
+#include "tap/drivers.hpp"
+#include "tap/communication/gpio/pwm.hpp"
+
+using namespace units::literals;
+TEST(minSpeedTest, repeatUltraMk2)
+{
+    constexpr Volts controllerVoltage{24};
+    constexpr Hertz frequency{500};
+    constexpr double gearRatio{14.0};
+
+    
+    tap::Drivers drivers{};
+    motors::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, data::motors::Directionality::UNIDIRECTIONAL, gearRatio};
+    motor.setSpeed(0_rpm);
+    drivers.pwm.write(0.0, tap::gpio::Pwm::C1);
+
+    EXPECT_CALL(drivers.pwm,write(0.0, tap::gpio::Pwm::C1));
+}
