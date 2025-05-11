5b945509f51d3a480d5e4a6658c2395858d968c2
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-10 22:08:42 -0500

    Initialize chassis subsystem

--- fang-mcb-project/src/control/chassis/chassissubsystem.hpp
+++ fang-mcb-project/src/control/chassis/chassissubsystem.hpp
@@ -1,4 +1,17 @@
 #ifndef FANG_ROBOTICS_MCB_CHASSIS_SUBSYSTEM_HPP
 #define FANG_ROBOTICS_MCB_CHASSIS_SUBSYSTEM_HPP
+#include "tap/control/subsystem.hpp"
 
+namespace control
+{
+    namespace chassis
+    {
+        class ChassisSubsystem : public tap::control::Subsystem
+        {
+        public:
+        
+
+        };
+    }//namespace chassis
+}//namespace control
 #endif
