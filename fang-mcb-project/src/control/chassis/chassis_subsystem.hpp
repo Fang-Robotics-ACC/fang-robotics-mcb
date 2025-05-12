7c317722a652acde0350bf04e106bdd9efac906f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-11 21:15:50 -0500

    feat(control): Add chassis configuration struct

--- fang-mcb-project/src/control/chassis/chassis_subsystem.hpp
+++ fang-mcb-project/src/control/chassis/chassis_subsystem.hpp
@@ -1,15 +1,23 @@
 #ifndef FANG_ROBOTICS_MCB_CHASSIS_SUBSYSTEM_HPP
 #define FANG_ROBOTICS_MCB_CHASSIS_SUBSYSTEM_HPP
 #include "tap/control/subsystem.hpp"
+#include "motors/gearboxrepeatultramk2.hpp"
 
 namespace control
 {
     namespace chassis
     {
+        using DriveMotor = motors::GearboxRepeatUltraMk2;
+        struct ChassisConfig
+        {
+            DriveMotor& frontLeft;
+            DriveMotor& frontRight;
+            DriveMotor& rearLeft;
+            DriveMotor& rearRight;
+        };
         class ChassisSubsystem : public tap::control::Subsystem
         {
         public:
-        
 
         };
     }//namespace chassis
