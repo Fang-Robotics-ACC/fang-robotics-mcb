96d87a5c89bc2df49b3e21516b53e5a3e2fa0d2b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-20 15:01:48 -0500

    feat(ChassisMecanumCommand): Initialize inherited declarations

---
+++ fang-mcb-project/src/control/chassis/chassis_mecanum_command.hpp
@@ -0,0 +1,33 @@
+#ifndef FANG_ROBOTICS_MCB_CHASSIS_MECANUM_COMMAND_HPP
+#define FANG_ROBOTICS_MCB_CHASSIS_MECANUM_COMMAND_HPP
+#include "control/input_handler.hpp"
+#include "control/chassis/chassis_subsystem.hpp"
+
+#include "tap/control/command.hpp"
+
+namespace control
+{
+    namespace chassis
+    {
+        /**
+         * This intermediates the inputs and the mecanum drive 
+         */
+        class ChassisMecanumCommand : public tap::control::Command
+        {
+            /**
+             * This takes a chassis subsystem and the respective inputHandler
+             */
+            ChassisMecanumCommand(ChassisSubsystem& chassisSubsystem, InputHandler& inputHandler);
+            const char* getName() const override;
+            void initialize() override;
+            void execute() override;
+            void end(bool interrupted) override;
+            bool isFinished() const;
+        private:
+            static constexpr char* mk_name{"Chassis tank drive"};
+            ChassisSubsystem& m_chassisSubsystem;
+            InputHandler& m_inputHandler;
+        };
+    }//namespace control
+}//namespace chassis
+#endif
