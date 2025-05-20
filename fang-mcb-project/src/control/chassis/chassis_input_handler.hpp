87b1fe0c070580f9f672fd8d306f712f435b7b9a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-20 12:08:07 -0500

    feat(ChassisInputHandler): Add configuration struct

---
+++ fang-mcb-project/src/control/chassis/chassis_input_handler.hpp
@@ -0,0 +1,45 @@
+#ifndef FANG_ROBOTICS_MCB_CHASSIS_INPUT_HANDLER_HPP
+#define FANG_ROBOTICS_MCB_CHASSIS_INPUT_HANDLER_HPP
+#include "configuration/unitaliases.hpp"
+#include "configuration/chassislogicaliases.hpp"
+#include "tap/communication/serial/remote.hpp"
+
+namespace control
+{
+    namespace chassis
+    {
+        class ChassisInputHandler
+        {
+        public:
+        /**
+         * These are the values that ChassisInputHandler scales
+         * They should be positive
+         */
+        struct Config
+        {
+            MetersPerSecond xTranslationMax;
+            MetersPerSecond yTranslationMax;
+            RPM rotationMax;
+        };
+            using Remote = tap::communication::serial::Remote;
+
+            ChassisInputHandler(Remote& remote);
+            /**
+             * Returns the desired translation motion
+             * Depending on which mode is used, this could be relative to the robot chassis,
+             * the field, or the 
+             * The barrel facing forward is the positive y direciton.
+             * This interpets the dji remote inputs
+             */
+            logic::chassis::Velocity2D getRemoteTranslation() const;
+
+            /**
+             * Returns the speed that the turret should rotate
+             */
+            Radians getRemoteTurretDisplacement() const;
+        private:
+            Remote& m_remote;
+        };
+    }// namespace chassis
+}//namespace control
+#endif
