ddcb7dd2d6b0c53a791cc84aee9dbd96f3787dbd
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-20 12:17:32 -0500

    feat(ChassisInputHandler): Add motion config

--- fang-mcb-project/src/control/chassis/chassis_input_handler.hpp
+++ fang-mcb-project/src/control/chassis/chassis_input_handler.hpp
@@ -1,6 +1,7 @@
 #ifndef FANG_ROBOTICS_MCB_CHASSIS_INPUT_HANDLER_HPP
 #define FANG_ROBOTICS_MCB_CHASSIS_INPUT_HANDLER_HPP
 #include "configuration/unitaliases.hpp"
+#include "configuration/motion_control_config.hpp"
 #include "configuration/chassislogicaliases.hpp"
 #include "tap/communication/serial/remote.hpp"
 
@@ -12,8 +13,9 @@ namespace control
         {
         public:
             using Remote = tap::communication::serial::Remote;
+            using MotionConfig = config::motion::MotionConfig;
 
-            ChassisInputHandler(Remote& remote);
+            ChassisInputHandler(Remote& remote, MotionConfig& motionConfig);
             /**
              * Returns the desired translation motion
              * Depending on which mode is used, this could be relative to the robot chassis,
@@ -29,6 +31,7 @@ namespace control
             Radians getRemoteTurretDisplacement() const;
         private:
             Remote& m_remote;
+            const MotionConfig mk_motionConfig;
         };
     }// namespace chassis
 }//namespace control
