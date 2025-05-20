21d77ed3b9762443a7a5dfe815abc9a6160ffd1a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-20 12:15:04 -0500

    feat(config): Add motion control constants and structs

---
+++ fang-mcb-project/src/configuration/motion_control_config.hpp
@@ -0,0 +1,29 @@
+#ifndef FANG_ROBOTICS_MCB_MOTION_CONTROL_CONFIG_HPP
+#define FANG_ROBOTICS_MCB_MOTION_CONTROL_CONFIG_HPP
+#include "unitaliases.hpp"
+namespace config
+{
+    namespace chassis
+    {
+        using namespace units::literals;
+        /**
+         * These are the values that are scaled
+         * 
+         * They should be positive.
+         */
+        struct MotionConfig
+        {
+            MetersPerSecond xTranslationMax;
+            MetersPerSecond yTranslationMax;
+            RPM rotationMax;
+        };
+
+        static const MotionConfig k_defaultMotionConfig
+        {
+            xTranslationMax = 5_mph,
+            yTranslationMax = 10_mph,
+            rotationMax = 60_rpm
+        };
+    }
+}
+#endif
