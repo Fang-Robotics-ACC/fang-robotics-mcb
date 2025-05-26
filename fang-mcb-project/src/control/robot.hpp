953a984997685404166f6d578e00090bcdc6e6bd
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-26 12:08:07 -0500

    refactor(Robot): Change robot class to a namespace alias

--- fang-mcb-project/src/control/robot.hpp
+++ fang-mcb-project/src/control/robot.hpp
@@ -1,6 +1,10 @@
 #ifndef FANG_ROBOTICS_MCB_ROBOT_HPP
 #define FANG_ROBOTICS_MCB_ROBOT_HPP
+namespace control
+{
 #if defined(TARGET_INFANTRY)
 #include "infantry.hpp"
+using Robot = Infantry;
 #endif
+}
 #endif
