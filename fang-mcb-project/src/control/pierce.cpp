1ff31bbba9aafc8e968e6bd4422c1dbbb5c3ccd9
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-26 15:19:38 -0500

    feat(Pierce): Have commands register subsystems

--- fang-mcb-project/src/control/pierce.cpp
+++ fang-mcb-project/src/control/pierce.cpp
@@ -12,18 +12,25 @@ namespace control
     
     void Pierce::initializeSubsystemCommands()
     {
+        initializeSubsystems();
+        registerSubsystems();
+        setDefaultCommands();
+        registerIoMappings();
     }
     
     void Pierce::initializeSubsystems()
     {
+        m_chassis.initialize();
     }
     
     void Pierce::registerSubsystems()
     {
+        m_drivers.commandScheduler.registerSubsystem(&m_chassis);
     }
     
     void Pierce::setDefaultCommands()
     {
+        m_chassis.setDefaultCommand(&m_fieldMecanumCommand);
     }
     
     void Pierce::registerIoMappings()
