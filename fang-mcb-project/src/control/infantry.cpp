6d3e8de8c05abcec6d004a423ce60bd2a8752648
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-26 12:21:32 -0500

    feat(Pierce): Initialize implmentation

---
+++ fang-mcb-project/src/control/infantry.cpp
@@ -0,0 +1,29 @@
+#include "infantry.hpp"
+#include "drivers.hpp"
+
+namespace control
+{
+    Pierce::Pierce(Drivers& drivers) : m_drivers{drivers}
+    {
+    }
+    
+    void Pierce::initializeSubsystemCommands()
+    {
+    }
+    
+    void Pierce::initializeSubsystems()
+    {
+    }
+    
+    void Pierce::registerSubsystems()
+    {
+    }
+    
+    void Pierce::setDefaultCommands()
+    {
+    }
+    
+    void Pierce::registerIoMappings()
+    {
+    }
+}
