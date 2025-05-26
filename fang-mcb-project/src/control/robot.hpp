e6517e407359991268bc886494be1217ac7fd844
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-26 13:27:06 -0500

    fix(Robot): Redo namespacing to remove scope errors
    Ahhh, the dangers of preprocessor directives

--- fang-mcb-project/src/control/robot.hpp
+++ fang-mcb-project/src/control/robot.hpp
@@ -1,11 +1,8 @@
 #ifndef FANG_ROBOTICS_MCB_ROBOT_HPP
 #define FANG_ROBOTICS_MCB_ROBOT_HPP
 #include "robot_type.hpp"
-namespace control
-{
-    #if defined(TARGET_INFANTRY_PIERCE)
-    #include "pierce.hpp"
-    using Robot = Pierce;
-    #endif
-}
+#if defined(TARGET_INFANTRY_PIERCE)
+#include "pierce.hpp"
+using Robot = control::Pierce;
+#endif
 #endif
