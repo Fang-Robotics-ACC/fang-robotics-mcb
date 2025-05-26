346f7859cf19e716e8454194b4ffcfb0af664215
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-25 19:56:06 -0500

    feat(FieldMecanumCommand): Implement switch for various execute modes
    Moved original execute cote to executeRemoteFieldRotate()

--- fang-mcb-project/src/control/chassis/field_mecanum_command.cpp
+++ fang-mcb-project/src/control/chassis/field_mecanum_command.cpp
@@ -1,6 +1,8 @@
 #include "field_mecanum_command.hpp"
 #include "chassislogicaliases.hpp"
 #include "unitaliases.hpp"
+#include <cassert>
+
 namespace control
 {
     namespace chassis
@@ -21,9 +23,23 @@ namespace control
 
         void FieldMecanumCommand::execute()
         {
-            const logic::chassis::Velocity2D translation{m_inputHandler.getChassisInputs().getRemoteTranslation()};
-            const RPM rotation{m_inputHandler.getChassisInputs().getRemoteRotation()};
-            m_chassisSubsystem.setMotion(translation, rotation);
+            switch(m_controlMode)
+            {
+                case ControlMode::REMOTE_TEST_FIELD_ROTATE:
+                    executeRemoteTestFieldRotate();
+                    break;
+                case ControlMode::REMOTE_TEST_STRAFE_TURRET:
+                    executeRemoteTestStrafeTurret();
+                    break;
+                case ControlMode::KEYBOARD_TEST_FIELD_ROTATE:
+                    executeKeyboardTestFieldRotate();
+                    break;
+                case ControlMode::KEYBOARD_TEST_STRAFE_TURRET:
+                    executeKeyboardTestStrafeTurret();
+                    break;
+                default:
+                    assert(!"Unexpected or unsupported mode.");
+            }
         }
 
         void FieldMecanumCommand::end(bool interrupted)
@@ -35,7 +51,11 @@ namespace control
         }
 
         void FieldMecanumCommand::executeRemoteTestFieldRotate()
-        {}
+        {
+            const logic::chassis::Velocity2D translation{m_inputHandler.getChassisInputs().getRemoteTranslation()};
+            const RPM rotation{m_inputHandler.getChassisInputs().getRemoteRotation()};
+            m_chassisSubsystem.setMotion(translation, rotation);
+        }
 
         void FieldMecanumCommand::executeRemoteTestStrafeTurret()
         {}
