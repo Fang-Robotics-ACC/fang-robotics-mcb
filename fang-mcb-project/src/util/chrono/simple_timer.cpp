1ef98a2bc950d71612d25db59b04b72cedc6974e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-27 01:02:06 -0500

    feat(SimpleTimer): Implement more accurate duration function

--- fang-mcb-project/src/util/chrono/simple_timer.cpp
+++ fang-mcb-project/src/util/chrono/simple_timer.cpp
@@ -6,8 +6,26 @@ namespace chrono
     {
         reset();
     }
+
     void SimpleTimer::reset()
     {
         m_epoch = m_clock.now();
+    }
+
+    Microseconds SimpleTimer::getDuration() const
+    {
+        const RawDuration rawDuration{m_clock.now() - m_epoch};
+        return Microseconds{rawDuration};
+    }
+
+    Microseconds SimpleTimer::getDurationAndReset()
+    {
+        //Store now so that the exact slice can be set as the new epoch
+        //as time will pas when the duration is being calculated
+        //and if now() was called again it could be different
+        const TimePoint now{m_clock.now()};
+        const RawDuration rawDuration{now - m_epoch};
+        m_epoch = now;
+        return Microseconds{rawDuration};
     }
 }
