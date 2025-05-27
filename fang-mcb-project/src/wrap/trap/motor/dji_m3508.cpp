ae9f5e0d1f1ff745964738aca8ef046fea02be61
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-26 22:51:53 -0500

    feat(DjiM3508): Initialize basic wrapper and constructors

---
+++ fang-mcb-project/src/wrap/trap/motor/dji_m3508.cpp
@@ -0,0 +1,16 @@
+#include "dji_m3508.hpp"
+namespace trap
+{
+    namespace motor
+    {
+        DjiM3508::DjiM3508(Drivers& drivers, const Config& config)
+        : DjiM3508{drivers, config.motorId, config.canBus, config.name,
+                   config.inverted, config.currentControl, config.gearRatio}
+        {}
+
+        DjiM3508::DjiM3508(Drivers& drivers, tap::motor::MotorId motorId, tap::can::CanBus canBus,
+                           const char* name, bool inverted, bool currentControl, double gearRatio)
+        : m_djiMotor{&drivers, motorId, canBus, inverted, name, currentControl, gearRatio}
+        {}
+    }
+}
---
+++ fang-mcb-project/src/wrap/trap/motor/dji_m3508.hpp
@@ -0,0 +1,36 @@
+#ifndef FANG_ROBOTICS_MCB_TRAP_DJI_M3508_HPP
+#define FANG_ROBOTICS_MCB_TRAP_DJI_M3508_HPP
+#include "unitaliases.hpp"
+
+#include "tap/motor/dji_motor.hpp"
+#include "tap/communication/can/can_bus.hpp"
+#include "drivers.hpp"
+
+namespace trap
+{
+    namespace motor
+    {
+        /**
+         * Wrapper for DJI motor for the DJI M3508 on a CAN bus
+         */
+        class DjiM3508
+        {
+        public:
+            struct Config
+            {
+                tap::motor::MotorId motorId;
+                tap::can::CanBus canBus;
+                const char* name;
+                bool inverted;
+                bool currentControl;
+                double gearRatio;
+            };
+            DjiM3508(Drivers& drivers, const Config& config);
+            DjiM3508(Drivers& drivers, tap::motor::MotorId motorId, tap::can::CanBus canBus,
+                     const char* name, bool inverted, bool currentControl, double gearRatio);
+        private:
+            tap::motor::DjiMotor m_djiMotor;
+        };
+    }
+}
+#endif
