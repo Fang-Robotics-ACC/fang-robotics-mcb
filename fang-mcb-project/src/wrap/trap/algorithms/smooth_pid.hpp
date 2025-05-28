b551476a4f214947f1ba4ed9287898b24b5e69b6
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-27 23:40:36 -0500

    feat(SmoothPid): change SmoothPid to work with non-unit types as well

--- fang-mcb-project/src/wrap/trap/algorithms/smooth_pid.hpp
+++ fang-mcb-project/src/wrap/trap/algorithms/smooth_pid.hpp
@@ -2,6 +2,7 @@
 #define FANG_ROBOTICS_MCB_TRAP_SMOOTH_PID_HPP
 #include "tap/algorithms/smooth_pid.hpp"
 #include "util/chrono/simple_timer.hpp"
+#include "configuration/unitaliases.hpp"
 
 namespace trap
 {
@@ -12,20 +13,14 @@ namespace trap
          * The error unit is the unit that the system takes as an input
          * The time unit is unit for the passage of time
          */
-        template<typename ErrorUnit, typename ControlUnit, typename TimeUnit>
+        template<typename ErrorType, typename ControlType>
         class SmoothPid
         {
         public:
-
-            using ControlUnit = units::unit_t<RawControlUnit>;
-            using ErrorUnit = units::unit_t<RawErrorUnit>;
-            using TimeUnit = units::unit_t<RawTimeUnit>;
-            using ErrorDerivativeUnit = units::compound_unit<RawErrorUnit, units::inverse<RawTimeUnit>>;
-
             //A bit of cheating
             using Config = tap::algorithms::SmoothPidConfig;
 
-            SmoothPid(const Config& config, ErrorUnit initialLastError = ErrorUnit{0})
+            SmoothPid(const Config& config, ErrorType initialLastError = ErrorType{0})
             : m_smoothPid{config}, m_lastError{initialLastError}
             {
             }
@@ -35,18 +30,19 @@ namespace trap
              * This wrapper automatically calculates how much the error has changed
              * and the amount of time which has passed between calls
              */
-            ControlUnit runController(ErrorUnit error)
+            ControlType runController(ErrorType error)
             {
-                const TimeUnit deltaTime{m_runControllerTimer.getDurationAndReset()};
-                const ErrorUnit deltaError{error - m_lastError};
-                const ErrorDerivativeUnit errorDerivative{error / deltaTime};
+                const Microseconds deltaTime{m_runControllerTimer.getDurationAndReset()};
+                const ErrorType deltaError{error - m_lastError};
+                const double errorDerivative{static_cast<double>(error) / deltaTime};
                 runController(error, errorDerivative, deltaTime);
             }
 
             /**
              * Manually include delta time (this is for testing purposes but may bear use to future testers)
+             * For implementation purposes, the errorDerivative is stripped of any unit
              */
-            ControlUnit runController(ErrorUnit error, ErrorDerivativeUnit errorDerivative, TimeUnit deltaTime)
+            ControlType runController(ErrorUnit error, double errorDerivative, Microseconds deltaTime)
             {
                 float result {m_smoothPid.runController(static_cast<float>(error), estatic_cast<float>(errorDerivative), static_cast<float>(deltaTime))};
                 return ControlUnit{result};
@@ -54,7 +50,7 @@ namespace trap
 
         private:
             tap::algorithms::SmoothPid m_smoothPid;
-            ErrorUnit m_lastError{0};
+            ErrorType m_lastError{0};
             chrono::SimpleTimer m_runControllerTimer{};
         };
 
