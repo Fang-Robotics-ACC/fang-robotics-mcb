2c25eb41fd3fbff2783b7b6d2bb3490405c88b87
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-28 12:15:22 -0500

    feat(DjiMotorAliases): Add DjiSpeedPid alias

--- fang-mcb-project/src/wrap/trap/motor/dji_motor_aliases.hpp
+++ fang-mcb-project/src/wrap/trap/motor/dji_motor_aliases.hpp
@@ -1,11 +1,14 @@
 #ifndef FANG_ROBOTICS_MCB_TRAP_DJI_MOTOR_ALIASES_HPP
 #define FANG_ROBOTICS_MCB_TRAP_DJI_MOTOR_ALIASES_HPP
+#include "wrap/trap/algorithms/smooth_pid.hpp"
+#include "unitaliases.hpp"
 #include <cstdint>
 namespace trap
 {
     namespace motor
     {
         using DjiMotorOutput = int16_t;
+        using DjiSpeedPid = trap::algorithms::SmoothPid<RPM, DjiMotorOutput>;
     }//namespace motor
 }//namespace trap
 #endif
