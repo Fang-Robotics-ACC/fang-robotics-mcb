d9af8346c09ef31d4628136f0715e627ecf78403
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-28 12:06:00 -0500

    feat(DjiMotorAliases): Add aliases

---
+++ fang-mcb-project/src/wrap/trap/motor/dji_motor_aliases.hpp
@@ -0,0 +1,11 @@
+#ifndef FANG_ROBOTICS_MCB_TRAP_DJI_MOTOR_ALIASES_HPP
+#define FANG_ROBOTICS_MCB_TRAP_DJI_MOTOR_ALIASES_HPP
+#include <cstdint>
+namespace trap
+{
+    namespace motor
+    {
+        using DjiMotorOutput = int16_t;
+    }//namespace motor
+}//namespace trap
+#endif
