45867ace3bdde983b522ed0e241fa5b969a87e7e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-30 19:39:44 -0500

    test(Vortex80AEsc): Update tests to new constructor

--- fang-mcb-project/test/vortex80aesclogictest.cpp
+++ fang-mcb-project/test/vortex80aesclogictest.cpp
@@ -1,46 +1,61 @@
 #include <gtest/gtest.h>
-#include "vortex80aesclogic.hpp"
+
+#include "motors/vortex80aesc.hpp"
+#include "trap/communication/pwm_data.hpp"
 #include "unitaliases.hpp"
+#include "drivers.hpp"
+
+#include "units.h"
+
+#include "tap/communication/gpio/pwm.hpp"
+
+using namespace units::literals;
+
+Drivers drivers{};
+trap::gpio::PwmData pwmData{tap::gpio::Pwm::C1, 500_Hz};
+trap::gpio::PwmData nonStandardPwmData{tap::gpio::Pwm::C1, 250_Hz};
+
 
 TEST(zeroTest, Vortex80AEsc)
 {
     //2 ms period
     const Hertz standardFrequency{500};
-    logic::motors::Vortex80AEsc vortex {};
+    logic::motors::Vortex80AEsc vortex {drivers.pwm, pwmData};
 
     EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(0.0, standardFrequency), 0.5);
 }
 
 TEST(midRangeTest, Vortex80AEsc)
 {
-    //2 ms period
+
     const Hertz standardFrequency{500};
-    logic::motors::Vortex80AEsc vortex {};
+    //2 ms period
+    logic::motors::Vortex80AEsc vortex {drivers.pwm, pwmData};
 
     EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(0.5, standardFrequency), 0.75);
 }
 
 TEST(fullRangeTest, Vortex80AEsc)
 {
-    //2 ms period
     const Hertz standardFrequency{500};
-    logic::motors::Vortex80AEsc vortex {};
+    //2 ms period
+    logic::motors::Vortex80AEsc vortex {drivers.pwm, pwmData};
 
     EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(1, standardFrequency), 1.0);
 }
 
 TEST(oddRangeTest, Vortex80AEsc)
 {
-    //4 ms period
     const Hertz nonStandardFrequency{250};
-    logic::motors::Vortex80AEsc vortex {};
+    //4 ms period
+    logic::motors::Vortex80AEsc vortex {drivers.pwm, nonStandardPwmData};
 
     EXPECT_DOUBLE_EQ(vortex.adaptedDutyCycle(1, nonStandardFrequency), 0.5);
 }
 
 TEST(bidirectionalTest, Vortex80AEsc)
 {
-    logic::motors::Vortex80AEsc vortex {data::motors::Directionality::BIDIRECTIONAL};
+    logic::motors::Vortex80AEsc vortex {drivers.pwm, pwmData};
 
     EXPECT_DOUBLE_EQ(vortex.calculateDutyCycle(-1.0), 0.5);
     EXPECT_DOUBLE_EQ(vortex.calculateDutyCycle(0.0), 0.75);
@@ -49,7 +64,7 @@ TEST(bidirectionalTest, Vortex80AEsc)
 
 TEST(unidirectionalTest, Vortex80AEsc)
 {
-    logic::motors::Vortex80AEsc vortex {data::motors::Directionality::UNIDIRECTIONAL};
+    logic::motors::Vortex80AEsc vortex {drivers.pwm, pwmData, data::motors::Directionality::UNIDIRECTIONAL};
 
     EXPECT_DOUBLE_EQ(vortex.calculateDutyCycle(0.0), 0.5);
     EXPECT_DOUBLE_EQ(vortex.calculateDutyCycle(0.50), 0.75);
