d13eda7a196c8adbf63f2d11df42e1f7ae8fb347
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-05-31 21:17:00 -0500

    clean(Vortex80AEsc): Remove unused documentation

--- fang-mcb-project/src/motors/vortex80aesc.hpp
+++ fang-mcb-project/src/motors/vortex80aesc.hpp
@@ -28,29 +28,6 @@ namespace logic
              */
             void setPulseDuration(const Microseconds& duration);
             Microseconds calculatePeriod (double speedRangePercentage);
-            /**
-             * 
-             * Returns a duty cycle from
-             * [0,1] (percentage in decimal form)
-             * 
-             * The Vortex motor controller expects
-             * a pulse from 1ms to 2ms with 1ms
-             * corresponding to the lowest possible input, 0 and
-             * 2ms corresponding to the highest input, 1.
-             * 
-             * In unidirectional, the speed percentage range
-             * is [0,1]
-             * Therefore, 0.5 would correspond to a motor moving
-             * 50% of the maximum speed
-             * 
-             * In bidirectional, the range is [-1,1]
-             * Therefore, 0.5 would correspond to a motor being still.
-             * 0 would correspond to a motor running at full speed in reverse
-             * Frequencies with a period lower than 2ms will not work on this
-             * controller. The required duty cycle would be greater than 1.
-             * In other words, anything greater than 500 Hz since you need to
-             * have a pulse at least 2ms long.
-             */
             void setDirectionality(const Directionality& directionality);
             Directionality getDirectionality() const;
         private:
