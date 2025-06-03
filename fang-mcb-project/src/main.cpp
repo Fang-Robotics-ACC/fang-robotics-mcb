9b4460019666e90019f2bf87ea1e74ddb2e860ad
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-02 20:15:47 -0500

    feat(main): Motor position test

--- fang-mcb-project/src/main.cpp
+++ fang-mcb-project/src/main.cpp
@@ -82,7 +82,7 @@ int main()
 
     Board::initialize();
     initializeIo(drivers);
-    trap::motor::DjiSpeedPid::Config motorPidConfig{0, 10, 0 ,50000, trap::motor::DjiGM6020::k_maxOutput};
+    trap::motor::DjiSpeedPid::Config motorPidConfig{2500, 100, 0 ,500, trap::motor::DjiGM6020::k_maxOutput};
     trap::motor::DjiGM6020::Config config{static_cast<tap::motor::MotorId>(tap::motor::MOTOR1), tap::can::CanBus::CAN_BUS1, "epic", false,  1.0, motorPidConfig, false};
     trap::motor::DjiGM6020 motor{*drivers, config};
 
@@ -94,7 +94,7 @@ int main()
     //bool ledFlash{djiDriver.isMotorOnline()};
     motor.initialize();
 
-    motor.setTargetSpeed(2000_rpm);
+    motor.setTargetPosition(0.0_rad);
     //motor.setDesiredOutput(1000);
 
 #ifdef PLATFORM_HOSTED
