fb12e5e4af1305e330a9c7a71227a7a4b951bf43
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-04 13:28:56 -0500

    test(ChassisInputHandler): Update tests to new api

--- fang-mcb-project/test/chassis_input_handler_test.cpp
+++ fang-mcb-project/test/chassis_input_handler_test.cpp
@@ -10,21 +10,20 @@
 
 #include <tuple>
 
-using Velocity2D = logic::chassis::Velocity2D;
+using AbstractVector2D = math::AbstractVector2D;
 using ::testing::Return;
 using namespace units::literals;
 
-const config::motion::MotionConfig& k_defaultConfig{config::motion::k_defaultMotionConfig};
-class ChassisInputHandlerTest :  public ::testing::TestWithParam<std::tuple<double, double, double, Velocity2D, RPM, Radians>>
+class ChassisInputHandlerTest :  public ::testing::TestWithParam<std::tuple<double, double, double, math::AbstractVector2D, double, double>>
 {
 protected:
-    double m_leftJoystickHorizontal{std::get<0>(GetParam())};
-    double m_leftJoystickVertical{std::get<1>(GetParam())};
-    double m_rightJoystickHorizontal{std::get<2>(GetParam())};
+    const double m_leftJoystickHorizontal{std::get<0>(GetParam())};
+    const double m_leftJoystickVertical{std::get<1>(GetParam())};
+    const double m_rightJoystickHorizontal{std::get<2>(GetParam())};
 
-    Velocity2D m_expectedTranslation{std::get<3>(GetParam())};
-    RPM m_expectedRPM{std::get<4>(GetParam())};
-    Radians m_expectedDisplacement{std::get<5>(GetParam())};
+    const math::AbstractVector2D m_expectedTranslation{std::get<3>(GetParam())};
+    const double m_expectedRPM{std::get<4>(GetParam())};
+    const double m_expectedDisplacement{std::get<5>(GetParam())};
 
     Drivers m_drivers{};
     tap::mock::RemoteMock& m_remote{m_drivers.remote};
@@ -46,22 +45,21 @@ TEST_P(ChassisInputHandlerTest, inputHandlingTest)
         .WillByDefault(Return(m_rightJoystickHorizontal));
 
     //Check answers
-    const Velocity2D translationOutput{m_chassisInputHandler.getRemoteTranslation()};
-    const RPM rotationOutput{m_chassisInputHandler.getRemoteRotation()};
-    const Radians angularDisplacementOutput{m_chassisInputHandler.getRemoteAngularDisplacement()};
+    const math::AbstractVector2D translationOutput{m_chassisInputHandler.getRemoteTranslation()};
+    const double rotationOutput{m_chassisInputHandler.getRemoteRotation()};
+    const double angularDisplacementOutput{m_chassisInputHandler.getRemoteAngularDisplacement()};
 
-    EXPECT_DOUBLE_EQ(translationOutput.x.to<double>(), m_expectedTranslation.x.to<double>());
-    EXPECT_DOUBLE_EQ(translationOutput.y.to<double>(), m_expectedTranslation.y.to<double>());
-    EXPECT_DOUBLE_EQ(rotationOutput.to<double>(), m_expectedRPM.to<double>());
-    EXPECT_DOUBLE_EQ(angularDisplacementOutput.to<double>(), m_expectedDisplacement.to<double>());
+    EXPECT_DOUBLE_EQ(translationOutput.x, m_expectedTranslation.x);
+    EXPECT_DOUBLE_EQ(translationOutput.y, m_expectedTranslation.y);
+    EXPECT_DOUBLE_EQ(rotationOutput, m_expectedRPM);
+    EXPECT_DOUBLE_EQ(angularDisplacementOutput, m_expectedDisplacement);
 }
 
-INSTANTIATE_TEST_SUITE_P(zeroTest, ChassisInputHandlerTest, testing::Values(std::make_tuple(0.0, 0.0, 0.0, Velocity2D{0.0_mps, 0.0_mps}, 0_rpm, 0_rad)));
+INSTANTIATE_TEST_SUITE_P(zeroTest, ChassisInputHandlerTest, testing::Values(std::make_tuple(0.0, 0.0, 0.0, AbstractVector2D{0.0, 0.0}, 0_rpm, 0_rad)));
 
 INSTANTIATE_TEST_SUITE_P(maxTest, ChassisInputHandlerTest,
-    testing::Values(std::make_tuple(1.0, 1.0, 1.0, Velocity2D{k_defaultConfig.maxXTranslation, k_defaultConfig.maxYTranslation},
-                    k_defaultConfig.maxRotation, k_defaultConfig.maxAngularDisplacement)));
+    testing::Values(std::make_tuple(1.0, 1.0, 1.0, AbstractVector2D{1.0, 1.0}, 1.0, 1.0)));
 
-INSTANTIATE_TEST_SUITE_P(negativeMaxTest, ChassisInputHandlerTest,
-    testing::Values(std::make_tuple(-1.0, -1.0, -1.0, Velocity2D{-k_defaultConfig.maxXTranslation, -k_defaultConfig.maxYTranslation},
-                    -k_defaultConfig.maxRotation, -k_defaultConfig.maxAngularDisplacement)));
+
+INSTANTIATE_TEST_SUITE_P(minTest, ChassisInputHandlerTest,
+    testing::Values(std::make_tuple(-1.0, -1.0, -1.0, AbstractVector2D{-1.0, -1.0}, -1.0, -1.0)));
