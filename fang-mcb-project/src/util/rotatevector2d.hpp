80c9e7234435872489e10f8b9351ea025e5d4b80
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-04 13:04:10 -0500

    fix(RotateVector2D): Update AbstractVector2D namespace to new type

--- fang-mcb-project/src/util/rotatevector2d.hpp
+++ fang-mcb-project/src/util/rotatevector2d.hpp
@@ -12,7 +12,7 @@ namespace util
          * This algorithm has an accuracy of within 1e-14; however, rotating the vector back will produce accuracy to only within
          * 1e-10
          */
-        math::AbstractVector2D rotateVector2D(const math::AbstractVector2D& vector, const Radians& rotationAngle);
+        ::math::AbstractVector2D rotateVector2D(const ::math::AbstractVector2D& vector, const Radians& rotationAngle);
     }
 }
 #endif
