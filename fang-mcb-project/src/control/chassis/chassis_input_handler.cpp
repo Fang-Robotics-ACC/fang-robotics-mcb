003007b5da876ace0140a7490baf69891e932005
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-04 15:44:42 -0500

    fix(ChassisInputHandler): Rearrange inputs due to faulty controller chanels

--- fang-mcb-project/src/control/chassis/chassis_input_handler.cpp
+++ fang-mcb-project/src/control/chassis/chassis_input_handler.cpp
@@ -15,22 +15,21 @@ namespace control
 
         math::AbstractVector2D ChassisInputHandler::getRemoteTranslation() const
         {
-            const double xTranslationScale{m_remote.getChannel(Remote::Channel::LEFT_HORIZONTAL)};
-
-            const double yTranslationScale{m_remote.getChannel(Remote::Channel::LEFT_VERTICAL)};
+            const double xTranslationScale{m_remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL)};
+            const double yTranslationScale{m_remote.getChannel(Remote::Channel::RIGHT_VERTICAL)};
 
             return math::AbstractVector2D{xTranslationScale, yTranslationScale};
         }
 
         double ChassisInputHandler::getRemoteAngularDisplacement() const
         {
-            const double angularDisplacementScale{m_remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL)};
+            const double angularDisplacementScale{m_remote.getChannel(Remote::Channel::LEFT_HORIZONTAL)};
             return angularDisplacementScale;
         }
 
         double ChassisInputHandler::getRemoteRotation() const
         {
-        const double rotationScale{m_remote.getChannel(Remote::Channel::RIGHT_HORIZONTAL)};
+        const double rotationScale{m_remote.getChannel(Remote::Channel::LEFT_HORIZONTAL)};
         return rotationScale;
         }
     }// namespce chassis
