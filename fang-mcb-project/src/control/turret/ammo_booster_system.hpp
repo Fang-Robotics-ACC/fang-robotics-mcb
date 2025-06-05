23c54744acb0c5d25d1808a10c164c664d0523c8
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-05 18:35:53 -0500

    feat(AmmoBooster): Initialize

---
+++ fang-mcb-project/src/control/turret/ammo_booster_system.hpp
@@ -0,0 +1,34 @@
+#ifndef FANG_ROBOTICS_MCB_CONTROL_AMMO_BOOSTER_HPP
+#define FANG_ROBOTICS_MCB_CONTROL_AMMO_BOOSTER_HPP
+#include "unitaliases.hpp"
+#include "flywheel_system.hpp"
+#include "drivers.hpp"
+
+namespace control::turret
+{
+    /**
+     * Manages two flywheels. Update must been called periodically.
+     * Assumes no motors are inverted.
+     */
+    class AmmoBoosterSystem
+    {
+    public:
+    struct Config
+    {
+        MetersPerSecond ammoVelocity; 
+        FlywheelSystem::Config leftFlywheelConfig;
+        FlywheelSystem::Config rightFlywheelConfig;
+    };
+        AmmoBoosterSystem(Drivers& drivers, const Config& config);
+        void autoFireOn();
+        void autoFireOff();
+
+        void setAmmoVelocity(const MetersPerSecond& velocity);
+        void update();
+    private:
+        FlywheelSystem m_leftFlywheel;
+        FlywheelSystem m_rightFlywheel;
+
+    };
+}
+#endif
