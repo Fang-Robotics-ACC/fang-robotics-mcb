05df6bef5435f91d30697779f706c1827b6500fb
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-05 18:40:45 -0500

    feat(AmmoBoosterSystem): Implement constructor and ammo velocity setting

---
+++ fang-mcb-project/src/control/turret/ammo_booster_system.cpp
@@ -0,0 +1,19 @@
+#include "ammo_booster_system.hpp"
+#include "drivers.hpp"
+
+namespace control::turret
+{
+    AmmoBoosterSystem::AmmoBoosterSystem(Drivers& drivers, const Config& config)
+    :  m_leftFlywheel{drivers, config.leftFlywheelConfig}, m_rightFlywheel{drivers, config.rightFlywheelConfig}
+    {
+        setAmmoVelocity(config.ammoVelocity);
+    }
+
+
+    void AmmoBoosterSystem::setAmmoVelocity(const MetersPerSecond& velocity)
+    {
+        m_leftFlywheel.setTargetRimSpeed(velocity);
+        m_rightFlywheel.setTargetRimSpeed(-velocity);
+    }
+
+}
