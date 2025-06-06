792c9cfcaaeec8c27d61c5ed38813271a86c9fe1
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-06 18:25:49 -0500

    feat(FeederSystem): Initialize feedRate convertion, constructor, and update

---
+++ fang-mcb-project/src/control/turret/feeder_system.cpp
@@ -0,0 +1,45 @@
+#include "feeder_system.hpp"
+#include "unitaliases.hpp"
+
+
+using namespace units::literals;
+namespace control
+{
+    namespace turret
+    {
+        using namespace units::literals;
+
+        FeederSystem::FeederSystem(Drivers& drivers, const Config& config)
+        :   mk_roundsPerRevolution{config.roundsPerRevolution}, m_motor{drivers, config.motorConfig}
+        {
+        }
+
+        void FeederSystem::setFeedRate(const Hertz& feedRate)
+        {
+            m_feedRate = feedRate;
+        }
+
+        void FeederSystem::feedOn()
+        {
+
+        }
+
+        void FeederSystem::update()
+        {
+            m_motor.update();
+        }
+
+        RPM FeederSystem::feedRateToRPM()
+        {
+            //If there are two rounds per revolution,
+            //The wheel only needs to rotate half as fast
+            //to meet the specified feedRate
+            const Hertz revolutionFrequency{m_feedRate / mk_roundsPerRevolution};
+            //Slightly risky use to convert to preoper unit. It is not too risky since
+            //We are simply converting from the frequency into how often revolutions happen.
+            //This gets implicitly converted into RPM :)
+            const units::angular_velocity::revolutions_per_second_t revolutionsPerSecond {revolutionFrequency.to<double>()};
+            return revolutionsPerSecond;
+        }
+    }
+}
