ad1928dc3810ca9f1a13eb9122c152826c698eb0
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-07 23:03:00 -0500

    test(FeederSystem): Add non-zero tests

--- fang-mcb-project/test/control/turret/feeder_system_test.cpp
+++ fang-mcb-project/test/control/turret/feeder_system_test.cpp
@@ -51,3 +51,19 @@ TEST_P(FeederSystemTest, basicTest)
 }
 
 INSTANTIATE_TEST_SUITE_P(zeroTest, FeederSystemTest, testing::Values(std::make_tuple(1, 0_Hz, 0_rpm)));
+//If the wheel does one shot per revolution, and we need one shot per second, then it must done 1 revolution per second
+INSTANTIATE_TEST_SUITE_P(oneTest, FeederSystemTest, testing::Values(std::make_tuple(1, 1_Hz, 1_rps)));
+
+//If the wheel does 11 shots per revolution, and we need one shot per second, then it must done 11 revolutions per second
+INSTANTIATE_TEST_SUITE_P(multiShotRevolutionTest, FeederSystemTest, testing::Values(std::make_tuple(1, 1_Hz, 1_rps)));
+
+//If the wheel does 1 shot per revolution, and we need 120 shot per second, then it must done 120 revolutions per second
+INSTANTIATE_TEST_SUITE_P(largeFPSTest, FeederSystemTest, testing::Values(std::make_tuple(1, 11_Hz, 11_rps)));
+
+//Mixed test based on the previous principles
+INSTANTIATE_TEST_SUITE_P(mixedTest, FeederSystemTest,
+    testing::Values(std::make_tuple(10, 10_Hz, 1_rps),
+                    std::make_tuple(100, 10_Hz, 0.1_rps),
+                    std::make_tuple(123, 1_Hz, (1.0_rps/123.0)),
+                    std::make_tuple(100, 412_Hz, 4.12_rps),
+                    std::make_tuple(500, 10_Hz, (10.0_rps/500.0))));
