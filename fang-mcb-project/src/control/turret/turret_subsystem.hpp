ab91879b021bcb18dd1eb1394289c42a14390d60
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-09 00:03:58 -0500

    feat(TurretSubsystem): Add private systems

--- fang-mcb-project/src/control/turret/turret_subsystem.hpp
+++ fang-mcb-project/src/control/turret/turret_subsystem.hpp
@@ -33,6 +33,10 @@ namespace control
 
             void initialize() override;
             void refresh() override;
+        private:
+            AmmoBoosterSystem& m_ammoBooster;
+            FeederSystem& m_feeder;
+            GimbalSystem& m_gimbal;
         };
     }
 }
