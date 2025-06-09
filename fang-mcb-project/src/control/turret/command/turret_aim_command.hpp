1023121988315a142b7cf10e6ea600e49aee5a5f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-09 15:50:53 -0500

    feat(TurretAimCommand): Initialize api

---
+++ fang-mcb-project/src/control/turret/command/turret_aim_command.hpp
@@ -0,0 +1,30 @@
+#ifndef FANG_ROBOTICS_MCB_TURRET_AIM_COMMAND_HPP
+#define FANG_ROBOTICS_MCB_TURRET_AIM_COMMAND_HPP
+#include "control/turret/turret_subsystem.hpp"
+#include "control/turret/turret_input_handler.hpp"
+
+#include "tap/control/command.hpp"
+
+namespace control
+{
+    namespace turret
+    {
+        /**
+         * Aims the turret
+         */
+        class TurretAimCommand : public tap::control::Command
+        {
+        public:
+            const char* getName() const override;
+            void initialize() override;
+            void execute() override;
+            void end(bool interrupted) override;
+            bool isFinished() const;
+        private:
+            void aimPitch();
+            void aimYaw();
+            static constexpr char* mk_name{"Turret Aim Command"};
+        };
+    }
+}
+#endif
