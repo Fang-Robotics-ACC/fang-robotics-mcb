53f95e2d1cdfb9251293bfff63f810b492672b5d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-08 23:19:08 -0500

    test(GimbalSystem): Move test archetypes to separate header

--- fang-mcb-project/test/control/turret/gimbal_system_test.cpp
+++ fang-mcb-project/test/control/turret/gimbal_system_test.cpp
@@ -1,3 +1,4 @@
+#include "gimbal_system_test_archetypes.hpp"
 #include "control/turret/gimbal_system.hpp"
 
 #include "unitaliases.hpp"
@@ -7,57 +8,7 @@
 #include "test/unit_matcher.h"
 
 using namespace units::literals;
-namespace test
-{
-    /**
-     * This class is to simply provide private member access to multiple test classes
-     * So that we can have variants
-     */
-    class GimbalSystemTest
-    {
-    public:
-        using GimbalSystem = control::turret::GimbalSystem;
-        using PositionMotor = GimbalSystem::PositionMotor;
 
-        GimbalSystemTest(const GimbalSystem::Config& config) : gimbalSystem{drivers, config} {}
-        Drivers drivers;
-    public:
-        GimbalSystem gimbalSystem;
-        PositionMotor& pitchMotor{gimbalSystem.m_pitchMotor};
-        PositionMotor& yawMotor{gimbalSystem.m_yawMotor};
-
-    static constexpr GimbalSystem::PositionMotor::DjiSpeedPid::Config k_defaultPidConfig 
-    {
-        1,
-        1,
-        1
-    };
-    static constexpr GimbalSystem::PositionMotor::Config k_defaultMotorConfig 
-    {
-        tap::motor::MOTOR1,
-        tap::can::CanBus::CAN_BUS1,
-        "test motor",
-        false,
-        1.0,
-        k_defaultPidConfig 
-    };
-    };
-
-    class GimbalPitchTest: public ::testing::TestWithParam<std::tuple<Radians, Radians, Radians, Radians>>
-    {
-    public:
-        const Radians pitchAngle{std::get<0>(GetParam())};
-        const Radians expectedPitchCall{std::get<1>(GetParam())};
-        Radians minPitch{std::get<2>(GetParam())};
-        Radians maxPitch{std::get<3>(GetParam())};
-    };
-
-    class GimbalYawTest: public ::testing::TestWithParam<std::tuple<Radians>>
-    {
-    public:
-        const Radians yawAngle{std::get<0>(GetParam())};
-    };
-}
 
 
 using namespace test;
