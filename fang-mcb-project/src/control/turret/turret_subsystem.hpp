0e1f6e5a98989ef6c7a97476d75526035921f0e6
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-08 23:57:23 -0500

    feat(TurretSubsystem): Initialize

--- fang-mcb-project/src/control/turret/turret_subsystem.hpp
+++ fang-mcb-project/src/control/turret/turret_subsystem.hpp
@@ -1,13 +1,29 @@
 #ifndef FANG_ROBOTICS_MCB_TURRET_SUBSYSTEM_HPP
 #define FANG_ROBOTICS_MCB_TURRET_SUBSYSTEM_HPP
 #include "tap/control/subsystem.hpp"
+#include "control/turret/ammo_booster_system.hpp"
+#include "control/turret/feeder_system.hpp"
+#include "control/turret/gimbal_system.hpp"
+
 namespace control
 {
     namespace turret
     {
-        class TurretSubsystem
+        class TurretSubsystem : public tap::control::Subsystem
         {
+        public:
+            void flywheelsOn();
+            void flywheelsOff();
+            void autoFireOn();
+            void autoFireOff();
+            void setPitch();
+            /**
+             * This is relative to the field.
+             */
+            void setFieldYaw();
 
+            void initialize() override;
+            void refresh() override;
         };
     }
 }
