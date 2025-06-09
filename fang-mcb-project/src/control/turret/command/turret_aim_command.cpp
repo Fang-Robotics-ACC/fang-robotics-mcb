8c5c6dcd64e155f85027b9f8f325bb2ac70a3595
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-09 16:05:59 -0500

    feat(TurretAimCommand): Implement taproot command inherited functions

--- fang-mcb-project/src/control/turret/command/turret_aim_command.cpp
+++ fang-mcb-project/src/control/turret/command/turret_aim_command.cpp
@@ -8,5 +8,28 @@ namespace control
         :   m_turret{turret}, m_input{input}
         {
         }
+
+        const char* TurretAimCommand::getName() const
+        {
+            return mk_name;
+        }
+
+        void TurretAimCommand::initialize()
+        {
+        }
+
+        void TurretAimCommand::execute()
+        {
+        }
+
+        void TurretAimCommand::end(bool interrupted)
+        {
+        }
+
+        bool TurretAimCommand::isFinished() const
+        {
+            //This is meant to be a default command
+            return false;
+        }
     }
 }
