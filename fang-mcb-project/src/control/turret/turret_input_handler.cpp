f9a46efabfc3a1c44585240385ace6fb8c94d661
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-11 15:40:35 -0500

    feat(TurretInputHandler): Initialize

---
+++ fang-mcb-project/src/control/turret/turret_input_handler.cpp
@@ -0,0 +1,17 @@
+#include "control/turret/turret_input_handler.hpp"
+
+namespace control
+{
+    namespace turret
+    {
+        TurretInputHandler::TurretInputHandler(Remote& remote)
+        :   m_remote{remote}
+        {
+        }
+
+        double TurretInputHandler::getPitch() const
+        {
+            return m_remote.getChannel(Remote::Channel::RIGHT_VERTICAL);
+        }
+    }
+}
---
+++ fang-mcb-project/src/control/turret/turret_input_handler.hpp
@@ -0,0 +1,21 @@
+#ifndef FANG_ROBOTICS_MCB_TURRET_INPUT_HANDLER_HPP
+#define FANG_ROBOTICS_MCB_TURRET_INPUT_HANDLER_HPP
+#include "tap/communication/serial/remote.hpp"
+namespace control
+{
+    namespace turret
+    {
+        class TurretInputHandler
+        {
+        public:
+            using Remote = tap::communication::serial::Remote;
+
+            TurretInputHandler(Remote& remote);
+
+            double getPitch() const;
+        private:
+            Remote& m_remote;
+        };
+    }
+}
+#endif
