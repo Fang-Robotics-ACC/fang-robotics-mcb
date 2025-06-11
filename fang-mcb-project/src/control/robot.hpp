717d78f962439016c3bb045456e9926db47d1271
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-11 15:03:26 -0500

    fix(Robot): Update Pierce include

--- fang-mcb-project/src/control/robot.hpp
+++ fang-mcb-project/src/control/robot.hpp
@@ -2,7 +2,7 @@
 #define FANG_ROBOTICS_MCB_ROBOT_HPP
 #include "robot_type.hpp"
 #if defined(TARGET_INFANTRY_PIERCE)
-#include "pierce.hpp"
+#include "control/robots/pierce/pierce.hpp"
 using Robot = control::Pierce;
 #endif
 #endif
