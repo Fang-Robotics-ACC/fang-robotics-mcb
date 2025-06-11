42b84be0e626e1e8dd950bda2a68dbc39f610917
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-11 16:43:35 -0500

    feat(Pierce): Add turret motion configuration file

---
+++ fang-mcb-project/src/control/robots/pierce/config/turret_motion_config.hpp
@@ -0,0 +1,14 @@
+#ifndef FANG_ROBOTICS_MCB_TURRET_MOTION_CONFIG_HPP
+#define FANG_ROBOTICS_MCB_TURRET_MOTION_CONFIG_HPP
+#include "configuration/motion/turret_motion_config.hpp"
+#include "unitaliases.hpp"
+namespace control
+{
+    using namespace units::literals;
+    static const config::motion::TurretMotionConfig k_turretMotionConfig
+    {
+        10_rpm,
+        10_rpm
+    };
+}
+#endif
