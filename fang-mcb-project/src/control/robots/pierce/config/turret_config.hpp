8970ded295da6d74cf62bc53ad291bcfac4ab690
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-11 18:25:50 -0500

    clean(Pierce): Add ammo booster config and move gimbal configuration constants to separate files

--- fang-mcb-project/src/control/robots/pierce/config/turret_config.hpp
+++ fang-mcb-project/src/control/robots/pierce/config/turret_config.hpp
@@ -2,60 +2,17 @@
 #define FANG_ROBOTICS_MCB_PIERCE_TURRET_CONFIG_HPP
 #include "control/turret/gimbal_system.hpp"
 #include "control/turret/turret_subsystem.hpp"
+#include "control/robots/pierce/config/ammo_booster_config.hpp"
+#include "control/robots/pierce/config/turret_gimbal_config.hpp"
 #include "unitaliases.hpp"
 namespace control
 {
-    using namespace units::literals;
-    static const trap::motor::DjiSpeedPid::Config k_pitchPidConfig
-    {
-        70000,
-        1000,
-        200,
-        1000,
-        trap::motor::DjiGM6020::k_maxOutput
-    };
-    static const trap::motor::DjiGM6020::Config k_pitchMotorConfig
-    {
-        tap::motor::MOTOR2,
-        tap::can::CanBus::CAN_BUS1,
-        "pitchMotor",
-        false,
-        1.0,
-        k_pitchPidConfig,
-        false
-    };
 
-    static const trap::motor::DjiSpeedPid::Config k_yawPidConfig
-    {
-        50000,
-        1000,
-        0,
-        100,
-        trap::motor::DjiGM6020::k_maxOutput
-    };
-    static const trap::motor::DjiGM6020::Config k_yawConfig
-    {
-        tap::motor::MOTOR1,
-        tap::can::CanBus::CAN_BUS1,
-        "yawMotor",
-        true,
-        1.0,
-        k_yawPidConfig,
-        false
-    };
-
-    using GimbalSystem = control::turret::GimbalSystem;
-    static const GimbalSystem::Config k_gimbalConfig
-    {
-        -25_deg,
-        30_deg,
-        k_pitchMotorConfig,
-        k_yawConfig
-    };
 
     static const control::turret::TurretSubsystem::Config k_turretConfig
     {
-        k_gimbalConfig
+        k_gimbalConfig,
+        k_ammoBoosterConfig
     };
 
 
---
+++ fang-mcb-project/src/control/robots/pierce/config/turret_gimbal_config.hpp
@@ -0,0 +1,55 @@
+#ifndef FANG_ROBOTICS_MCB_PIERCE_TURRET_GIMBAL_CONFIG_HPP
+#define FANG_ROBOTICS_MCB_PIERCE_TURRET_GIMBAL_CONFIG_HPP
+#include "control/turret/gimbal_system.hpp"
+
+namespace control
+{
+    using namespace units::literals;
+    static const trap::motor::DjiSpeedPid::Config k_pitchPidConfig
+    {
+        70000,
+        1000,
+        200,
+        1000,
+        trap::motor::DjiGM6020::k_maxOutput
+    };
+    static const trap::motor::DjiGM6020::Config k_pitchMotorConfig
+    {
+        tap::motor::MOTOR2,
+        tap::can::CanBus::CAN_BUS1,
+        "pitchMotor",
+        false,
+        1.0,
+        k_pitchPidConfig,
+        false
+    };
+
+    static const trap::motor::DjiSpeedPid::Config k_yawPidConfig
+    {
+        50000,
+        1000,
+        0,
+        100,
+        trap::motor::DjiGM6020::k_maxOutput
+    };
+    static const trap::motor::DjiGM6020::Config k_yawConfig
+    {
+        tap::motor::MOTOR1,
+        tap::can::CanBus::CAN_BUS1,
+        "yawMotor",
+        true,
+        1.0,
+        k_yawPidConfig,
+        false
+    };
+
+    using GimbalSystem = control::turret::GimbalSystem;
+    static const GimbalSystem::Config k_gimbalConfig
+    {
+        -25_deg,
+        30_deg,
+        k_pitchMotorConfig,
+        k_yawConfig
+    };
+}
+#endif
