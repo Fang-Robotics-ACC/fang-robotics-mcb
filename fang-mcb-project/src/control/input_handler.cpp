31121da11d84277d7e627c914cd1065f3052b8aa
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-11 15:56:25 -0500

    feat(InputHandler): Add turret input handler

--- fang-mcb-project/src/control/input_handler.cpp
+++ fang-mcb-project/src/control/input_handler.cpp
@@ -2,12 +2,17 @@
 namespace control
 {
     InputHandler::InputHandler(tap::communication::serial::Remote& remote):
-        m_remote{remote}, m_chassisInputHandler{remote} 
+        m_remote{remote}, m_chassisInputHandler{remote} , m_turretInputHandler{remote}
     {
     }
 
     control::chassis::ChassisInputHandler& InputHandler::getChassisInputs()
     {
         return m_chassisInputHandler;
+    }
+
+    control::turret::TurretInputHandler& InputHandler::getTurretInputs()
+    {
+        return m_turretInputHandler;
     }
 }
--- fang-mcb-project/src/control/input_handler.hpp
+++ fang-mcb-project/src/control/input_handler.hpp
@@ -4,6 +4,7 @@
 #include "configuration/chassislogicaliases.hpp"
 
 #include "control/chassis/chassis_input_handler.hpp"
+#include "control/turret/turret_input_handler.hpp"
 
 #include "tap/communication/serial/remote.hpp"
 
@@ -15,10 +16,12 @@ namespace control
     InputHandler(tap::communication::serial::Remote& remote);
 
     control::chassis::ChassisInputHandler& getChassisInputs();
+    control::turret::TurretInputHandler& getTurretInputs();
 
     private:
     tap::communication::serial::Remote& m_remote;
     control::chassis::ChassisInputHandler m_chassisInputHandler;
+    turret::TurretInputHandler m_turretInputHandler;
     };
 }
 #endif
