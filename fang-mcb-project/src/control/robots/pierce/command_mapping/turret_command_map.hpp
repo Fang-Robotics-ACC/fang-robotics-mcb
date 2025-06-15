915248270dcf27f7ff5922ffc6be0aad010280f4
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-15 18:49:32 -0500

    clean(Pierce): Use class alias

--- fang-mcb-project/src/control/robots/pierce/command_mapping/turret_command_map.hpp
+++ fang-mcb-project/src/control/robots/pierce/command_mapping/turret_command_map.hpp
@@ -3,10 +3,17 @@
 #include "tap/control/remote_map_state.hpp"
 namespace control
 {
+    using Remote = tap::communication::serial::Remote;
     static const tap::control::RemoteMapState k_activateBoosterRemoteState
     {
-        tap::communication::serial::Remote::Switch::LEFT_SWITCH,
-        tap::communication::serial::Remote::SwitchState::UP
+        Remote::Switch::LEFT_SWITCH,
+        Remote::SwitchState::UP
+    };
+
+    static const tap::control::RemoteMapState k_activateBoosterRemoteState
+    {
+        Remote::Switch::LEFT_SWITCH,
+        Remote::SwitchState::UP
     };
 
 }
