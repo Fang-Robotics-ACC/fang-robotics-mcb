ca4bd84f5c5a25118a1aa3e83a182229e9f0c0d3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-15 20:26:21 -0500

    fix(MathAliases): Make abstract vector capable of being a constexpr

--- fang-mcb-project/src/configuration/mathaliases.hpp
+++ fang-mcb-project/src/configuration/mathaliases.hpp
@@ -1,9 +1,10 @@
 #ifndef FANG_ROBOTICS_MCB_MATH_ALIASES_HPP
 #define FANG_ROBOTICS_MCB_MATH_ALIASES_HPP
 #include "modm/math/geometry/vector.hpp"
+#include "data/vector2d.hpp"
 
 namespace math
 {
-    using AbstractVector2D = modm::Vector<double, 2>;
+    using AbstractVector2D = data::math::Vector2D<double>;
 }
 #endif
