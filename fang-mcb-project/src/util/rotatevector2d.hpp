6250b454a14e65bc29f2db9cd566e3e1952498f5
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-16 01:31:57 -0500

    feat(RotateVector2D): Make it generic

--- fang-mcb-project/src/util/rotatevector2d.hpp
+++ fang-mcb-project/src/util/rotatevector2d.hpp
@@ -2,7 +2,7 @@
 #define FANG_ROBOTICS_MCB_UTIL_ROTATE_VECTOR_2D
 #include "unitaliases.hpp"
 #include "mathaliases.hpp"
-#include "modm/math/geometry/vector2.hpp"
+#include "data/vector2d.hpp"
 
 namespace util
 {
@@ -12,7 +12,21 @@ namespace util
          * This algorithm has an accuracy of within 1e-14; however, rotating the vector back will produce accuracy to only within
          * 1e-10
          */
-        ::math::AbstractVector2D rotateVector2D(const ::math::AbstractVector2D& vector, const Radians& rotationAngle);
+        template<typename Unit>
+        data::math::Vector2D<Unit> rotateVector2D(const data::math::Vector2D<Unit>& vector, const Radians& rotationAngle)
+        {
+            //Rotation matrix
+            //https://www.youtube.com/watch?v=1oYEo7PNIBQ
+            //I know it's Wikipedia but it's will help wwith the youtube video
+            //In general, Wikipedia is pretty good as a math reference, too.
+            //https://en.wikipedia.org/wiki/Rotation_matrix
+
+            const double angle{rotationAngle.to<double>()};
+            const double rotatedx{vector.x * (std::cos(angle)) - vector.y * (std::sin(angle))};
+            const double rotatedy{vector.x * (std::sin(angle)) + vector.y * (std::cos(angle))};
+
+            return data::math::Vector2D<Unit>{Unit{rotatedx}, Unit{rotatedy}};
+        }
     }
 }
 #endif
