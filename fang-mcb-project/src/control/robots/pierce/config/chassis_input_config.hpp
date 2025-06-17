8b6955cecde67d63665980a758003abb94118451
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-17 18:10:41 -0500

    feat(Pierce): Add chassis input configuration file

---
+++ fang-mcb-project/src/control/robots/pierce/config/chassis_input_config.hpp
@@ -0,0 +1,32 @@
+#ifndef FANG_ROBOTICS_MCB_PIERCE_CHASSIS_INPUT_CONFIG_HPP
+#define FANG_ROBOTICS_MCB_PIERCE_CHASSIS_INPUT_CONFIG_HPP
+#include "control/chassis/chassis_input_handler.hpp"
+namespace control
+{
+
+    using Remote = tap::communication::serial::Remote;
+    using Channel = Remote::Channel;
+    using Key = Remote::Key;
+
+    static const chassis::ChassisInputHandler::RemoteConfig k_chassisInputRemoteConfig
+    {
+        Channel::LEFT_HORIZONTAL,   //xTranslation
+        Channel::LEFT_VERTICAL,     //yTranslation
+        Channel::WHEEL              //rotation
+    };
+
+    static const chassis::ChassisInputHandler::KeyboardConfig k_chassisInputKeyboardConfig
+    {
+        Key::W, //forward
+        Key::S, //backward
+        Key::A, //left
+        Key::D  //right
+    };
+
+    static const chassis::ChassisInputHandler::Config k_chassisInputConfig
+    {
+        k_chassisInputRemoteConfig,
+        k_chassisInputKeyboardConfig
+    };
+}
+#endif
