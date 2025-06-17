ac566612cf44adfb524b687a9b234d642eefe754
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-17 18:32:44 -0500

    clean(Pierce): Remove deprecated files

--- fang-mcb-project/src/control/robots/pierce/pierce_config.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef FANG_ROBOTICS_MCB_PIERCE_CONFIG_HPP
-#define FANG_ROBOTICS_MCB_PIERCE_CONFIG_HPP
-#include "pierce.hpp"
-namespace control
-{
-    extern const Pierce::Config k_config;
-}
-#endif
