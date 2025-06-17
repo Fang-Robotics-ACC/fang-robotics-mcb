625704877245815ac8c35b35922f3ca3618fe9e3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-17 13:44:26 -0500

    fix(ShurikenCommand): Remove header guard collision

--- fang-mcb-project/src/control/chassis/shuriken_command.hpp
+++ fang-mcb-project/src/control/chassis/shuriken_command.hpp
@@ -1,5 +1,5 @@
-#ifndef FANG_ROBOTICS_MCB_FIELD_MECANUM_COMMAND_HPP
-#define FANG_ROBOTICS_MCB_FIELD_MECANUM_COMMAND_HPP
+#ifndef FANG_ROBOTICS_MCB_SHURIKEN_COMMAND_HPP
+#define FANG_ROBOTICS_MCB_SHURIKEN_COMMAND_HPP
 #include "control/input_handler.hpp"
 #include "control/chassis/chassis_subsystem.hpp"
 #include "control/turret/turret_subsystem.hpp"
