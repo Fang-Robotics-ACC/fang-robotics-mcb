2f7c4bf7b8112dfeacaf2a37df786ebcb940ec1a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-17 16:18:44 -0500

    clean!(InputHandler): Remove chassis input handler

--- fang-mcb-project/src/control/input_handler.cpp
+++ fang-mcb-project/src/control/input_handler.cpp
@@ -2,13 +2,8 @@
 namespace control
 {
     InputHandler::InputHandler(tap::communication::serial::Remote& remote):
-        m_remote{remote}, m_chassisInputHandler{remote} , m_turretInputHandler{remote}
+        m_remote{remote}, m_turretInputHandler{remote}
     {
-    }
-
-    control::chassis::ChassisInputHandler& InputHandler::getChassisInputs()
-    {
-        return m_chassisInputHandler;
     }
 
     control::turret::TurretInputHandler& InputHandler::getTurretInputs()
