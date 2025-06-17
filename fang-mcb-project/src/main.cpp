b0a7c141ea3f40d2463858e9dbca5cdaa2601d38
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-17 15:42:07 -0500

    refactor(main): Move configuration variable to io file

--- fang-mcb-project/src/main.cpp
+++ fang-mcb-project/src/main.cpp
@@ -51,7 +51,6 @@
 #include <iostream>
 
 /* define timers here -------------------------------------------------------*/
-static constexpr float MAIN_LOOP_FREQUENCY = 500.0f;
 tap::arch::PeriodicMilliTimer sendMotorTimeout(1000.0f / MAIN_LOOP_FREQUENCY);
 
 
@@ -97,32 +96,4 @@ int main()
         modm::delay_us(10);
     }
     return 0;
-}
-
-void initializeIo(Drivers& drivers)
-{
-    drivers.analog.init();
-    drivers.pwm.init();
-    drivers.digital.init();
-    drivers.leds.init();
-    drivers.can.initialize();
-    drivers.errorController.init();
-    drivers.remote.initialize();
-    drivers.bmi088.initialize(MAIN_LOOP_FREQUENCY, 0.1, 0);
-    drivers.refSerial.initialize();
-    drivers.terminalSerial.initialize();
-    drivers.schedulerTerminalHandler.init();
-    drivers.djiMotorTerminalSerialHandler.init();
-}
-
-void updateIo(Drivers& drivers)
-{
-#ifdef PLATFORM_HOSTED
-    tap::motor::motorsim::DjiMotorSimHandler::getInstance()->updateSims();
-#endif
-
-    drivers.canRxHandler.pollCanData();
-    drivers.refSerial.updateSerial();
-    drivers.remote.read();
-    drivers.bmi088.read();
 }
