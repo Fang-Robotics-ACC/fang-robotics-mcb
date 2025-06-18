d7f2659322475e9343618487ca9fea1234bd20f8
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-18 13:46:50 -0500

    clean(FeederSubsystem): Remove unncessary virtual specifier and docs

--- fang-mcb-project/src/control/turret/feeder_subsystem.hpp
+++ fang-mcb-project/src/control/turret/feeder_subsystem.hpp
@@ -9,22 +9,13 @@ namespace control::turret
         class FeederSubsystem: public virtual tap::control::Subsystem
         {
         public:
-
-            /**
-             * yawHomeOffset - how many degrees the zero yaw of the yaw motor fr
-             * the forward direction
-             */
             using Config = FeederSystem::Config;
 
             FeederSubsystem(Drivers& drivers, const Config& config);
 
-            virtual void initialize() override;
-            virtual void refresh() override;
+            void initialize() override;
+            void refresh() override;
 
-            /**
-             * Wherever the the yaw is aiming to look fieldwise, returns
-             * a unit vector.
-             */
             void autoFireOn();
             void autoFireOff();
 
