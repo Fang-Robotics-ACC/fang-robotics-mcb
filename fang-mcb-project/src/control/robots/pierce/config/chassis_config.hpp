8e99a09613a31860ad013ea46d18649de312ce7f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-17 21:53:12 -0500

    clean(Pierce): Add chassis config

---
+++ fang-mcb-project/src/control/robots/pierce/config/chassis_config.hpp
@@ -0,0 +1,49 @@
+#ifndef FANG_ROBOTICS_MCB_CHASSIS_CONFIG_PIERCE_HPP
+#define FANG_ROBOTICS_MCB_CHASSIS_CONFIG_PIERCE_HPP
+#include "control/chassis/chassis_subsystem.hpp"
+#include "unitaliases.hpp"
+
+namespace control
+{
+    static const Hertz k_chassisPwmFreq{400};
+
+    using namespace units::literals;
+        
+    static const chassis::ChassisSubsystem::ChassisDimensionConfig k_defaultDimensionConfig
+    {
+        150_mm, //Wheel Radius
+        15_in,  //Vertical Wheel Distance
+        13.5_in   //Horizontal Wheel Distance
+    };
+
+    static const chassis::ChassisSubsystem::DriveMotor::UnifiedProperties k_unifiedMotorProperties
+    {
+        24.0_V,                                      //Controller Input Voltage
+        data::motors::Directionality::BIDIRECTIONAL, //Directionality
+        14.0                                         //GearRatio
+    };
+    static const chassis::ChassisSubsystem::ChassisMotorConfig k_defaultMotorConfig
+    {
+        k_unifiedMotorProperties,                   //Unified motor properties
+        trap::gpio::PwmData{tap::gpio::Pwm::C1, k_chassisPwmFreq},    //Front Left PwmData
+        trap::gpio::PwmData{tap::gpio::Pwm::C2, k_chassisPwmFreq},    //Front Right PwmData
+        trap::gpio::PwmData{tap::gpio::Pwm::C3, k_chassisPwmFreq},    //Rear Left PwmData
+        trap::gpio::PwmData{tap::gpio::Pwm::C4, k_chassisPwmFreq}     //Rear Right PwmData
+    };
+
+    static const chassis::ChassisSubsystem::PowerLimiter::Config k_chassisPowerLimiterConfig
+    {
+        60_J,
+        10_J,
+        5_J
+    };
+
+
+    static const chassis::ChassisSubsystem::ChassisConfig k_chassisConfig 
+    {
+        k_defaultDimensionConfig,
+        k_defaultMotorConfig,
+        k_chassisPowerLimiterConfig
+    };
+}
+#endif
