df98ad680093c86777461bf46aac464a236b2f78
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-18 16:25:48 -0500

    feat(Pierce): Add configuration variable

---
+++ fang-mcb-project/src/control/robots/pierce/pierce_config.cpp
@@ -0,0 +1,48 @@
+#include "pierce_config.hpp"
+#include "control/robots/pierce/pierce.hpp"
+
+//Subsystem configs
+#include "config/chassis_config.hpp"
+#include "config/turret_gimbal_config.hpp"
+#include "config/feeder_config.hpp"
+#include "config/ammo_booster_config.hpp"
+
+//Input configs 
+#include "config/turret_input_config.hpp"
+#include "config/chassis_input_config.hpp"
+
+//Command configs
+#include "command_config/turret_aim_config.hpp"
+#include "command_config/field_mecanum_config.hpp"
+#include "command_config/shuriken_config.hpp"
+
+namespace control
+{
+    const Pierce::SubsystemConfig k_pierceSubsytemConfig 
+    {
+        k_chassisConfig,
+        k_gimbalSubsystemConfig,
+        k_feederConfig,
+        k_ammoBoosterConfig
+    };
+
+    const Pierce::InputConfig k_pierceInputConfig
+    {
+        k_chassisInputConfig,
+        k_turretInputRemoteConfig,
+    };
+
+    const Pierce::CommandConfig k_pierceCommandConfig
+    {
+        k_turretAimConfig,
+        k_fieldMecanumConfig,
+        k_shurikenConfig
+    };
+
+    const Pierce::Config k_pierceConfig
+    {
+        k_pierceSubsytemConfig,
+        k_pierceInputConfig,
+        k_pierceCommandConfig
+    };
+}
