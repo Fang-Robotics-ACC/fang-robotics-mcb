0a4c1263becac58bd37f8f8b626497aefb13649a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-18 13:45:10 -0500

    clean(FeederSubsystem): Organize fucntion declarations

--- fang-mcb-project/src/control/turret/feeder_subsystem.hpp
+++ fang-mcb-project/src/control/turret/feeder_subsystem.hpp
@@ -1,6 +1,7 @@
 #ifndef FANG_ROBOTICS_MCB_FEEDER_SUBSYSTEM_HPP
 #define FANG_ROBOTICS_MCB_FEEDER_SUBSYSTEM_HPP
 #include "feeder_system.hpp"
+#include "drivers.hpp"
 #include "tap/control/subsystem.hpp"
 
 namespace control::turret
@@ -15,10 +16,10 @@ namespace control::turret
              */
             using Config = FeederSystem::Config;
 
+            FeederSubsystem(Drivers& drivers, const Config& config);
+
             virtual void initialize() override;
             virtual void refresh() override;
-
-            FeederSubsystem(Drivers& drivers, const Config& config);
 
             /**
              * Wherever the the yaw is aiming to look fieldwise, returns
