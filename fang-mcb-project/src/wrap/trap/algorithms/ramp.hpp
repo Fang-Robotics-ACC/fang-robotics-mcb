4b4118e516ed2430ad63fe4ca0c77d9eae0fa6c8
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-20 19:06:54 -0500

    feat(Ramp): Initialize

---
+++ fang-mcb-project/src/wrap/trap/algorithms/ramp.hpp
@@ -0,0 +1,65 @@
+#ifndef FANG_ROBOTICS_MCB_TRAP_ALGORITHMS_RAMP_HPP
+#define FANG_ROBOTICS_MCB_TRAP_ALGORITHMS_RAMP_HPP
+#include "util/chrono/simple_timer.hpp"
+
+#include "tap/algorithms/ramp.hpp"
+namespace trap::algorithms
+{
+    /**
+     * Dimensional wrap for taproot ramp
+     */
+    template <typename ValueUnit, typename TimeUnit>
+    class Ramp
+    {
+        /**
+         * Speed should be positive
+         */
+        Ramp(ValueUnit initialValue, double rampSpeed)
+        :   m_ramp(static_cast<float>(initialValue))
+        {
+        }
+
+        void setTarget(ValueUnit target)
+        {
+            m_ramp.setTarget(static_cast<float>(target));
+        }
+
+        /**
+         * How many units per time unit the ramp moves towards the target value
+         * 
+         * This should be positive`
+         * 
+         * This is a float to allow ramp to use primitive types
+         */
+        void setSpeed(double speed)
+        {
+            m_speed = speed;
+        }
+
+        void update()
+        {
+            const TimeUnit deltaTime{static_cast<TimeUnit>(m_updateTimer.getDurationAndReset())};
+            const double rawDeltaTime{static_cast<double>(deltaTime)};
+            const double increment{m_speed * rawDeltaTime};
+
+            m_ramp.update(increment);
+        }
+
+        ValueUnit getValue() const
+        {
+            return ValueUnit{m_ramp.getValue()};
+        }
+
+        bool isTargetReached() const
+        {
+            return m_ramp.isTargetReached();
+        }
+
+    private:
+        tap::algorithms::Ramp m_ramp;
+        double m_speed;
+        chrono::SimpleTimer m_updateTimer{};
+
+    };
+}
+#endif
