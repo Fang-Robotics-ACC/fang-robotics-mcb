1c10fdf33743765eb16a9be0c520e18e99f0696c
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-22 05:12:15 -0500

    feat(HeatLimiter): Implement getting of heat limits

--- fang-mcb-project/src/control/turret/util/heat_limiter.cpp
+++ fang-mcb-project/src/control/turret/util/heat_limiter.cpp
@@ -8,4 +8,19 @@ namespace control::turret
     {
     }
 
+    HeatLimiter::HeatType HeatLimiter::getTurretHeat() const
+    {
+        const Referee::Rx::TurretData turretData{m_referee.getRobotData().turret};
+        switch(m_turretId)
+        {
+            case TurretId::TURRET_17MM_1:
+                return turretData.heat17ID1;
+                break;
+            case TurretId::TURRET_17MM_2:
+                return turretData.heat17ID2;
+            case TurretId::TURRET_42MM:
+                return turretData.heat42;
+        }
+    }
+
 }
