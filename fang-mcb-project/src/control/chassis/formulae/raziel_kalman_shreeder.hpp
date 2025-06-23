9d952b12c46ea82b033984df1036b0d3da24f2dc
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-23 13:40:51 -0700

    feat(RazielKalmanShredder): Initialize

---
+++ fang-mcb-project/src/control/chassis/formulae/raziel_kalman_shreeder.hpp
@@ -0,0 +1,20 @@
+#ifndef FANG_ROBOTICS_MCB_CHASSIS_FORMULAE_RAZIEL_KALMAN_SHREDDER_HPP
+#define FANG_ROBOTICS_MCB_CHASSIS_FORMULAE_RAZIEL_KALMAN_SHREDDER_HPP
+namespace control::chassis
+{
+    /**
+     * Raven Asher Raziel's self-named anti CV algorithm!
+     * Hopefully this is not relevant to military applications :D
+     * 
+     * This returns a scaling coefficient to be used in relation to rotation or even translation
+     * 
+     * https://www.desmos.com/calculator/e19d96ky1e 
+     * input - the control value, could be time phase, or anything, really 
+     * biteDepth - how much the shredder decreases the speed in a "bite"
+     * biteForce - how steep the "bite" is
+     * period - how often a bite happens
+     * phase, the underlying function involves sine in radians
+     */
+    double razielKalmanShredder(double input, double biteDepth, double biteForce,  double period, double phase);
+}
+#endif
