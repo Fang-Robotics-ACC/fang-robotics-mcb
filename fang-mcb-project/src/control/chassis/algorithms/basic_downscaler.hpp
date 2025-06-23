a9c5e12c3c8ab45c9eaacae922eda564c3d6a7b1
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-23 11:40:10 -0700

    feat(BasicDownscaler): Initialize

---
+++ fang-mcb-project/src/control/chassis/algorithms/basic_downscaler.hpp
@@ -0,0 +1,25 @@
+#ifndef FANG_ROBOTICS_MCB_CHASSIS_ALGORITHMS_BASIC_DOWNSCALERS_HPP
+#define FANG_ROBOTICS_MCB_CHASSIS_ALGORITHMS_BASIC_DOWNSCALERS_HPP
+#include "unitaliases.hpp"
+namespace control::chassis
+{
+    /**
+     * Small downscaler for finding an amount to downscale for the shuriken
+     * mode in order to conserve power
+     */
+    class BasicDownscaler
+    {
+    public:
+        /**
+         * The high the downscale coefficient, the less downscaling it will do
+         * with respect to speed
+         * https://www.desmos.com/calculator/5xqaxqilmnj
+         */
+        BasicDownscaler(double downscaleCoefficient);
+        double getDownscale (const MetersPerSecond& speed);
+
+    private:
+        const double m_downscaleCoefficient;
+    };
+}
+#endif
