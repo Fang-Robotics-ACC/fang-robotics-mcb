77f6ba489398bb23ae22799dfee6a6830f17ac74
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-06-28 02:14:00 -0700

    feat(music::Frequency): Initialize

---
+++ fang-mcb-project/src/music/frequency.hpp
@@ -0,0 +1,10 @@
+#ifndef FANG_ROBOTICS_MCB_MUSIC_FREQUENCY_HPP
+#define FANG_ROBOTICS_MCB_MUSIC_FREQUENCY_HPP
+#include "unitaliases.hpp"
+#include "pitch.hpp"
+
+namespace music
+{
+    constexpr Hertz pitchToFrequency(Pitch pitch);
+}
+#endif
