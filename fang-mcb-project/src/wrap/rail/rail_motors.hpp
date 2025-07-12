7f431a2835be2f51ea938ebcc278be9d1fb70e33
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 14:36:44 -0500

    feat(RailMotors): Modularize rail use

---
+++ fang-mcb-project/src/wrap/rail/rail_motors.hpp
@@ -0,0 +1,13 @@
+#ifndef FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_MOTORS_HPP
+#define FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_MOTORS_HPP
+#include "rail/motor/ispeed_motor.hpp"
+#include "rail/motor/iposition_motor.hpp"
+
+#include "unitaliases.hpp"
+
+namespace motors
+{
+    using ISpeedMotor = rail::motor::ISpeedMotor<RPM>;
+    using IPositionMotor = rail::motor::IPositionMotor<RPM>;
+}
+#endif
