667c901ac5b6a23e4546213b5afce40e3bb1f19e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 14:54:57 -0500

    test(GearboxRepeatUltraMk2): Update mock to new api

--- fang-mcb-project/test/mock/motors/gearboxrepeatultramk2mock.hpp
+++ fang-mcb-project/test/mock/motors/gearboxrepeatultramk2mock.hpp
@@ -2,7 +2,7 @@
 #define FANG_ROBOTICS_MCB_GEARBOX_REPEAT_ULTRA_MK2_MOCK_HPP
 #include "unitaliases.hpp"
 #include "directionality.hpp"
-#include "gearboxrepeatultramk2.hpp"
+#include "motor/gearboxrepeatultramk2.hpp"
 
 #include "tap/drivers.hpp"
 #include "tap/util_macros.hpp"
@@ -18,8 +18,8 @@ namespace mock
             using ::motor::GearboxRepeatUltraMk2::GearboxRepeatUltraMk2;
             virtual ~GearboxRepeatUltraMk2Mock() = default;
 
-    		MOCK_METHOD(void, setSpeed, (const RPM& speed), (override));
-    		MOCK_METHOD(RPM, getSpeed, (), (const, override));
+    		MOCK_METHOD(void, setTargetSpeed, (const RPM& speed), (override));
+    		MOCK_METHOD(RPM, getTargetSpeed, (), (const, override));
     		MOCK_METHOD(void, setMaxSpeed, (const RPM& maxSpeed), (override));
     		MOCK_METHOD(RPM, getMaxSpeed, (), (const, override));
     		MOCK_METHOD(void, setMinSpeed, (const RPM& minSpeed), (override));
