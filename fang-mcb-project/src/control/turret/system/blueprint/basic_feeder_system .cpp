b31c9c3ba9b077e7b600ddf9a92ef832364bacf6
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 18:11:12 -0500

    feat(BasicFeederSystem): Create dependency injection based feeder
    It is better to have the test be based off of dependency injection instead of trying to fix something that will be deprecated by the end of the week

---
+++ fang-mcb-project/src/control/turret/system/blueprint/basic_feeder_system .cpp
@@ -0,0 +1,64 @@
+#include "basic_feeder_system.hpp"
+
+
+#include "unitaliases.hpp"
+
+
+using namespace units::literals;
+namespace control::turret
+{
+    using namespace units::literals;
+
+    BasicFeederSystem::BasicFeederSystem(Drivers& drivers, motor::ISpeedMotor& feedMotor, const Config& config)
+    :   kRoundsPerRevolution_{config.roundsPerRevolution},
+        kFeedRate_{config.feedRate},
+        kUnjamSpeed_{config.unjamSpeed},
+        feedMotor_{feedMotor}
+    {
+        //assert(mk_roundsPerRevolution != 0 && "roundsPerRevolution must be zero");
+        //assert(mk_roundsPerRevolution  > 0 && "roundsPerRevolution cannot be less than zero");
+    }
+
+    void BasicFeederSystem::feedOn()
+    {
+        feedMotor_.setTargetSpeed(feedRateToRPM());
+    }
+
+    void BasicFeederSystem::feedOff()
+    {
+        feedMotor_.setTargetSpeed(kStillSpeed_);
+    }
+
+    void BasicFeederSystem::unjamOn()
+    {
+        feedMotor_.setTargetSpeed(-kUnjamSpeed_);
+    }
+
+    void BasicFeederSystem::unjamOff()
+    {
+        feedOff();
+    }
+
+    void BasicFeederSystem::update()
+    {
+        feedMotor_.update();
+    }
+
+    void BasicFeederSystem::initialize()
+    {
+        feedMotor_.initialize();
+    }
+
+    RPM BasicFeederSystem::feedRateToRPM()
+    {
+        //If there are two rounds per revolution,
+        //The wheel only needs to rotate half as fast
+        //to meet the specified feedRate
+        const Hertz revolutionFrequency{kFeedRate_ / kRoundsPerRevolution_};
+        //Slightly risky use to convert to preoper unit. It is not too risky since
+        //We are simply converting from the frequency into how often revolutions happen.
+        //This gets implicitly converted into RPM :)
+        const units::angular_velocity::revolutions_per_second_t revolutionsPerSecond {revolutionFrequency.to<double>()};
+        return revolutionsPerSecond;
+    }   
+}
---
+++ fang-mcb-project/src/control/turret/system/blueprint/basic_feeder_system.hpp
@@ -0,0 +1,60 @@
+#ifndef FANG_ROBOTICS_MCB_CONTROL_TURRET_SYSTEM_BLUEPRINT_BASIC_FEEDER_SYSTEM_HPP
+#define FANG_ROBOTICS_MCB_CONTROL_TURRET_SYSTEM_BLUEPRINT_BASIC_FEEDER_SYSTEM_HPP
+#include "unitaliases.hpp"
+#include "wrap/rail/rail_motors.hpp"
+
+#include "drivers.hpp"
+
+namespace control::turret
+{   
+    /**
+     * Intermediate dependency injection based class using the deprecated feeder api
+     */
+    class BasicFeederSystem
+    {
+    public:
+        /**
+         * roundsPerRevolution cannot be negative or zero which would cause corruption.
+         * feedRate - this is the starting rounds per second
+         * motorConfig - this is the configuration of the feeder motor
+         */
+        struct Config
+        {
+            int roundsPerRevolution;
+            Hertz feedRate;
+            RPM unjamSpeed;
+        };
+
+        /**
+         * A positive value sent to the feedMotor means that it will feed.
+         */
+        BasicFeederSystem(Drivers& drivers, motor::ISpeedMotor &feedMotor, const Config& config);
+
+        void feedOn();
+        void feedOff();
+
+        void unjamOn();
+        void unjamOff();
+
+        /**
+         * Check if the feed system is feeding ammo or not.
+         */
+        bool getActiveStatus();
+
+        void update();
+        void initialize();
+    private:
+        RPM feedRateToRPM();
+
+        static constexpr RPM kStillSpeed_{0.0};
+
+        const int kRoundsPerRevolution_;
+        const Hertz kFeedRate_;
+        const RPM kUnjamSpeed_;
+
+        bool activeStatus_{false};
+
+        motor::ISpeedMotor& feedMotor_;
+    };  
+}
+#endif
