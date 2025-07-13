01b202b251ccdeb0671dd5637f9942fb1ee1d5e5
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 19:14:38 -0500

    fix(BasicFeederSystem): Return to old arithmetic

--- fang-mcb-project/src/control/turret/system/blueprint/basic_feeder_system .cpp
+++ fang-mcb-project/src/control/turret/system/blueprint/basic_feeder_system .cpp
@@ -58,8 +58,7 @@ namespace control::turret
         //Slightly risky use to convert to preoper unit. It is not too risky since
         //We are simply converting from the frequency into how often revolutions happen.
         //This gets implicitly converted into RPM :)
-        //The frequency is how many revolutions per second
-        const RPM revolutionsPerMinute {revolutionFrequency.to<double>() / 60};
-        return revolutionsPerMinute;
+        const units::angular_velocity::revolutions_per_second_t revolutionsPerSecond {revolutionFrequency.to<double>()};
+        return revolutionsPerSecond;
     }   
 }
