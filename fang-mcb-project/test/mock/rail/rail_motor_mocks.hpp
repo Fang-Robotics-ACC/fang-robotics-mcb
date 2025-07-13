aa3edcd7096193691c2c4e5f3dabd6e2c42a074c
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-12 19:06:00 -0500

    test(rail): Add aliases for speed motors

---
+++ fang-mcb-project/test/mock/rail/rail_motor_mocks.hpp
@@ -0,0 +1,9 @@
+#ifndef FANG_ROBOTICS_MCB_TEST_MOCK_MOTOR_RAIL_RAIL_MOTOR_MOCKS_HPP
+#define FANG_ROBOTICS_MCB_TEST_MOCK_MOTOR_RAIL_RAIL_MOTOR_MOCKS_HPP
+#include "unitaliases.hpp"
+#include "rail/mock/motor/ispeed_motor_mock.hpp"
+namespace mock::motor
+{
+    using ISpeedMotorMock = rail::motor::ISpeedMotorMock<RPM>;
+}
+#endif
