e8118e79c18262e09f39b0687693c5d66ce9c2cf
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-14 18:22:16 -0500

    fix(rail): Allow commands to register subsystem

---
+++ fang-mcb-project/src/wrap/rail/turret/i_simple_feeder_subsystem_control.hpp
@@ -0,0 +1,15 @@
+#ifndef FANG_ROBOTICS_MCB_RAIL_TURRET_I_SIMPLE_FEEDER_SUBSYSTEM_CONTROL_HPP
+#define FANG_ROBOTICS_MCB_RAIL_TURRET_I_SIMPLE_FEEDER_SUBSYSTEM_CONTROL_HPP
+#include "tap/control/subsystem.hpp"
+#include "rail/turret/feeder/isimple_feeder_control.hpp"
+namespace rail::turret
+{
+    class ISimpleFeederSubsystemControl:
+        virtual public tap::control::Subsystem,
+        virtual public ISimpleFeederControl
+    {
+    public:
+        ~ISimpleFeederSubsystemControl() {};
+    };
+}
+#endif
