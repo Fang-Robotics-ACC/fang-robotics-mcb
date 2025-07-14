93c509ffe4c031f346dd325795c11c13a506741d
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-14 17:30:30 -0500

    feat(rail_turret_aliases): Allow uniform access to instantiated rail turret tempaltes

---
+++ fang-mcb-project/src/wrap/rail/rail_turret_aliases.hpp
@@ -0,0 +1,8 @@
+#ifndef FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_TURRET_ALIASES_HPP
+#define FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_TURRET_ALIASES_HPP
+#include "rail/turret/feeder/isimple_feeder.hpp"
+namespace control::turret
+{
+    using ISimpleFeeder = rail::turret::ISimpleFeeder;
+}
+#endif
