6b1209c32c5dad6d92d31a710b891d20b91203d2
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-16 14:23:37 -0500

    refactor!(mecanum): Simplify namespace

--- fang-mcb-project/src/control/chassis/mecanum/logic/mecanum_calculator.cpp
+++ fang-mcb-project/src/control/chassis/mecanum/logic/mecanum_calculator.cpp
@@ -1,53 +1,45 @@
 #include "mecanum_calculator.hpp"
 #include "unitaliases.hpp"
 #include <iostream>
-namespace logic
+namespace chassis
 {
-    namespace chassis
+    void MecanumCalculator::setWheelSpeeds(const QuadDriveData& quadDriveData)
     {
+        //The input is in RPM; however, the implementation needs the angular velocity in radians
+        //This is a quick fix. A more proper way to do this is to add operator overloads for the templates
+        quadDriveData_.frontLeft = quadDriveData.frontLeft;
+        quadDriveData_.frontRight = quadDriveData.frontRight;
+        quadDriveData_.rearLeft = quadDriveData.rearLeft;
+        quadDriveData_.rearRight = quadDriveData.rearRight;
 
-        void MecanumCalculator::setWheelSpeeds(const QuadDriveData& quadDriveData)
-        {
-            //The input is in RPM; however, the implementation needs the angular velocity in radians
-            //This is a quick fix. A more proper way to do this is to add operator overloads for the templates
-            quadDriveData_.frontLeft = quadDriveData.frontLeft;
-            quadDriveData_.frontRight = quadDriveData.frontRight;
-            quadDriveData_.rearLeft = quadDriveData.rearLeft;
-            quadDriveData_.rearRight = quadDriveData.rearRight;
-        }
-
-        //lx is half of the total horizontal distance between the wheels
-        //ly is half of the total vertical distance between the wheels
-        MecanumCalculator::MecanumCalculator(const Meters& horizontalWheelDistance,
-                                             const Meters& verticalWheelDistance,
-                                             const Meters& wheelRadius):
-            kwheelDistanceConstant_{(horizontalWheelDistance + verticalWheelDistance) / 2.0},
-            kWheelRadius_{wheelRadius}
-        {}
-        
-        Velocity2D MecanumCalculator::getTranslation() const
-        {
-            MetersPerSecond xVelocity{((kWheelRadius_ / 4.0)
-                                    * (quadDriveData_.frontLeft - quadDriveData_.frontRight
-                                     - quadDriveData_.rearLeft  + quadDriveData_.rearRight)).to<double>()};
-
-            MetersPerSecond yVelocity{((kWheelRadius_ / 4.0)
-                                    * (  quadDriveData_.frontLeft + quadDriveData_.frontRight
-                                       + quadDriveData_.rearLeft  + quadDriveData_.rearRight)).to<double>()};
-
-            return Velocity2D{xVelocity, yVelocity};
-        }
-
-        RPM MecanumCalculator::getRotation() const
-        {
-
-        //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
-        //Equation 26
-        const RadiansPerSecond rotationSum{- quadDriveData_.frontLeft + quadDriveData_.frontRight
-                              - quadDriveData_.rearLeft  + quadDriveData_.rearRight};
-        const auto rotationConstant{kWheelRadius_ / (4.0 * kwheelDistanceConstant_)};
-
-        return rotationConstant * rotationSum; 
-        }
     }
+    //lx is half of the total horizontal distance between the wheels
+    //ly is half of the total vertical distance between the wheels
+    MecanumCalculator::MecanumCalculator(const Meters& horizontalWheelDistance,
+                                         const Meters& verticalWheelDistance,
+                                         const Meters& wheelRadius):
+        kwheelDistanceConstant_{(horizontalWheelDistance + verticalWheelDistance) / 2.0},
+        kWheelRadius_{wheelRadius}
+    {}
+ 
+    Velocity2D MecanumCalculator::getTranslation() const
+    {
+        MetersPerSecond xVelocity{((kWheelRadius_ / 4.0)
+                                * (quadDriveData_.frontLeft - quadDriveData_.frontRight
+                                 - quadDriveData_.rearLeft  + quadDriveData_.rearRight)).to<double>()};
+        MetersPerSecond yVelocity{((kWheelRadius_ / 4.0)
+                                * (  quadDriveData_.frontLeft + quadDriveData_.frontRight
+                                   + quadDriveData_.rearLeft  + quadDriveData_.rearRight)).to<double>()};
+        return Velocity2D{xVelocity, yVelocity};
+    }
+    RPM MecanumCalculator::getRotation() const
+    {
+    //https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+    //Equation 26
+    const RadiansPerSecond rotationSum{- quadDriveData_.frontLeft + quadDriveData_.frontRight
+                          - quadDriveData_.rearLeft  + quadDriveData_.rearRight};
+    const auto rotationConstant{kWheelRadius_ / (4.0 * kwheelDistanceConstant_)};
+    return rotationConstant * rotationSum;
+    }
+}
 }
--- fang-mcb-project/src/control/chassis/mecanum/logic/mecanum_calculator.hpp
+++ fang-mcb-project/src/control/chassis/mecanum/logic/mecanum_calculator.hpp
@@ -7,41 +7,34 @@
 #include "control/chassis/mecanum/logic/abstract_mecanum_calculator.hpp"
 #include "quaddrivedata.hpp"
 
-namespace logic
+namespace chassis
 {
-    namespace chassis
+    using QuadDriveData = data::chassis::QuadDriveData<RPM>;
+    /**
+     * This uses the equations from this paper
+     * https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+     */
+    class MecanumCalculator
     {
-        using QuadDriveData = data::chassis::QuadDriveData<RPM>;
+    public:
+        MecanumCalculator(const Meters& horizontalWheelDistance,
+                          const Meters& verticalWheelDistance,
+                          const Meters& wheelRadius);
+        void setWheelSpeeds(const QuadDriveData& quadDriveData);
         /**
-         * This uses the equations from this paper
-         * https://research.ijcaonline.org/volume113/number3/pxc3901586.pdf
+         * Returns the movement of the robot relative to the robot.
          */
-
-        class MecanumCalculator
-        {
-        public:
-            MecanumCalculator(const Meters& horizontalWheelDistance,
-                              const Meters& verticalWheelDistance,
-                              const Meters& wheelRadius);
-
-            void setWheelSpeeds(const QuadDriveData& quadDriveData);
-
-            /**
-             * Returns the movement of the robot relative to the robot.
-             */
-            Velocity2D getTranslation() const;
-
-            /**
-             * Counterclockwise is positive
-             */
-            RPM getRotation() const;
-        private:
-            data::chassis::QuadDriveData<RadiansPerSecond> quadDriveData_{RPM{0}, RPM{0}, RPM{0}, RPM{0}};
-            ::chassis::AbstractMecanumCalculator abstractMecanumCalculator_{};
-            //l_x + l_y
-            const Meters kwheelDistanceConstant_;
-            const Meters kWheelRadius_;
-        };
-    }
+        Velocity2D getTranslation() const;
+        /**
+         * Counterclockwise is positive
+         */
+        RPM getRotation() const;
+    private:
+        data::chassis::QuadDriveData<RadiansPerSecond> quadDriveData_{RPM{0}, RPM{0}, RPM{0}, RPM{0}};
+        ::chassis::AbstractMecanumCalculator abstractMecanumCalculator_{};
+        //l_x + l_y
+        const Meters kwheelDistanceConstant_;
+        const Meters kWheelRadius_;
+    };
 }
 #endif
