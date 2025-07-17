f75de5c3c39d5624b02f689fe245eaadaff9bd8f
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-17 14:24:38 -0500

    clean!(chassislogicaliases): Remove unused namespace scheme

--- fang-mcb-project/src/configuration/chassislogicaliases.hpp
+++ fang-mcb-project/src/configuration/chassislogicaliases.hpp
@@ -6,28 +6,17 @@
 #include "mathaliases.hpp"
 #include "modm/math/geometry/vector.hpp"
 
-namespace logic
-{
-    namespace chassis
-    {
-        using AbstractQuadDriveData = data::chassis::QuadDriveData<double>;
-        using QuadDriveData = data::chassis::QuadDriveData<RPM>;
-        using Translation2D = math::AbstractVector2D;
-
-        //Messy for backward compatibility. Will be refactored during the next refactor cycle
-        //Oof.
-        using Velocity2D = data::math::Vector2D<MetersPerSecond>;
-
-        using WheelDirection = modm::Vector<double, 2>;
-    }
-}
-
 namespace chassis
 {
-    using Translation2D = logic::chassis::Translation2D; //Patch to make refactoring more smooth
-    using AbstractQuadDriveData = logic::chassis::AbstractQuadDriveData;
-    using Velocity2D = logic::chassis::Velocity2D;
-    using WheelDirection = logic::chassis::WheelDirection;
-    using QuadDriveData = logic::chassis::QuadDriveData;
+    using AbstractQuadDriveData = data::chassis::QuadDriveData<double>;
+    using QuadDriveData = data::chassis::QuadDriveData<RPM>;
+    using Translation2D = math::AbstractVector2D;
+
+    //Messy for backward compatibility. Will be refactored during the next refactor cycle
+    //Oof.
+    using Velocity2D = data::math::Vector2D<MetersPerSecond>;
+
+    using WheelDirection = modm::Vector<double, 2>;
 }
+
 #endif
