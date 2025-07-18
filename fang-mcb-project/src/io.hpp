42b86a51575cb326613563bfb8a48b0f5fcc6716
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-18 01:43:54 -0500

    clean(io): Remove unused drivers parameter

--- fang-mcb-project/src/io.hpp
+++ fang-mcb-project/src/io.hpp
@@ -1,20 +1,20 @@
 #ifndef FANG_ROBOTICS_MCB_IO_HPP
 #define FANG_ROBOTICS_MCB_IO_HPP
-
-#include "drivers.hpp"
-
-
+/**
+ * How often the main loop should loop
+ */
 static constexpr float kMainLoopFrequency{500.0f}; //Hertz
+
 /**
  * Place any sort of input/output initialization here. For example, place
  * serial init stuff here.
  */
-void initializeIo(Drivers& drivers);
+void initializeIo();
 
 /**
  * Anything that you would like to be called place here. It will be called
  * very frequently. Use PeriodicMilliTimers if you don't want something to be
  * called as frequently.
  */
-void updateIo(Drivers& drivers);
+void updateIo();
 #endif
