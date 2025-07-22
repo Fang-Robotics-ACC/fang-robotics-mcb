d016f426c9e9ac019727802eea2438b725679938
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-22 15:25:47 -0500

    fix(modm_abandon): Cause all activities to cease

--- fang-mcb-project/src/system/assert/modm_abandon.cpp
+++ fang-mcb-project/src/system/assert/modm_abandon.cpp
@@ -10,15 +10,13 @@ void modm_abandon(const modm::AssertionInfo &info)
     //TODO: write to terminal or buzz in morse code
 
     Drivers& drivers{DriversSingleton::getDrivers()};
+
     //Create signal and preserve info
     drivers.pwm.setTimerFrequency(kBuzzerTimer, kAbandonFrequency);
     drivers.pwm.write(kBuzzerVolume, tap::gpio::Pwm::Buzzer);
 
-    //Required for buzzing to occur
+    //Stop execution completely
     while(true)
     {
-
-        drivers.update();
-        //Stop execution completely
     }
 }
