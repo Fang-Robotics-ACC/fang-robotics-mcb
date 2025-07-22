55bd23fd42ba8a1d31454c10ea9dfbf745252fa0
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-21 19:18:18 -0500

    fix(build): Add proper include for unique pointers
    Oh well ahhh

--- fang-mcb-project/src/wrap/rail/rail_turret_aliases.hpp
+++ fang-mcb-project/src/wrap/rail/rail_turret_aliases.hpp
@@ -2,6 +2,7 @@
 #define FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_TURRET_ALIASES_HPP
 #include "rail/turret/feeder/isimple_feeder.hpp"
 #include "wrap/rail/turret/i_simple_feeder_subsystem_control.hpp" //TODO: Figure out how to incorporate taproot without entaglement
+#include <memory>
 namespace fang::turret
 {
     using ISimpleFeeder = rail::turret::ISimpleFeeder;
