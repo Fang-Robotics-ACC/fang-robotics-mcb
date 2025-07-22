69efd3e73cda08a907e9f48c023b6a8d040d3881
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-22 15:53:48 -0500

    refactor!(modm_abandon): Move system killing responsibility away from modm_abandon
    Single responsibility principle

--- fang-mcb-project/src/system/assert/modm_abandon.cpp
+++ fang-mcb-project/src/system/assert/modm_abandon.cpp
@@ -1,19 +1,17 @@
 #include "modm_abandon.hpp"
 #include "driver/drivers_singleton.hpp"
+#include "system/emergency/kill_system.hpp"
 #include "system/error/signal/assert_failed_buzz.hpp"
 
 void modm_abandon(const modm::AssertionInfo &info)
 {
     //This forces the debugger to see it via copy constructor
     static modm::AssertionInfo readInfo = info; 
-
     //TODO: write to terminal or buzz in morse code
-
     Drivers& drivers{DriversSingleton::getDrivers()};
-    fang::error::assertFailedBuzz(drivers.pwm);
+    fang::emergency::killSystem();
 
-    //Prevent motors from flailing
-    drivers.pwm.writeAllZeros();
+    fang::error::assertFailedBuzz(drivers.pwm);
 
     //Stop execution completely
     while(true)
