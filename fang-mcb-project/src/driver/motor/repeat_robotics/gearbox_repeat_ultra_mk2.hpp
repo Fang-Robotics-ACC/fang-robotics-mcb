e68512bd029ebd2660a7a7663099404a0afa47c1
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-22 23:20:19 -0500

    refactor!(GearboxRepeatUltraMk2): Update to new file name convention

--- fang-mcb-project/src/driver/motor/repeat_robotics/gearbox_repeat_ultra_mk2.hpp
+++ fang-mcb-project/src/driver/motor/repeat_robotics/gearbox_repeat_ultra_mk2.hpp
@@ -1,5 +1,5 @@
-#ifndef FANG_ROBOTICS_MCB_GEARED_REPEAT_ULTRA_MK2_HPP
-#define FANG_ROBOTICS_MCB_GEARED_REPEAT_ULTRA_MK2_HPP
+#ifndef FANG_ROBOTICS_MCB_DRIVER_MOTOR_GEARBOX_REPEAT_ULTRA_MK2_HPP
+#define FANG_ROBOTICS_MCB_DRIVER_MOTOR_GEARBOX_REPEAT_ULTRA_MK2_HPP
 #include "repeat_ultra_mk2.hpp"
 #include "driver/motor/data/directionality.hpp"
 #include "wrap/trap/communication/pwm_data.hpp"
