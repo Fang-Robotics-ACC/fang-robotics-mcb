5a86d2438cdeaf3f51893db63ceed0e4164b7755
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-25 14:39:37 -0500

    refactor!(turret): Update subsystems and inputs to fang namespace convention

--- fang-mcb-project/src/control/turret/gimbal_subsystem.hpp
+++ fang-mcb-project/src/control/turret/gimbal_subsystem.hpp
@@ -9,64 +9,61 @@
 
 #include "tap/control/subsystem.hpp"
 
-namespace control
+namespace fang::turret
 {
-    namespace turret
+    class GimbalSubsystem : public virtual tap::control::Subsystem
     {
-        class GimbalSubsystem : public virtual tap::control::Subsystem
+    public:
+
+        using Imu = trap::communication::sensors::Imu;
+
+        struct Config
         {
-        public:
-
-            using Imu = trap::communication::sensors::Imu;
-
-            struct Config
-            {
-                Degrees homeYawOffset;
-                GimbalSystem::Config gimbalConfig;
-            };
-
-            /**
-             * yawHomeOffset - how many degrees the zero yaw of the yaw motor fr
-             * the forward direction
-             */
-
-            virtual void initialize() override;
-            virtual void refresh() override;
-
-            GimbalSubsystem(Drivers& drivers, Imu& chassisImu, const Config& config);
-
-            /**
-             * Wherever the the yaw is aiming to look fieldwise, returns
-             * a unit vector.
-             */
-            math::AbstractVector2D getTargetFieldDirection() const;
-
-            void setFieldYaw(const Radians& yaw);
-            void addFieldYaw(const Radians& angle);
-
-            Radians getTargetFieldYaw() const;
-
-            void setPitch(const Radians& pitch);
-            Radians getPitch() const;
-            void addPitch(const Radians& pitch);
-
-            Radians getMaxPitch() const;
-            Radians getMinPitch() const;
-
-        private:
-            static constexpr math::AbstractVector2D mk_forward{0.0, 1.0};
-            void syncFieldYaw();
-            Radians getChassisYaw() const;
-
-            //Robotwise yaw where zero degrees is the forward direciton on the chassis
-            void setRawTurretRobotwiseYaw(const Radians& yaw);
-            const Radians mk_homeYawOffset;
-            GimbalSystem m_gimbal;
-
-            Imu& m_chassisImu;
-
-            Radians m_targetFieldYaw{};
+            Degrees homeYawOffset;
+            GimbalSystem::Config gimbalConfig;
         };
-    }
+
+        /**
+         * yawHomeOffset - how many degrees the zero yaw of the yaw motor fr
+         * the forward direction
+         */
+
+        virtual void initialize() override;
+        virtual void refresh() override;
+
+        GimbalSubsystem(Drivers& drivers, Imu& chassisImu, const Config& config);
+
+        /**
+         * Wherever the the yaw is aiming to look fieldwise, returns
+         * a unit vector.
+         */
+        math::AbstractVector2D getTargetFieldDirection() const;
+
+        void setFieldYaw(const Radians& yaw);
+        void addFieldYaw(const Radians& angle);
+
+        Radians getTargetFieldYaw() const;
+
+        void setPitch(const Radians& pitch);
+        Radians getPitch() const;
+        void addPitch(const Radians& pitch);
+
+        Radians getMaxPitch() const;
+        Radians getMinPitch() const;
+
+    private:
+        static constexpr math::AbstractVector2D mk_forward{0.0, 1.0};
+        void syncFieldYaw();
+        Radians getChassisYaw() const;
+
+        //Robotwise yaw where zero degrees is the forward direciton on the chassis
+        void setRawTurretRobotwiseYaw(const Radians& yaw);
+        const Radians mk_homeYawOffset;
+        GimbalSystem m_gimbal;
+
+        Imu& m_chassisImu;
+
+        Radians m_targetFieldYaw{};
+    };
 }
 #endif
--- fang-mcb-project/src/control/turret/turret_input_handler.hpp
+++ fang-mcb-project/src/control/turret/turret_input_handler.hpp
@@ -1,57 +1,54 @@
 #ifndef FANG_ROBOTICS_MCB_TURRET_INPUT_HANDLER_HPP
 #define FANG_ROBOTICS_MCB_TURRET_INPUT_HANDLER_HPP
 #include "tap/communication/serial/remote.hpp"
-namespace control
+namespace fang::turret
 {
-    namespace turret
+    class TurretInputHandler
     {
-        class TurretInputHandler
+    public:
+        using Remote = tap::communication::serial::Remote;
+        struct RemoteConfig
         {
-        public:
-            using Remote = tap::communication::serial::Remote;
-            struct RemoteConfig
-            {
-                Remote::Channel pitchChannel;
-                Remote::Channel yawChannel;
-                Remote::Channel rotationChannel;
-            };
-
-            struct MouseConfig
-            {
-                double pitchPercentagePerPx;
-                double yawPercentagePerPx;
-            };
-
-            struct Config
-            {
-                RemoteConfig remoteConfig;
-                MouseConfig mouseConfig;
-            };
-
-            TurretInputHandler(Remote& remote, const Config& config);
-
-            double getPitch() const;
-            /**
-             * Counterclockwise is positive, clockwise is negative
-             * (Math convention.)
-             */
-            double getYaw() const;
-            bool getFire() const;
-        private:
-            static constexpr double mk_abstractMin{-1.0};
-            static constexpr double mk_abstractMax{1.0};
-
-            double getRemotePitch() const;
-            double getRemoteYaw() const;
-
-            double getMousePitch() const;
-            double getMouseYaw() const;
-            static constexpr double mk_firingWheelThreshold{750};
-            Remote& m_remote;
-
-            const RemoteConfig mk_remoteConfig;
-            const MouseConfig mk_mouseConfig;
+            Remote::Channel pitchChannel;
+            Remote::Channel yawChannel;
+            Remote::Channel rotationChannel;
         };
-    }
+
+        struct MouseConfig
+        {
+            double pitchPercentagePerPx;
+            double yawPercentagePerPx;
+        };
+
+        struct Config
+        {
+            RemoteConfig remoteConfig;
+            MouseConfig mouseConfig;
+        };
+
+        TurretInputHandler(Remote& remote, const Config& config);
+
+        double getPitch() const;
+        /**
+         * Counterclockwise is positive, clockwise is negative
+         * (Math convention.)
+         */
+        double getYaw() const;
+        bool getFire() const;
+    private:
+        static constexpr double mk_abstractMin{-1.0};
+        static constexpr double mk_abstractMax{1.0};
+
+        double getRemotePitch() const;
+        double getRemoteYaw() const;
+
+        double getMousePitch() const;
+        double getMouseYaw() const;
+        static constexpr double mk_firingWheelThreshold{750};
+        Remote& m_remote;
+
+        const RemoteConfig mk_remoteConfig;
+        const MouseConfig mk_mouseConfig;
+    };
 }
 #endif
