2e8f4dc006d6d214e17a1dd2e4a1623bae59a11a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-24 23:44:36 -0500

    perf(CounterStrikeCommand): Autodocument invariants

--- fang-mcb-project/src/control/chassis/holonomic/command/counter_strike_command.cpp
+++ fang-mcb-project/src/control/chassis/holonomic/command/counter_strike_command.cpp
@@ -45,6 +45,29 @@ namespace fang::chassis
         //Relative to turret
         const math::AbstractVector2D turretFrameTranslation{m_input.getTranslation()};
         const physics::Velocity2D frameTranslation{turretFrameTranslation.x * mk_config.maxXTranslation, turretFrameTranslation.y * mk_config.maxYTranslation};
+
+        //If the turret frame translation is positive, the frame translation must be possitive
+        //Because the configuration scalars must be positive themselves
+
+        math::Vector2D<bool> turretFrameTranslationSigns
+        {
+            std::signbit(turretFrameTranslation.x),
+            std::signbit(turretFrameTranslation.y)
+        };
+
+        math::Vector2D<bool> frameTranslationSigns 
+        {
+            std::signbit(frameTranslation.x),
+            std::signbit(frameTranslation.y)
+        };
+
+        //These will be optimized away on performance builds
+        const bool sameSignCheckX{turretFrameTranslationSigns.x == frameTranslationSigns.x};
+        const bool sameSignCheckY{turretFrameTranslationSigns.y == frameTranslationSigns.y};
+
+        modm_assert(sameSignCheckX, "CounterStrikeCommand.getFieldTraslation.sameSign.x", "Scaling should not invert motion");
+        modm_assert(sameSignCheckY, "CounterStrikeCommand.getFieldTraslation.sameSign.y", "Scaling should not invert motion");
+
         const Radians turretBearing{m_gimbal.getTargetFieldYaw()};
 
         const physics::Velocity2D fieldTranslation{math::rotateVector2D(frameTranslation, turretBearing)};
@@ -58,7 +81,7 @@ namespace fang::chassis
         const RPM rotation{abstractRotation * mk_config.maxRotation};
 
         const bool sameSignCheck{std::signbit(abstractRotation) == std::signbit(static_cast<double>(abstractRotation))};
-        modm_assert(sameSignCheck, "CounterStrikeCommand.getFieldRotation.sameSign", "Signs should be the same");
+        modm_assert(sameSignCheck, "CounterStrikeCommand.getFieldRotation.sameSign", "Scaling should not invert rotation");
 
         return rotation;
     }
