329d9d18c081db3068b086dfeb6d2a71853f1e13
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-25 11:03:19 -0500

    fix(FANG_ASSERT): Fix information passing

--- fang-mcb-project/src/system/assert/fang_assert.hpp
+++ fang-mcb-project/src/system/assert/fang_assert.hpp
@@ -12,7 +12,7 @@
      * NAME and DESC should be 
      * 
      */
-    #ifdef PLATFORM_HOSTED
+    #ifndef PLATFORM_HOSTED
         /**
          * Pass should be a boolean variable
          * if it's false, then an assert will be triggered
@@ -20,17 +20,17 @@
          * The file
          * NAME and DESC should be c style strings
          */
-        #define FANG_ASSERT(PASS, NAME, DESC)                       \
-            {                                                       \
-                const std::string FANG_ASSERT_FILE{__FILE__};       \
-                constexpr std::string FANG_ASSERT_LINE{__LINE__};   \
-                constexpr std::string DESCRIPTION{DESC};            \
-                {                                                   \
-                    FANG_ASSERT_FILE +                              \
-                    " : " + FANG_ASSERT_LINE +                      \
-                    " : " + DESC                                    \
-                };                                                  \
-                modm_assert(PASS, NAME, DESC);                      \
+        #define FANG_ASSERT(PASS, NAME, DESC)                                               \
+            {                                                                               \
+                const std::string FANG_ASSERT_FILE{__FILE__};                               \
+                const std::string FANG_ASSERT_LINE{"Line " + std::to_string(__LINE__)};     \
+                const std::string DESCRIPTION                                               \
+                {                                                                           \
+                    FANG_ASSERT_FILE +                                                      \
+                    " : " + FANG_ASSERT_LINE +                                              \
+                    " : " + DESC                                                            \
+                };                                                                          \
+                modm_assert(PASS, NAME, DESCRIPTION.c_str());                               \
             }
         #else
             /**
