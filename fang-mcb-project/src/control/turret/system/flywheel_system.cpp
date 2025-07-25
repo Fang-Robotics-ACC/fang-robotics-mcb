b436d97b3bc1ef8a07087b2ec946ff01203c8ba9
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-25 14:36:23 -0500

    refactor!(turret): Move everything under fang

--- fang-mcb-project/src/control/turret/system/flywheel_system.cpp
+++ fang-mcb-project/src/control/turret/system/flywheel_system.cpp
@@ -2,34 +2,31 @@
 #include "wrap/units/units_alias.hpp"
 
 using namespace units::literals;
-namespace control
+namespace fang::turret
 {
-    namespace turret
+    OldFlywheelSystem::OldFlywheelSystem(Drivers& drivers, const Config& config)
+    :   m_radius{config.radius / 1_rad},
+        m_motor{drivers, config.motorConfig}
     {
-        OldFlywheelSystem::OldFlywheelSystem(Drivers& drivers, const Config& config)
-        :   m_radius{config.radius / 1_rad},
-            m_motor{drivers, config.motorConfig}
-        {
-        }
+    }
 
-        void OldFlywheelSystem::setTargetRimSpeed(const MetersPerSecond& rimSpeed)
-        {
-            m_motor.setTargetSpeed(RimSpeedToMotorSpeed(rimSpeed));
-        }
+    void OldFlywheelSystem::setTargetRimSpeed(const MetersPerSecond& rimSpeed)
+    {
+        m_motor.setTargetSpeed(RimSpeedToMotorSpeed(rimSpeed));
+    }
 
-        RPM OldFlywheelSystem::RimSpeedToMotorSpeed(const MetersPerSecond& rimSpeed) const
-        {
-            return rimSpeed / m_radius;
-        }
+    RPM OldFlywheelSystem::RimSpeedToMotorSpeed(const MetersPerSecond& rimSpeed) const
+    {
+        return rimSpeed / m_radius;
+    }
 
-        void OldFlywheelSystem::update()
-        {
-            m_motor.update();
-        }
+    void OldFlywheelSystem::update()
+    {
+        m_motor.update();
+    }
 
-        void OldFlywheelSystem::initialize()
-        {
-            m_motor.initialize();
-        }
+    void OldFlywheelSystem::initialize()
+    {
+        m_motor.initialize();
     }
 }
