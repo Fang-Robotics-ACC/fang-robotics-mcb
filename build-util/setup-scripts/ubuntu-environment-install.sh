8a8ce5d2615725e5f7925e22740d5049421cf73e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-27 12:15:35 -0500

    fix(setup-scripts): Add python to path properly
    missing quotation mark

--- build-util/setup-scripts/ubuntu-environment-install.sh
+++ build-util/setup-scripts/ubuntu-environment-install.sh
@@ -6,15 +6,16 @@ libgmock-dev libgtest-dev pipenv wget curl -y
 wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 tar -xf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 rm gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
+
 mkdir ~/.local/share
 mv gcc-arm-none-eabi-10.3-2021.10 ~/.local/share
 echo 'export PATH="$HOME/gcc-arm-none-eabi-10.3-2021.10/bin:$HOME/.local/bin:$PATH:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+
 curl -fsSL https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
-
+source ~/.bashrc
 pyenv install 3.8.10
 cd ../../fang-mcb-project
 pipenv install --python 3.8.10
