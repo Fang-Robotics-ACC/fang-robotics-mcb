0c1c166c1b3f5abf1a036826bd0eb0c73fd8c091
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-27 13:35:06 -0500

    docs(setup-scripts): Add copyright and licensing info

--- build-util/setup-scripts/ubuntu-environment-install.sh
+++ build-util/setup-scripts/ubuntu-environment-install.sh
@@ -1,5 +1,30 @@
 #!/bin/bash -i
 
+# COPYRIGHT 2025 RAVEN ASHER RAZIEL under MIT license
+# This version is dual licensed under agplv3 as well
+# (view license of fang-robotics-acc/fang-robotics-acc)
+# Your project must upgrade from gplv3 to agplv3 if you use this script
+# If you use taproot, you already are gplv3, might as well get better protection
+# with agplv3
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the “Software”), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 #General build requirements
 sudo apt-get install python3 python-is-python3 \
 python3-pip git openocd gcc build-essential \
