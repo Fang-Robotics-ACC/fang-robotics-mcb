8fdde2452f907c40d39b608a92f2b368e0bffafe
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-27 12:10:02 -0500

    fix(setup-scripts): Install necessary component

--- build-util/setup-scripts/ubuntu-environment-install.sh
+++ build-util/setup-scripts/ubuntu-environment-install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install python3 python-is-python3 \
 python3-pip git openocd gcc build-essential \
 libboost-all-dev openocd stlink-tools \
-libgmock-dev libgtest-dev pipenv wget -y
+libgmock-dev libgtest-dev pipenv wget curl -y
 
 wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
 tar -xf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
