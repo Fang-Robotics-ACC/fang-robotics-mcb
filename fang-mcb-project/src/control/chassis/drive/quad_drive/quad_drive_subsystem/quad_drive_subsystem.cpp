b9b00ea441395c7f026f64f9d1b48c436ab13a2a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-29 02:34:49 -0500

    feat(QuadDriveData): Implement constructor

---
+++ fang-mcb-project/src/control/chassis/drive/quad_drive/quad_drive_subsystem/quad_drive_subsystem.cpp
@@ -0,0 +1,22 @@
+#include "quad_drive_subsystem.hpp"
+namespace fang::chassis
+{
+    QuadDriveSubsystem::QuadDriveSubsystem
+    (
+        Drivers& drivers,
+        std::unique_ptr<Motor> frontLeftMotor,
+        std::unique_ptr<Motor> frontRightMotor,
+        std::unique_ptr<Motor> rearLeftMotor,
+        std::unique_ptr<Motor> rearRightMotor
+    ):
+        Subsystem{&drivers},
+        motors_
+        {
+            std::move(frontLeftMotor),
+            std::move(frontRightMotor),
+            std::move(rearLeftMotor),
+            std::move(rearRightMotor)
+        }
+    {
+    }
+}
