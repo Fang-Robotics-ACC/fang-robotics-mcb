2138ab810bcfb5a0fddbb0229dc463e25a62da92
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-29 17:53:52 -0500

    feat(QuadDriveSubsystem): Make variant for Pierce hardware

---
+++ fang-mcb-project/src/control/chassis/drive/quad_drive/quad_drive_subsystem/repeat_ultra_mk2_quad_drive_subsystem.hpp
@@ -0,0 +1,34 @@
+#ifndef FANG_ROBOTICS_MCB_CONTROL_CASSIS_DRIVE_QUAD_DRIVE_QUAD_DRIVE_SUBSYSTEM_REPEAT_ULTRA_MK2_QUAD_DRIFVE_SUBSYSTE_HPP
+#define FANG_ROBOTICS_MCB_CONTROL_CASSIS_DRIVE_QUAD_DRIVE_QUAD_DRIVE_SUBSYSTEM_REPEAT_ULTRA_MK2_QUAD_DRIFVE_SUBSYSTE_HPP
+#include "quad_drive_subsystem.hpp"
+#include "driver/motor/repeat_robotics/gearbox_repeat_ultra_mk2.hpp"
+
+#include <memory>
+namespace fang::chassis
+{
+    class RepeatUltraMk2QuadDriveSubsystem : public QuadDriveSubsystem
+    {
+    public:
+        using Motor = motor::GearboxRepeatUltraMk2;
+        struct Config
+        {
+            Motor::Config frontLeftConfig;
+            Motor::Config frontRightConfig;
+            Motor::Config rearLeftConfig;
+            Motor::Config rearRightConig;
+        };
+
+        RepeatUltraMk2QuadDriveSubsystem(Drivers& drivers, const Config& config):
+            QuadDriveSubsystem
+            {
+                drivers,
+                std::make_unique<Motor>(config.frontLeftConfig),
+                std::make_unique<Motor>(config.frontRightConfig),
+                std::make_unique<Motor>(config.rearLeftConfig),
+                std::make_unique<Motor>(config.rearRightConig)
+            }
+        {
+        }
+    };
+}
+#endif
