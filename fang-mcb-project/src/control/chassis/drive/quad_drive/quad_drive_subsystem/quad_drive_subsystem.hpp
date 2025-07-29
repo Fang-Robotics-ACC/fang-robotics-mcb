8bffd0c947a60a658458fc75ee1c8ac0c99e4e14
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-28 20:40:43 -0500

    feat(QuadDriveSubsystem): Prepare for test-prewriting

--- fang-mcb-project/src/control/chassis/drive/quad_drive/quad_drive_subsystem/quad_drive_subsystem.hpp
+++ fang-mcb-project/src/control/chassis/drive/quad_drive/quad_drive_subsystem/quad_drive_subsystem.hpp
@@ -3,16 +3,31 @@
 #include "control/chassis/drive/quad_drive/data/quad_drive_data.hpp"
 #include "wrap/rail/chassis/iquad_drive_subsystem.hpp"
 #include "wrap/rail/rail_motor_owner.hpp"
+
+#include <memory>
+
 namespace fang::chassis
 {
     class QuadDriveSystem:
         virtual public IQuadDriveSubsystem
     {
     public:
+        virtual void IQuadDriveSubsystem
+        (
+            std::unique_ptr<motor::ISpeedMotor> frontLeftMotor,
+            std::unique_ptr<motor::ISpeedMotor> frontRightMotor,
+            std::unique_ptr<motor::ISpeedMotor> rearLeftMotor,
+            std::unique_ptr<motor::ISpeedMotor> rearRightMotor
+        );
+
         virtual void setTargetWheelSpeeds(const QuadRPM& wheelSpeeds) override;
         virtual void initialize() override;
         virtual void refresh() override;
     private:
+        std::unique_ptr<motor::ISpeedMotor> frontLeftMotor_;
+        std::unique_ptr<motor::ISpeedMotor> frontRightMotor_;
+        std::unique_ptr<motor::ISpeedMotor> rearLeftMotor_;
+        std::unique_ptr<motor::ISpeedMotor> rearRightMotor_;
     };
 }
 #endif
