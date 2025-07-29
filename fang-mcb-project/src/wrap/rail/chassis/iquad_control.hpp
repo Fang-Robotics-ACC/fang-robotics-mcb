bd81204b818e8b4353a988dec5f0ba67c57e532e
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-28 20:02:22 -0500

    refactor(rail): Organize into category

--- fang-mcb-project/src/wrap/rail/chassis/iquad_control.hpp
+++ fang-mcb-project/src/wrap/rail/chassis/iquad_control.hpp
@@ -2,7 +2,7 @@
 #define FANG_ROBOTICS_MCB_WRAP_RAIL_CHASSIS_IQUAD_CONTROL_HPP
 
 #include "rail/chassis/iquad_control.hpp"
-#include "control/chassis/drive/quad_drive/data/quad_drive_data.hpp"
+#include "quad_data_alias.hpp"
 
 namespace fang::chassis
 {
