c1e4864a5f5c413eb5dbae03fe9a7efb11f7c32b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-28 20:35:37 -0500

    refactor(rail): Align to new directory structure

--- fang-mcb-project/src/wrap/rail/rail_motors.hpp
+++ fang-mcb-project/src/wrap/rail/rail_motors.hpp
@@ -1,14 +1,13 @@
 #ifndef FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_MOTORS_HPP
 #define FANG_ROBOTICS_MCB_WRAP_RAIL_RAIL_MOTORS_HPP
 #include "wrap/units/units_alias.hpp"
+#include "motor/ispeed_motor.hpp"
 
-#include <rail/motor/ispeed_motor.hpp>
 #include <rail/motor/iposition_motor.hpp>
 #include <rail/motor/ispeed_wheel.hpp>
 
 namespace fang::motor
 {
-    using ISpeedMotor = rail::motor::ISpeedMotor<RPM>;
     using IPositionMotor = rail::motor::IPositionMotor<Radians>;
     using ISpeedWheel = rail::motor::ISpeedWheel<MetersPerSecond>;
 }
