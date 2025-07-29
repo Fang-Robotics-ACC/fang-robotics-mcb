6a3178d11330daf95ecfa12a538b23f9d712608b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-28 20:00:43 -0500

    refactor(QuadDriveData): Organize aliases into wrapper

--- fang-mcb-project/src/control/chassis/drive/quad_drive/data/quad_drive_data.hpp
+++ fang-mcb-project/src/control/chassis/drive/quad_drive/data/quad_drive_data.hpp
@@ -1,12 +1,5 @@
 #ifndef FANG_ROBOTICS_MCB_CONTROL_CHASSIS_DATA_QUAD_DRIVE_DATA_HPP
 #define FANG_ROBOTICS_MCB_CONTROL_CHASSIS_DATA_QUAD_DRIVE_DATA_HPP
-#include "wrap/rail/chassis/rail_drive_alias.hpp"
-#include "wrap/units/units_alias.hpp"
+#include "wrap/rail/chassis/quad_data_alias.hpp"
 
-namespace fang::chassis
-{
-    using AbstractQuadSpeeds = QuadDriveData<double>;
-    using QuadRPM = QuadDriveData<RPM>;
-    using QuadSpeedsRadPersec = QuadDriveData<RadiansPerSecond>;
-}
 #endif
