df795d473bc5fc934637baf29f758385b82c7b8a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-07-29 21:41:35 -0500

    fix(ISimpleFeederSubsystem): Stop requiring duplicate base class initialization

--- fang-mcb-project/src/wrap/rail/turret/isimple_feeder_subsystem.hpp
+++ fang-mcb-project/src/wrap/rail/turret/isimple_feeder_subsystem.hpp
@@ -1,17 +1,23 @@
 #ifndef FANG_ROBOTICS_MCB_RAIL_TURRET_I_SIMPLE_FEEDER_SUBSYSTEM_HPP
 #define FANG_ROBOTICS_MCB_RAIL_TURRET_I_SIMPLE_FEEDER_SUBSYSTEM_HPP
-#include "wrap/rail/drivers.hpp"
-#include "tap/control/subsystem.hpp"
+
 #include "rail/turret/feeder/isimple_feeder_control.hpp"
+
+#include "tap/control/subsystem.hpp"
+#include "tap/drivers.hpp"
+
 namespace rail::turret
 {
+    /**
+     * Provide taproot commands with uniform access to
+     * simple feeder subsystem
+     */
     class ISimpleFeederSubsystem:
-        virtual public tap::control::Subsystem,
-        virtual public ISimpleFeederControl
+        public ISimpleFeederControl,
+        public tap::control::Subsystem
     {
     public:
-        //Trivial hack
-        ISimpleFeederSubsystem(Drivers& drivers):
+        ISimpleFeederSubsystem(tap::Drivers& drivers):
             tap::control::Subsystem(&drivers)
         {}
         virtual ~ISimpleFeederSubsystem() {};
