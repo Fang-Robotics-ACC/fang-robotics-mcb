e1e5bb76265063c178926070993226c124b69a90
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-01 17:45:05 -0500

    test(GearboxUltraMk2): Remove tests related to deprecated functions

--- fang-mcb-project/test/gearboxrepeatultramk2test.cpp
+++ fang-mcb-project/test/gearboxrepeatultramk2test.cpp
@@ -8,157 +8,8 @@
 
 using namespace units::literals;
 using namespace fang;
-TEST(minSpeedTest, repeatUltraMk2)
-{
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //For a unidirectional configuration, this is 0 rpm
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr double gearRatio{14.0};
 
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::UNIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(0.5, tap::gpio::Pwm::C1));
-
-    motor.setTargetSpeed(0_rpm);
-    }
-
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //The default max speed is the maximum theoretical speed
-    //For a bidirectional configuration, the minimum speed is the negative of the maximum theoretical speed
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr RPMPerVolt kv{1450};
-    constexpr double gearRatio{14.0};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::BIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(0.5, tap::gpio::Pwm::C1));
-    motor.setTargetSpeed(motor.getMinSpeed());
-    }
-}
-
-TEST(minClampSpeedtest, repeatUltraMk2)
-{
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //For a unidirectional configuration, this is 0 rpm
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr double gearRatio{14.0};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::UNIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(0.5, tap::gpio::Pwm::C1));
-
-    //Attempt to set it below the minimum speed
-    motor.setTargetSpeed(-10_rpm);
-    }
-
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //The default max speed is the maximum theoretical speed
-    //For a bidirectional configuration, the minimum speed is the negative of the maximum theoretical speed
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr RPMPerVolt kv{1450};
-    constexpr double gearRatio{14.0};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::BIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(0.5, tap::gpio::Pwm::C1));
-    //Attempt to set it below the minimum speed
-    motor.setTargetSpeed(motor.getMinSpeed() - 100_rpm);
-    }
-}
-
-TEST(maxSpeedTest, repeatUltraMk2)
-{
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //For a unidirectional configuration, this is 0 rpm
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr double gearRatio{14.0};
-    constexpr double maxDutyCycle{1};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::UNIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(maxDutyCycle, tap::gpio::Pwm::C1));
-
-    motor.setTargetSpeed(motor.getMaxSpeed());
-    }
-
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //The default max speed is the maximum theoretical speed
-    //For a bidirectional configuration, the minimum speed is the negative of the maximum theoretical speed
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr RPMPerVolt kv{1450};
-    constexpr double gearRatio{14.0};
-    constexpr double maxDutyCycle{1};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::BIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(maxDutyCycle, tap::gpio::Pwm::C1));
-    motor.setTargetSpeed(motor.getMaxSpeed());
-    }    
-}
-
-TEST(maxClampSpeedTest, repeatUltraMk2)
-{
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //For a unidirectional configuration, this is 0 rpm
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr double gearRatio{14.0};
-    constexpr double maxDutyCycle{1};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::UNIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(maxDutyCycle, tap::gpio::Pwm::C1));
-
-    motor.setTargetSpeed(motor.getMaxSpeed() + 123.3_rpm);
-    }
-
-    {
-    //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
-    //The default max speed is the maximum theoretical speed
-    //For a bidirectional configuration, the minimum speed is the negative of the maximum theoretical speed
-    constexpr Volts controllerVoltage{24};
-    constexpr Hertz frequency{500};
-    constexpr RPMPerVolt kv{1450};
-    constexpr double gearRatio{14.0};
-    constexpr double maxDutyCycle{1};
-
-    
-    tap::Drivers drivers{};
-    motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::BIDIRECTIONAL, false, gearRatio};
-
-    EXPECT_CALL(drivers.pwm,write(maxDutyCycle, tap::gpio::Pwm::C1));
-    motor.setTargetSpeed(motor.getMaxSpeed() + 234.5_rpm);
-    }    
-}
-
-TEST(zeroSpeedTest, repeatUltraMk2)
+TEST(UltraMk2, zeroSpeed)
 {
     {
     //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
@@ -167,7 +18,6 @@ TEST(zeroSpeedTest, repeatUltraMk2)
     constexpr Hertz frequency{500};
     constexpr double gearRatio{14.0};
     constexpr double stillDutyCycle{0.5};
-
     
     tap::Drivers drivers{};
     motor::GearboxRepeatUltraMk2 motor{drivers, controllerVoltage, tap::gpio::Pwm::C1, frequency, motor::Directionality::UNIDIRECTIONAL, false, gearRatio};
@@ -196,7 +46,7 @@ TEST(zeroSpeedTest, repeatUltraMk2)
     }    
 }
 
-TEST(invertedTest, repeatUltraMk2)
+TEST(UltraMk2, inversion)
 {
     {
     //The speed at the minimum range coresponds to a 1ms pulse to the vortex controller
