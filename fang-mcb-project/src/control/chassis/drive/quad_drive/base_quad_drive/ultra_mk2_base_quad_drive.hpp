58f10b6a065981a929fa47dc6e549893ace2dd5a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-01 10:07:35 -0500

    refactor!(UltraMk2BaseQuadDrive): Concise name

--- fang-mcb-project/src/control/chassis/drive/quad_drive/base_quad_drive/ultra_mk2_base_quad_drive.hpp
+++ fang-mcb-project/src/control/chassis/drive/quad_drive/base_quad_drive/ultra_mk2_base_quad_drive.hpp
@@ -1,12 +1,12 @@
-#ifndef FANG_ROBOTICS_MCB_CONTROL_CASSIS_DRIVE_QUAD_DRIVE_QUAD_DRIVE_SUBSYSTEM_REPEAT_ULTRA_MK2_QUAD_DRIFVE_SUBSYSTE_HPP
-#define FANG_ROBOTICS_MCB_CONTROL_CASSIS_DRIVE_QUAD_DRIVE_QUAD_DRIVE_SUBSYSTEM_REPEAT_ULTRA_MK2_QUAD_DRIFVE_SUBSYSTE_HPP
+#ifndef FANG_ROBOTICS_MCB_CONTROL_CASSIS_DRIVE_QUAD_DRIVE_ULTRA_MK2_BASE_QUAD_DRIVE_HPP
+#define FANG_ROBOTICS_MCB_CONTROL_CASSIS_DRIVE_QUAD_DRIVE_ULTRA_MK2_BASE_QUAD_DRIVE_HPP
 #include "base_quad_drive.hpp"
 #include "driver/motor/repeat_robotics/gearbox_repeat_ultra_mk2.hpp"
 
 #include <memory>
 namespace fang::chassis
 {
-    class RepeatUltraMk2QuadDriveSubsystem : public BaseQuadDrive
+    class UltraMk2BaseQuaddrive : public BaseQuadDrive
     {
     public:
         using Motor = motor::GearboxRepeatUltraMk2;
@@ -18,7 +18,7 @@ namespace fang::chassis
             Motor::Config rearRightConig;
         };
 
-        RepeatUltraMk2QuadDriveSubsystem(Drivers& drivers, const Config& config):
+        UltraMk2BaseQuaddrive(Drivers& drivers, const Config& config):
             BaseQuadDrive
             {
                 drivers,
