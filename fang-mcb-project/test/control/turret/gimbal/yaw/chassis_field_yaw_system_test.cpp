0b888fd8486b02eac32ab78277ad5466cf2651d0
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-04 14:03:46 -0500

    test(ChassisFieldYaw): Check for compensation

--- fang-mcb-project/test/control/turret/gimbal/yaw/chassis_field_yaw_system_test.cpp
+++ fang-mcb-project/test/control/turret/gimbal/yaw/chassis_field_yaw_system_test.cpp
@@ -7,6 +7,7 @@
 
 namespace fang::turret
 {
+    using namespace units::literals;
     class FieldYawTestSetup
     {
     public:
@@ -33,4 +34,119 @@ namespace fang::turret
     ImuMock& imu_;
     ChassisFieldYawSystem yawSystem_;
     };
+
+    struct TargetFieldYawParam
+    {
+        Radians targetFieldYaw;
+        Radians chassisAngle;
+        Radians expectedMotorAngle;
+    };
+
+    /**
+     * Check if the system properly compensates for the chassis input
+     */
+    class TargetFieldYawTest:
+        public FieldYawTestSetup,
+        public ::testing::TestWithParam<TargetFieldYawParam>
+    {
+    public:
+        TargetFieldYawTest():
+            FieldYawTestSetup({0_rad}) //Test the field yaw independent from yaw error
+        {}
+    protected:
+        const TargetFieldYawParam param{GetParam()};
+        Radians targetFieldYaw_{param.targetFieldYaw};
+        Radians chassisAngle_{param.chassisAngle};
+        Radians expectedAngle_{param.expectedMotorAngle};
+    };
+
+    
+    TEST_P(TargetFieldYawTest, targetFieldYaw)
+    {
+        ON_CALL(imu_, getYaw()).WillByDefault(testing::Return(chassisAngle_));
+        EXPECT_CALL(motor_, setTargetPosition(expectedAngle_));
+        yawSystem_.setTargetFieldYaw(targetFieldYaw_);
+    }
+
+    INSTANTIATE_TEST_CASE_P
+    (
+        zero,
+        TargetFieldYawTest,
+        testing::Values
+        (
+            TargetFieldYawParam
+            {
+                .targetFieldYaw     = 0_rad,
+                .chassisAngle       = 0_rad,
+                .expectedMotorAngle = 0_rad,
+            }
+        )
+    );
+
+    INSTANTIATE_TEST_CASE_P
+    (
+        positive,
+        TargetFieldYawTest,
+        testing::Values
+        (
+            TargetFieldYawParam
+            {
+                .targetFieldYaw     = 15_rad,
+                .chassisAngle       = 15_rad,
+                .expectedMotorAngle = 0_rad
+            },
+            TargetFieldYawParam
+            {
+                .targetFieldYaw     = 30_rad,
+                .chassisAngle       = 15_rad,
+                .expectedMotorAngle = 15_rad
+            }
+        )
+    );
+
+    INSTANTIATE_TEST_CASE_P
+    (
+        negative,
+        TargetFieldYawTest,
+        testing::Values
+        (
+            TargetFieldYawParam
+            {
+                .targetFieldYaw     = -15_rad,
+                .chassisAngle       =  15_rad,
+                .expectedMotorAngle = -30_rad
+            },
+            TargetFieldYawParam
+            {
+                .targetFieldYaw     = -30_rad,
+                .chassisAngle       =  30_rad,
+                .expectedMotorAngle = -60_rad
+            }
+        )
+    );
+
+    struct YawErrorCorrectionParam
+    {
+        Radians targetFieldYaw;
+        ChassisFieldYawSystem::Config config; //Yaw error correction
+    };
+
+    /**
+     * Tests if the zero position compensates for a zero position on the
+     * motor which is offset from the chassis zero position.
+     * (e.g. the turret faces to the left of the robot)
+     */
+    class YawErrorCorrectionTest:
+        public FieldYawTestSetup,
+        public ::testing::TestWithParam<YawErrorCorrectionParam>
+    {
+    public:
+        YawErrorCorrectionTest():
+            FieldYawTestSetup(param.config)
+        {}
+    protected:
+        const YawErrorCorrectionParam param{GetParam()};
+        Radians targetFieldYaw_{param.targetFieldYaw};
+    };
+
 }
