5d3a23e4e2ad7c05b095d9114b8e2cf174a26877
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-04 16:32:35 -0500

    feat(ChassisFieldYawSystem): Create variant to prepare for Pierce use

---
+++ fang-mcb-project/src/control/turret/gimbal/yaw/chassis_field_yaw_system/gm6020_chassis_field_yaw_system.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "chassis_field_yaw_system.hpp"
+
+#include "driver/drivers.hpp"
+#include "wrap/trap/motor/dji_gm6020.hpp"
+
+namespace fang::turret
+{
+    class Gm6020FieldYawSystem : public ChassisFieldYawSystem
+    {
+        using Motor = trap::motor::DjiGM6020;
+        struct Config
+        {
+            Motor::Config motorConfig;
+            Radians yawError;
+        };
+        Gm6020FieldYawSystem(Drivers& drivers, std::unique_ptr<Imu> chassisImu, const Config& config):
+            ChassisFieldYawSystem
+            (
+                std::make_unique<Motor>(drivers, config.motorConfig),
+                std::move(chassisImu),
+                ChassisFieldYawSystem::Config{config.yawError}
+            )
+        {}
+    };
+}
