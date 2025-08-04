c05a400de1cae15e0a99733c86e4865a1cc06dca
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-04 12:45:46 -0500

    feat(ChassisFieldYawSystem): Declare interface

---
+++ fang-mcb-project/src/control/turret/gimbal/yaw/chassis_imu_yaw_system/chassis_field_yaw_system.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "wrap/trap/communication/sensors/iimu.hpp"
+#include "wrap/rail/turret/ifield_yaw_system.hpp"
+#include "wrap/rail/motor/iposition_motor.hpp"
+
+#include <memory>
+
+namespace fang::turret
+{
+    /**
+     * Assume that with the axis pointing up, the positive
+     * yaw counterclockwise
+     */
+    class ChassisFieldYawSystem : virtual public IFieldYawSystem
+    {
+    public:
+        using Motor = motor::IPositionMotor;
+        using Imu = trap::communication::sensors::IImu;
+        /**
+         * @var yawError - corrects the
+         * zero position of the motor. If the motor's zero
+         * position 5 degrees counterclockwise, the error 
+         * would be 5 degrees.
+         */
+        struct Config
+        {
+            Radians yawCorrection;
+        };
+
+        ChassisFieldYawSystem
+        (
+            std::unique_ptr<Motor> motor,
+            std::unique_ptr<Imu> imu,
+            const Config& config
+        );
+        void initialize() override;
+        void update() override;
+        void setTargetFieldYaw(const Radians& yaw) override;
+    private:
+        std::unique_ptr<Motor> motor_;
+        std::unique_ptr<Imu> imu_;
+        const Radians kYawError_;
+    };
+}
