99a658270284d29acd26dfadbd736d26323a7486
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-04 22:07:33 -0500

    test(FieldGimbal): Check for proper values

--- fang-mcb-project/test/control/turret/gimbal/field_gimbal_test.cpp
+++ fang-mcb-project/test/control/turret/gimbal/field_gimbal_test.cpp
@@ -1,11 +1,15 @@
 #include "control/turret/gimbal/field_gimbal/field_gimbal.hpp"
 #include "test/mock/rail/rail_turret_mocks.hpp"
+
+#include "units.h"
+
 #include <gmock/gmock.h>
 
 namespace fang::turret::fieldGimbalTest
 {
+    using namespace units::literals;
     // Constructs common FieldGimbal for testing with checker references
-    class Setup 
+    class Setup
     {
     public:
         using PitchSystemMock = testing::NiceMock<IFieldPitchSystemMock>;
@@ -28,4 +32,90 @@ namespace fang::turret::fieldGimbalTest
         YawSystemMock& yawSystem_;
         FieldGimbal fieldGimbal_;
     };
+
+    struct PitchYawParam
+    {
+        Radians targetPitch;
+        Radians targetYaw;
+    };
+
+    class PitchYawTest:
+        public Setup,
+        public testing::TestWithParam<PitchYawParam>
+    {
+    protected:
+        const Radians kTargetPitch_{GetParam().targetPitch};
+        const Radians kTargetYaw_{GetParam().targetYaw};
+    };
+
+    TEST_P(PitchYawTest, pitchYawMatch)
+    {
+        EXPECT_CALL(pitchSystem_, setTargetFieldPitch(kTargetPitch_));
+        EXPECT_CALL(yawSystem_, setTargetFieldYaw(kTargetYaw_));
+        fieldGimbal_.setTargetFieldPitch(kTargetPitch_);
+        fieldGimbal_.setTargetFieldYaw(kTargetYaw_);
+    };
+
+    INSTANTIATE_TEST_CASE_P
+    (
+        zero,
+        PitchYawTest,
+        testing::Values
+        (
+            PitchYawParam
+            {
+                .targetPitch = 0_deg,
+                .targetYaw   = 0_deg
+            }
+        )
+    );
+
+    INSTANTIATE_TEST_CASE_P
+    (
+        positive,
+        PitchYawTest,
+        testing::Values
+        (
+            PitchYawParam
+            {
+                .targetPitch = 10_deg,
+                .targetYaw   = 43_deg
+            },
+            PitchYawParam
+            {
+                .targetPitch = 1_deg,
+                .targetYaw   = 4_deg
+            },
+            PitchYawParam
+            {
+                .targetPitch = 12_deg,
+                .targetYaw   = 24_deg
+            }
+        )
+    );
+
+    INSTANTIATE_TEST_CASE_P
+    (
+        negative,
+        PitchYawTest,
+        testing::Values
+        (
+            PitchYawParam
+            {
+                .targetPitch = -10_deg,
+                .targetYaw   = -43_deg
+            },
+            PitchYawParam
+            {
+                .targetPitch = -1_deg,
+                .targetYaw   = -4_deg
+            },
+            PitchYawParam
+            {
+                .targetPitch = -12_deg,
+                .targetYaw   = -24_deg
+            }
+        )
+    );
+
 }
