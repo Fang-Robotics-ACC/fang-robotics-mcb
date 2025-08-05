2103a1cc567ada6ff16bbbb1e4df9e6f8a39e7cf
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-04 21:14:28 -0500

    feat(FieldGimbal): Initialize

---
+++ fang-mcb-project/src/control/turret/gimbal/field_gimbal/field_gimbal.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "control/turret/gimbal/field_gimbal_subsystem.hpp"
+#include "driver/drivers.hpp"
+#include "wrap/rail/turret/ifield_yaw_system.hpp"
+#include "wrap/rail/turret/ifield_pitch_system.hpp"
+
+#include <memory>
+
+namespace fang::turret
+{
+    /**
+     * Takes a field and pitch system
+     */
+    class FieldGimbal: public FieldGimbalSubsystem
+    {
+        FieldGimbal
+        (
+            Drivers& drivers,
+            std::unique_ptr<IFieldPitchSystem> pitchSystem,
+            std::unique_ptr<IFieldYawSystem> yawSystem
+        ):
+            FieldGimbalSubsystem{drivers},
+            pitchSystem_{std::move(pitchSystem)},
+            yawSystem_{std::move(yawSystem)}
+        {}
+
+        void initialize() override;
+        void update() override;
+        void setTargetFieldPitch(const Radians& pitch) override;
+        void setTargetFieldYaw(const Radians& yaw) override;
+
+    private:
+        std::unique_ptr<IFieldPitchSystem> pitchSystem_;
+        std::unique_ptr<IFieldYawSystem> yawSystem_;
+    };
+}
