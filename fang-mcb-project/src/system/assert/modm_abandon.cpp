916506b85b0a19644edcce3ed7438de9e83f7a13
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-08-05 14:46:55 -0500

    refactor(system): use unified method of freezing system

--- fang-mcb-project/src/system/assert/modm_abandon.cpp
+++ fang-mcb-project/src/system/assert/modm_abandon.cpp
@@ -1,6 +1,7 @@
 #include "modm_abandon.hpp"
 #include "driver/drivers_singleton.hpp"
 #include "system/emergency/kill_system.hpp"
+#include "system/emergency/freeze_system.hpp"
 #include "system/error/signal/assert_failed_buzz.hpp"
 
 void modm_abandon(const modm::AssertionInfo &info)
@@ -13,12 +14,5 @@ void modm_abandon(const modm::AssertionInfo &info)
 
     fang::error::assertFailedBuzz(drivers.pwm);
 
-    //Stop execution completely
-    //TODO: Decide if there is a way to have a freezeSystem()
-    //Function in here without throwing away AssertionInfo
-    //without tying it t assertion info while preserving debugger
-    //access to the assertion info
-    while(true)
-    {
-    }
+    FREEZE_SYSTEM();
 }
