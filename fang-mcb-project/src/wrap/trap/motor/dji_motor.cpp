359192b034bbdf05727c48e92567ba220015805b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-09 19:58:28 -0500

    clean(Align to proper indentation

--- fang-mcb-project/src/wrap/trap/motor/dji_motor.cpp
+++ fang-mcb-project/src/wrap/trap/motor/dji_motor.cpp
@@ -2,70 +2,67 @@
 
 #include <cassert>
 
-namespace trap
+namespace trap::motor
 {
-    namespace motor
+    DjiMotor::DjiMotor(Drivers& drivers, const Config& config)
+    : DjiMotor{drivers, config.motorId, config.canBus, config.name,
+               config.inverted, config.externalEncoderGearRatio, config.speedPidConfig, config.currentControl}
     {
-        DjiMotor::DjiMotor(Drivers& drivers, const Config& config)
-        : DjiMotor{drivers, config.motorId, config.canBus, config.name,
-                   config.inverted, config.externalEncoderGearRatio, config.speedPidConfig, config.currentControl}
-        {
-        }
+    }
 
-        DjiMotor::DjiMotor(Drivers& drivers, tap::motor::MotorId motorId, tap::can::CanBus canBus,
-                           const char* name, bool inverted, double externalEncoderGearRatio, const DjiSpeedPid::Config& speedPidConfig, bool currentControl)
-            :
-            m_djiMotor
-            {
-                &drivers, 
-                motorId,
-                canBus, 
-                inverted,
-                name, 
-                currentControl, 
-                externalEncoderGearRatio
-            } //Gear ratio is for extern encoder, causes glitches if non-zero with no external encoder
+    DjiMotor::DjiMotor(Drivers& drivers, tap::motor::MotorId motorId, tap::can::CanBus canBus,
+                       const char* name, bool inverted, double externalEncoderGearRatio, const DjiSpeedPid::Config& speedPidConfig, bool currentControl)
+        :
+        m_djiMotor
         {
-        }
+            &drivers, 
+            motorId,
+            canBus, 
+            inverted,
+            name, 
+            currentControl, 
+            externalEncoderGearRatio
+        } //Gear ratio is for extern encoder, causes glitches if non-zero with no external encoder
+    {
+    }
 
-        /// @brief  left blank
-        void DjiMotor::update()
-        {
-        }
+    /// @brief  left blank
+    void DjiMotor::update()
+    {
+    }
 
-        void DjiMotor::initialize()
-        {
-            m_djiMotor.initialize();
-        }
+    void DjiMotor::initialize()
+    {
+        m_djiMotor.initialize();
+    }
 
-		void DjiMotor::setDesiredOutput(const DjiMotorOutput& output)
-        {
-            m_djiMotor.setDesiredOutput(output);
-        }
+	void DjiMotor::setDesiredOutput(const DjiMotorOutput& output)
+    {
+        m_djiMotor.setDesiredOutput(output);
+    }
 
-        void DjiMotor::setTargetOutput(const DjiMotorOutput& desiredOutput)
-        {
-            setDesiredOutput(desiredOutput);
-        }
+    void DjiMotor::setTargetOutput(const DjiMotorOutput& desiredOutput)
+    {
+        setDesiredOutput(desiredOutput);
+    }
 
-        bool DjiMotor::isMotorOnline() const
-        {
-            return m_djiMotor.isMotorOnline();
-        }
+    bool DjiMotor::isMotorOnline() const
+    {
+        return m_djiMotor.isMotorOnline();
+    }
 
-        Celsius DjiMotor::getTemperature() const
-        {
-            return Celsius{m_djiMotor.getTemperature()};
-        }
+    Celsius DjiMotor::getTemperature() const
+    {
+        return Celsius{m_djiMotor.getTemperature()};
+    }
 
-        tap::can::CanBus DjiMotor::getCanBus() const
-        {
-            return m_djiMotor.getCanBus();
-        }
+    tap::can::CanBus DjiMotor::getCanBus() const
+    {
+        return m_djiMotor.getCanBus();
+    }
 
-        const char* DjiMotor::getName() const
-        {
-            return m_djiMotor.getName();
-        }
+    const char* DjiMotor::getName() const
+    {
+        return m_djiMotor.getName();
     }
 }
