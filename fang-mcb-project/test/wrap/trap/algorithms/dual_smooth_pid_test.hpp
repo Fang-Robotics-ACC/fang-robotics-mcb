b84b5609d757097426371ee760eab7dee9d8d2a7
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-17 17:04:32 -0500

    test(DualSmoothPid): Create main test fixture

--- fang-mcb-project/test/wrap/trap/algorithms/dual_smooth_pid_test.hpp
+++ fang-mcb-project/test/wrap/trap/algorithms/dual_smooth_pid_test.hpp
@@ -1,8 +1,38 @@
+#include "wrap/trap/algorithms/smooth_pid.hpp"
 #include "wrap/trap/algorithms/dual_smooth_pid.hpp"
 #include <gmock/gmock.h>
 
 
 namespace trap::algorithms::dualSmoothPidTest
 {
+    using DoubleDualSmoothPid = DualSmoothPid<double, double, double, double>;
+    using DoubleSmoothPid = SmoothPid<double, double, double>;
+    struct MatchTestParam 
+    {
+        double mainTarget;
+        double mainCurrent;
+        double intermediateCurrent;
+        double deltaTime;
+        DoubleDualSmoothPid::Config config;
+    };
+
+    /**
+     * Sets up for manually calculating a cascade pid
+     * and comparing if DualSmoothPid returns the same results
+     */
+    class MatchTest : testing::TestWithParam<MatchTestParam>
+    {
+    protected:
+        const MatchTestParam& kParam{GetParam()};
+        const double kMainTarget{kParam.mainTarget};
+        const double kMainCurrent{kParam.mainCurrent};
+        const double kIntermediateCurrent{kParam.intermediateCurrent};
+        const double kDeltaTime{kParam.deltaTime};
+        const DoubleDualSmoothPid::Config kConfig{kParam.config};
+
+        DoubleDualSmoothPid dualPid{kConfig};
+        DoubleSmoothPid mainPid{kConfig.mainPidConfig};
+        DoubleSmoothPid intermediatePid{kConfig.intermediatePidConfig};
+    };
 
 }
