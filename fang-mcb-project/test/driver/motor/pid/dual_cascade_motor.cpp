d8d4f2ffa90993b172bcfabde4e4fb2a51124ea6
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-21 16:00:51 -0500

    test(DualCascadePid): Plan out constructor api

--- fang-mcb-project/test/driver/motor/pid/dual_cascade_motor.cpp
+++ fang-mcb-project/test/driver/motor/pid/dual_cascade_motor.cpp
@@ -1,11 +1,44 @@
 #include "driver/motor/pid/dual_cascade_motor/dual_cascade_motor.hpp"
+#include "wrap/trap/algorithms/dual_cascade_pid.hpp"
 
 #include "rail/mock/telemetry/itelemetry_mock.hpp"
 
+#include <memory>
 #include <gtest/gtest.h>
 
 namespace fang::motor::dualCascadeMotorTest
 {
+    using DoubleDualSmoothPid = trap::algorithms::DualCascadePid<double, double, double, double>;
+    using TelemetryMock = rail::telemetry::ITelemetryMock<double>;
+
+    struct MatchTestParam 
+    {
+        double mainTarget;
+        double mainCurrent;
+        double intermediateCurrent;
+        double deltaTime;
+        DoubleDualSmoothPid::Config config;
+        double mainErrorDerivative = 0.0;
+        double intermediateErrorDerivative = 0.0;
+    };
+
+    class DualCascadeMotorMatchTest : public testing::TestWithParam<MatchTestParam>
+    {
+    protected:
+        const MatchTestParam kParam{GetParam()};
+        DoubleDualSmoothPid dualPid{kParam.config};
+        std::unique_ptr<TelemetryMock> positionTelemetry{std::make_unique<TelemetryMock>()};
+        std::unique_ptr<TelemetryMock> speedTelemetry{std::make_unique<TelemetryMock>()};
+
+        /*
+        DualCascadeMotor<double, double, double> cascadeMotor
+        {
+
+
+        };
+        */
+    };
+
     TEST(dualCascadeMotor, zero)
     {
         // Basic match test
