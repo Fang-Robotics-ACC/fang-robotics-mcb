aca472d5d99f085426e5b546eb49b277b6d2861b
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-21 16:12:17 -0500

    feat(DualCascadeMotor): Prepare for constructor

--- fang-mcb-project/src/driver/motor/pid/dual_cascade_motor/dual_cascade_motor.hpp
+++ fang-mcb-project/src/driver/motor/pid/dual_cascade_motor/dual_cascade_motor.hpp
@@ -1,7 +1,16 @@
 #pragma once
-namespace motor
+#include "wrap/rail/motor/ioutput_motor.hpp"
+namespace fang::motor
 {
-    class DualCascadePid 
+    /**
+     * Template parameters:
+     * Output - the output motor it accepts
+     * Control - the type used for controlling
+     * Intermediate - the type used in between controlling and outputting
+     * (2nd pid)
+     */
+    template <typename Output, typename Control, typename Intermediate>
+    class DualCascadeMotor : public IOutputMotor<Output>
     {
 
     };
