74eb0d172bdf961f9bd5c5a7edf89e5cc032c4d7
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-21 16:37:22 -0500

    feat(DualCascadeMotor): Create constructor

--- fang-mcb-project/src/driver/motor/pid/dual_cascade_motor/dual_cascade_motor.hpp
+++ fang-mcb-project/src/driver/motor/pid/dual_cascade_motor/dual_cascade_motor.hpp
@@ -1,5 +1,10 @@
 #pragma once
+
 #include "wrap/rail/motor/ioutput_motor.hpp"
+#include "wrap/rail/telemetry/itelemetry.hpp"
+
+#include <memory>
+
 namespace fang::motor
 {
     /**
@@ -10,8 +15,29 @@ namespace fang::motor
      * (2nd pid)
      */
     template <typename Output, typename Control, typename Intermediate>
-    class DualCascadeMotor : public IOutputMotor<Output>
+    class DualCascadeMotor
     {
+    public:
+        using ControlledMotor = IOutputMotor<Output>;
+        using ControlTelemetry = telemetry::ITelemetry<Control>;
+        using IntermediateTelemetry = telemetry::ITelemetry<Intermediate>;
 
+        DualCascadeMotor
+        (
+            std::unique_ptr<ControlledMotor> motor,
+            std::unique_ptr<ControlTelemetry> controlTelemetry,
+            std::unique_ptr<IntermediateTelemetry> intermediateTelemetry>
+        )
+            :
+            motor_{std::move(motor)},
+            controlTelemetry_{std::move(controlTelemetry)},
+            intermediateTelemetry_{std::move(intermediateTelemetry)}
+        {
+        }
+
+    private:
+        std::unique_ptr<ControlledMotor> motor_;
+        std::unique_ptr<ControlTelemetry> controlTelemetry_;
+        std::unique_ptr<IntermediateTelemetry> intermediateTelemetry_;
     };
 }
