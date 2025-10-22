af019516cf0beecae03e32d79f8f1bf21647ac2a
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-22 16:08:42 -0500

    refactor(DualCascadeMotor): Move to proper location and have proper units

--- fang-mcb-project/src/driver/motor/pid/dual_cascade_position/dual_cascade_position.hpp
+++ fang-mcb-project/src/driver/motor/pid/dual_cascade_position/dual_cascade_position.hpp
@@ -3,6 +3,7 @@
 #include "driver/motor/pid/dual_cascade_motor/dual_cascade_motor.hpp"
 #include "wrap/units/units_alias.hpp"
 #include "wrap/rail/motor/iposition_motor.hpp"
+#include "wrap/trap/algorithms/ring_radians.hpp"
 
 namespace fang::motor
 {
@@ -15,11 +16,11 @@ namespace fang::motor
     template <typename Output, typename Intermediate>
     class DualCascadePosition
         :
-        public DualCascadeMotor<Output, Radians, Intermediate>,
+        public DualCascadeMotor<Output, trap::algorithms::RingRadians, Intermediate>,
         virtual public IPositionMotor
     {
     public:
-        using CascadeMotor = DualCascadeMotor<Output, Radians, Intermediate>;
+        using CascadeMotor = DualCascadeMotor<Output, trap::algorithms::RingRadians, Intermediate>;
         using Config = CascadeMotor::Config;
 
         using CascadeMotor::CascadeMotor;
