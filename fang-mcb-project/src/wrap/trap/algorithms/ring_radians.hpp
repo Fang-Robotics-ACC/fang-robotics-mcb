a9900aaff72171afc553dabbec1e2d96487b7949
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-22 12:52:04 -0500

    feat(RingRadians): Create PID friendly wrapped function

---
+++ fang-mcb-project/src/wrap/trap/algorithms/ring_radians.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "wrapped_radians.hpp"
+
+namespace trap::algorithms
+{
+    /**
+     * This allows interfacing with general pid classes
+     * when utilizing rotations
+     * 
+     * All this does is override operator- to use minDifference
+     * which is more suitable for pid
+     */
+    class RingRadians : public WrappedRadians
+    {
+        Radians operator-(const WrappedRadians& other)
+        {
+            return minDifference(other);
+        }
+    };
+}
