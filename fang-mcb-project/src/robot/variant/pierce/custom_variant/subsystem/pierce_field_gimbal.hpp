415649c1ff026a68ae44a0041a104feeb10d66c7
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-10-22 21:06:58 -0500

    perf(Pierce): Switch to using dual cascade systems

--- fang-mcb-project/src/robot/variant/pierce/custom_variant/subsystem/pierce_field_gimbal.hpp
+++ fang-mcb-project/src/robot/variant/pierce/custom_variant/subsystem/pierce_field_gimbal.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "control/turret/gimbal/field_gimbal/field_gimbal.hpp"
-#include "control/turret/gimbal/yaw/chassis_field_yaw_system/gm6020_chassis_field_yaw_system.hpp"
-#include "control/turret/gimbal/pitch/basic_field_pitch_system/gm6020_basic_field_pitch.hpp"
+#include "control/turret/gimbal/pitch/basic_field_pitch_system/gm6020_cascade_field_pitch.hpp"
+#include "control/turret/gimbal/yaw/chassis_field_yaw_system/gm6020_cascade_chassis_yaw.hpp"
 
 #include "wrap/trap/communication/sensors/imu.hpp"
 namespace fang::turret
@@ -9,8 +9,8 @@ namespace fang::turret
     class PierceFieldGimbal : public FieldGimbal
     {
     public:
-        using PitchSystem = GM6020BasicFieldPitch;
-        using YawSystem   = GM6020FieldYawSystem;
+        using PitchSystem = GM6020CascadeFieldPitch;
+        using YawSystem   = GM6020CascadeFieldYaw;
         using Imu = trap::communication::sensors::Imu;
         struct Config
         {
