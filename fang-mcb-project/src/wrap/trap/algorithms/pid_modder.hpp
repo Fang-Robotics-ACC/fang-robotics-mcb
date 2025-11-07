9905ae8b202c5dca334aaf11dda93ab6ec1f2b92
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-07 15:32:56 -0600

    feat: Allow various types of pid modding

---
+++ fang-mcb-project/src/wrap/trap/algorithms/pid_modder.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "wrap/units/units_alias.hpp"
+
+namespace trap::algorithms
+{
+/**
+ * Takes in the output of a pid and modifies it
+ * this is useful for sprinkling in feedforward
+ * to pid
+ */
+template<typename Control>
+class IPidModder
+{
+public:
+    /**
+     * This has a null behavior so that
+     */
+    virtual Control getModdedOutput(const Control& output) = 0;
+    virtual ~IPidModder = default;
+};
+
+/**
+ * This provides default behavior of not modifying the output at all
+ */
+template<typename Control>
+class NullPidModder : public IPidModder<Control>
+{
+public:
+    virtual Control getModdedOutput(const Control& output) override
+    {
+        return output;
+    }
+};
+}
