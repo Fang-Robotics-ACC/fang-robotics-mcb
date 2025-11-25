13e6e91f7901ac42b06abceef2c9da1df1a04281
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-25 08:14:32 -0600

    fix: Test segmentation fault triggered by taproot
    Taproot has a bug in DjiMotorEncoderMock. This is now triggered because the
    actual robots are being instantiated during the unit test builds due to the
    removal of their definitions from main(). Since they are just part of your
    average translation unit, it happens to be built.
    
    A future solution could be adding it to the exlusion file for unit tests.
    
    For now, this will do.

--- fang-mcb-project/src/robot/robot_singleton.cpp
+++ fang-mcb-project/src/robot/robot_singleton.cpp
@@ -1,3 +1,4 @@
+#include "base_robot.hpp"
 #include "robot_singleton.hpp"
 #include "driver/drivers_singleton.hpp"
 /*
@@ -12,19 +13,40 @@ fang::robot::IRobot& fang::RobotSingleton::getRobot()
 // For convenience
 fang::Drivers& drivers{fang::DriversSingleton::getDrivers()};
 
-#if defined(TARGET_INFANTRY_PIERCE)
+/**
+ * 
+ * This is required to not trigger build errors
+ * as taproot automatically converts motors to mock motors
+ * whose constructors may or may not be as friendly :P
+ * 
+ * Their oddness causes segmentation faults involving DjiMotorEcoderMock's constructor
+ * ON_CALL(*this, isOnline).WillByDefault(testing::Return(true));
+ * 
+ * TODO: File a bug error (it's 8:08 and I need to start doing chest day) - Rave
+ * We should consider making a of taproot, modm, and protothreads to use modules
+ * (get access to arm-none-eabi-gcc with C++23)
+ * File pull requests all that jazz..
+ */
+
+#if defined(ENV_UNIT_TESTS)
+    // This is a bit of a HACK: by creating an empty baseRobot
+    // in unit tests, it is not expected for any actions to be performed
+    // on the BaseRobot anyways
+    fang::robot::BaseRobot robot{drivers.commandScheduler, {}, {}};
+    fang::robot::IRobot& fang::RobotSingleton::robot{robot};
+#elif defined(TARGET_INFANTRY_PIERCE)
     #include "robot/variant/pierce/pierce_config.hpp"
     #include "robot/variant/pierce/pierce.hpp"
     using Robot = fang::robot::Pierce;
     Robot robot{drivers, fang::robot::k_pierceConfig};
     fang::robot::IRobot& fang::RobotSingleton::robot{robot};
-#endif
-
-#if defined(TARGET_PIERCE_AUTO_TEST)
+#elif defined(TARGET_PIERCE_AUTO_TEST)
     #include "robot/variant/pierce_auto_test/pierce_auto_test.hpp"
     #include "robot/variant/pierce_auto_test/pierce_auto_test_config.hpp"
     using Robot = fang::robot::PierceAutoTest;
     static const Robot::Config& k_robotConfig{fang::robot::k_pierceAutoTestConfig};
     Robot robot{drivers, k_robotConfig};
+    RobotSingleton::robot{robot};
+#else
     RobotSingleton::robot{robot};
 #endif
