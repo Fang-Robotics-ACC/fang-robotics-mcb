d6e8c5dfa584c844cec1b2e1c49646382af0e595
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-11-26 11:29:15 -0600

    fix: Prevent header guard collisions

--- fang-mcb-project/src/robot/variant/pierce/pierce_config.hpp
+++ fang-mcb-project/src/robot/variant/pierce/pierce_config.hpp
@@ -1,8 +1,6 @@
-#ifndef FANG_ROBOTICS_MCB_CONTROL_PIERCE_CONFIGURATION_HPP
-#define FANG_ROBOTICS_MCB_CONTROL_PIERCE_CONFIGURATION_HPP
+#pragma once
 #include "pierce.hpp"
 namespace fang::robot 
 {
     extern const Pierce::Config k_pierceConfig;
 }
-#endif
