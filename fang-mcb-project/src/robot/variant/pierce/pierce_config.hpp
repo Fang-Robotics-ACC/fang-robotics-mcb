32dff0aeb02f279cf540481dcfcf03da518606b3
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-02 11:10:02 -0600

    fix: Convert to macro-pure-static-solution
    This weighs out a lot of design pros and cons

--- fang-mcb-project/src/robot/variant/pierce/pierce_config.hpp
+++ fang-mcb-project/src/robot/variant/pierce/pierce_config.hpp
@@ -1,5 +1,26 @@
 #pragma once
 #include "pierce.hpp"
+//Subsystem configs
+#include "config/subsystem/chassis_config.hpp"
+#include "config/subsystem/turret_gimbal_config.hpp"
+#include "config/subsystem/feeder_config.hpp"
+#include "config/subsystem/ammo_booster_config.hpp"
+
+//Input configs 
+#include "config/input/turret_input_config.hpp"
+#include "config/input/chassis_input_config.hpp"
+
+//Command configs
+#include "config/command/turret_aim_config.hpp"
+#include "config/command/field_mecanum_config.hpp"
+#include "config/command/shuriken_config.hpp"
+#include "config/command/tardis_config.hpp"
+
+//Mapping configs
+#include "config/mapping/chassis_command_map.hpp"
+#include "config/mapping/turret_command_map.hpp"
+
+
 namespace fang::robot 
 {
 static const Pierce::SubsystemConfig kPierceSubsystemConfig 
