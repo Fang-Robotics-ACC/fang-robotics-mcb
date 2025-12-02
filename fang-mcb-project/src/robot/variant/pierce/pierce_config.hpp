ba102d89f34f2f363eded9f9d395bcc47fb621c6
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-02 09:42:27 -0600

    fix: Prevent SIOF
    Dump all static variables into the file where the config is used. Since the robot is constructed in a .cpp it won't propogate the header variables and pollute the namespace.

--- fang-mcb-project/src/robot/variant/pierce/pierce_config.hpp
+++ fang-mcb-project/src/robot/variant/pierce/pierce_config.hpp
@@ -2,9 +2,63 @@
 #include "pierce.hpp"
 namespace fang::robot 
 {
-    /**
-     * Prevent's static initialization fiasco
-     * https://isocpp.org/wiki/faq/ctors#static-init-order-on-first-use
-     */
-    const Pierce::Config& getPierceConfig();
+static const Pierce::SubsystemConfig kPierceSubsystemConfig 
+{
+    .chassisConfig  = kChassisConfig,
+    .gimbalConfig   = kGimbalSubsystemConfig,
+    .feederConfig   = kFeederMakerConfig,
+    .boosterConfig  = kAmmoBoosterConfig
+};
+
+static const command::PierceCommandPack::InputConfig kPierceInputConfig 
+{
+    .chassisInputConfig = kChassisInputConfig,
+    .turretInputConfig  = kTurretInputConfig
+};
+
+static const command::PierceCommandPack::CommandConfig kPierceCommandConfig
+{
+    .aimConfig           = kTurretAimConfig,
+    .counterStrikeConfig = kFieldMecanumConfig,
+    .shurikenConfig      = kShurikenConfig,
+    .tardisConfig        = k_tardisConfig
+};
+
+static const command::PierceCommandPack::ComputerMapping kPierceComputerMappingCOnfig 
+{
+    .mouseFire     = kAutofireMouseState,
+    .mouseUnjam    = kUnjamMouseState,
+    .counterStrike = kFieldMecanumKeyboardState,
+    .shuriken      = kShurikenModeKeyboardState,
+    .tardis        = kTardisModeKeyboardState
+};
+
+static const command::PierceCommandPack::RemoteMapping kPierceRemoteMappingConfig 
+{
+    .activateBooster = kActivateBoosterRemoteState,
+    .fire            = kAutofireRemoteState,
+    .unjam           = kUnjamRemoteState,
+    .counterStrike   = kFieldMecanumRemoteState,
+    .shuriken        = kShurikenModeRemoteState,
+    .tardis          = kTardisModeRemoteState
+};
+
+static const command::PierceCommandPack::MappingConfig kPierceMappingConfig 
+{
+    .remote     = kPierceRemoteMappingConfig,
+    .computer   = kPierceComputerMappingCOnfig
+};
+
+static const command::PierceCommandPack::Config kPierceCommandPackConfig
+{
+    .inputConfig   = kPierceInputConfig,
+    .mappingConfig = kPierceMappingConfig,
+    .commandConfig = kPierceCommandConfig
+};
+
+static const Pierce::Config kPierceConfig
+{
+    .subsystemConfig   = kPierceSubsystemConfig,
+    .commandPackConfig = kPierceCommandPackConfig
+};
 }
