3d684c9bd82ad619902baa0c5064a74912378680
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-05 15:52:37 -0600

    feat: create method of storing channel information

---
+++ fang-mcb-project/src/communication/i_bus/i_bus_channel_data.hpp
@@ -0,0 +1,12 @@
+#include "i_bus.hpp"
+
+#include <array>
+
+namespace fang::communication
+{
+    class IBusChannelData : public std::array<Byte, IBus::kChannelCount>
+    {
+    public:
+
+    };
+}
