d18cab55e5bac58e3f5122ab96e8695c6b77da57
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-05 15:34:51 -0600

    feat: Add more protocol information constants

--- fang-mcb-project/src/communication/i_bus/i_bus.hpp
+++ fang-mcb-project/src/communication/i_bus/i_bus.hpp
@@ -1,4 +1,5 @@
 #include "communication/uart/byte.hpp"
+#include <cstdint>
 
 #pragma once
 /**
@@ -15,6 +16,23 @@ class IBus
 public:
     static constexpr Byte kFirstStartOfFrame{0x20};
     static constexpr Byte kSecondStartofFrame{0x40};
-private:
+
+    static constexpr int kStartOfFrameSize{2}; // Bytes
+    static constexpr int kChannelSectionSize{30}; // Bytes (Channel + 16 bit Checkshum)
+    static constexpr int kPacketSize{32}; // Bytes
+
+    static constexpr uint16_t kChecksumConstant{0xffff};
+
+    // Unlike UMN, the start of frame and end of frame are not stored in the buffer
+    static constexpr uint16_t kInitialCheksum{kFirstStartOfFrame + kSecondStartofFrame};
+
+    /**
+     * The checksum is calculated by taking bytes 0-30 and adding them together.
+     * The sum is subtracted from the checksum constant.
+     * 
+     * If the packet is valid, bytes 31 (most significant byte) and 32 (least significant) 
+     * will be the same as the calculated checksum
+     */
+    // https://github.com/mokhwasomssi/stm32_hal_ibus/blob/main/ibus.c
 };
 }
