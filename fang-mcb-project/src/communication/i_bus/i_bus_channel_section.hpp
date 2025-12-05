85ff6b9d929ca3eff52dc3a517c48f96ec1c6049
RavenRandomz <58748161+RavenRandomz@users.noreply.github.com>
2025-12-05 16:29:28 -0600

    feat: Add deserialization capabilities

--- fang-mcb-project/src/communication/i_bus/i_bus_channel_section.hpp
+++ fang-mcb-project/src/communication/i_bus/i_bus_channel_section.hpp
@@ -1,11 +1,30 @@
 #include "i_bus.hpp"
+#include "i_bus_channel_data.hpp"
+#include "util/serialization/deserialize_uint16.hpp"
 
 #include <array>
 namespace fang::communication
 {
-    class IBusDataSection : public std::array<Byte
+    /**
+     * This stores the bytes of the channel section. It is capable of deserializing
+     * the bytes into 
+     */
+    class IBusChannelSection : public std::array<Byte, IBus::kChannelSectionSize>
     {
     public:
+        IBusChannelData getChannelData() const
+        {
+            IBusChannelData channelData{};
+            // There are two bytes per 16 bit int
+            // msb is the first int
+            for(int channel{0}; channel < IBus::kChannelSectionSize; ++ channel)
+            {
+                // They are sent in reverse
+                const Byte kMostSignificantByte{at(channel + 1)};
+                const Byte kLestSignificantByte{at(channel)};
 
+                channelData.at(channel) = serialization::deserializeUInt16(kMostSignificantByte, kLestSignificantByte);
+            }
+        }
     };
 }
