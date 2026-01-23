509ec0fcb94fe2d7e18de24da757f3c14071bb4e
RavenRandomz <raven.randomz.code@protonmail.com>
2026-01-23 15:40:01 -0600

    refactor: Separate implementation
    Oops got too used to modules
    
    Signed-off-by: Rave.n Asher Raziel <raven.randomz.code@protonmail.com>

---
+++ fang-mcb-project/src/communication/i_bus/i_bus_channel_section.cpp
@@ -0,0 +1,46 @@
+#include "i_bus_channel_section.hpp"
+namespace fang::communication
+{
+IBusChannelData IBusChannelSection::getChannelData() const
+    {
+        IBusChannelData channelData{};
+        // There are two bytes per 16 bit int
+        // msb is the first int
+        for(int channel{0}; channel < IBus::kChannelSectionSize; channel += 2)
+        {
+            // They are sent in reverse
+            const Byte kMostSignificantByte{at(channel + 1)};
+            const Byte kLestSignificantByte{at(channel)};
+
+            channelData.at(channel) = serialization::deserializeUInt16(kMostSignificantByte, kLestSignificantByte);
+        }
+
+        return channelData; 
+    }
+
+    /**
+     * The IBus checksum is calculated by summing all of the bytes and subtracting them from 
+     * 0xFFFF
+     * https://blog.dsp.id.au/posts/2017/10/22/flysky-ibus-protocol/
+     */
+    uint16_t IBusChannelSection::calculateChecksum()
+    {
+        uint16_t byteSum{0};
+
+        for(const Byte& byte : *this)
+        {
+            byteSum += byte;
+        }
+
+        // For some reason the actual checksum is the sum subtracted from a constant :P
+        return IBus::kChecksumConstant - byteSum;
+    }
+
+    /**
+     * You can feed a checksum from the checksum section to verify
+     */
+    bool IBusChannelSection::isValid(uint16_t checkSum)
+    {
+        return checkSum == calculateChecksum();
+    }
+}
