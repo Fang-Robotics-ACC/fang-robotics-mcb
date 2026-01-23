57a1287a67d952737a592d78cef9e4f7cf27db71
RavenRandomz <raven.randomz.code@protonmail.com>
2026-01-23 13:46:01 -0600

    clean: Align to new indentation convention

--- fang-mcb-project/src/communication/i_bus/i_bus_channel_section.hpp
+++ fang-mcb-project/src/communication/i_bus/i_bus_channel_section.hpp
@@ -5,26 +5,26 @@
 #include <array>
 namespace fang::communication
 {
-    /**
-     * This stores the bytes of the channel section. It is capable of deserializing
-     * the bytes into 
-     */
-    class IBusChannelSection : public std::array<Byte, IBus::kChannelSectionSize>
+/**
+ * This stores the bytes of the channel section. It is capable of deserializing
+ * the bytes into 
+ */
+class IBusChannelSection : public std::array<Byte, IBus::kChannelSectionSize>
+{
+public:
+    IBusChannelData getChannelData() const
     {
-    public:
-        IBusChannelData getChannelData() const
+        IBusChannelData channelData{};
+        // There are two bytes per 16 bit int
+        // msb is the first int
+        for(int channel{0}; channel < IBus::kChannelSectionSize; ++ channel)
         {
-            IBusChannelData channelData{};
-            // There are two bytes per 16 bit int
-            // msb is the first int
-            for(int channel{0}; channel < IBus::kChannelSectionSize; ++ channel)
-            {
-                // They are sent in reverse
-                const Byte kMostSignificantByte{at(channel + 1)};
-                const Byte kLestSignificantByte{at(channel)};
+            // They are sent in reverse
+            const Byte kMostSignificantByte{at(channel + 1)};
+            const Byte kLestSignificantByte{at(channel)};
 
-                channelData.at(channel) = serialization::deserializeUInt16(kMostSignificantByte, kLestSignificantByte);
-            }
+            channelData.at(channel) = serialization::deserializeUInt16(kMostSignificantByte, kLestSignificantByte);
         }
-    };
+    }
+};
 }
