dd0f461bfb992010de8a0ac8dad4f0cdd4abea6f
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-02-08 00:34:54 -0600

    test: Ensure rejection of invalid packets

--- fang-mcb-project/test/communication/i_bus/dynamic_i_bus_parser/dynamic_i_bus_parser_test.cpp
+++ fang-mcb-project/test/communication/i_bus/dynamic_i_bus_parser/dynamic_i_bus_parser_test.cpp
@@ -133,4 +133,56 @@ TEST(DynamicParser, segmement)
     EXPECT_CALL(dataFoundListener, channelDataFound(kExpectedData));
     parser.update();
 }
+
+/**
+ * Takes known ibus signals from :https://basejunction.wordpress.com/2015/08/23/en-flysky-i6-14-channels-part1/
+ * and checks if it is parsed correctly :D
+ */
+TEST(DynamicParser, invalidChecksum)
+{
+    const coolSerial::Bytes kKnownIbusFrame
+    {
+        0x20, 0x40, // sof header
+        0xdc, 0x05,
+        0xdb, 0x05,
+        0xEF, 0x03,
+        0xdd, 0x05,
+        0xd0, 0x07,
+        0xd0, 0x07,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0xdc, 0x05,
+        0x54, 0xf4  // invalid checksum (0xf4 is supposed to be 0xf3)
+    };
+
+    const ChannelData kExpectedData
+    {
+        1500,
+        1499,
+        1007,
+        1501,
+        2000,
+        2000,
+        1500,
+        1500,
+        1500,
+        1500,
+        1500,
+        1500,
+        1500,
+        1500
+    };
+
+    coolSerial::ByteQueue queue{std::deque<coolSerial::Byte>{kKnownIbusFrame.begin(), kKnownIbusFrame.end()}};
+    testing::StrictMock<IChannelDataFoundListenerMock> dataFoundListener{};
+    DynamicParser parser{queue, dataFoundListener};
+
+    // Expect no call since the checksum is invalid
+    parser.update();
+}
 }
