663153fd448f4ac6fc21aec668626df4c72778ec
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-02-11 16:44:08 -0600

    feat: define main api
    Similar with taproot's Dji Remote based interface

--- fang-mcb-project/src/driver/remote/fly_sky.hpp
+++ fang-mcb-project/src/driver/remote/fly_sky.hpp
@@ -1,15 +1,25 @@
 #pragma once
 
+#include "tap/communication/serial/remote.hpp"
+
 namespace fang::remote
 {
 
 /**
  * Based off of the information here
  * https://github.com/GOFIRST-Robotics/northstar-robomaster/blob/main/northstar-robomaster-project/src/communication/serial/fly_sky.hpp
+ * 
+ * 
+ * Note that Switch B updates the commandMappe LEFT_SWICH and Switch C updates the RIGHT_SWITCH
+ * WARNING: switch A, B, and D can only be up or down, switch C is the only one that can have a center option.
+ * 
+ * TODO: Consider combining switch A and B to provide for up, middle, and down, then a 4th default state
  */
 class FlySky
 {
 public:
+    using TapRemote = tap::communication::serial::Remote;
+
     enum class Channel 
     {
         rightHorizontal = 0,
@@ -23,5 +33,11 @@ public:
         wheelA,
         wheelB 
     };
+
+    void initialize();
+    void update() ;
+
+    double getChannel(Channel channel) const;
+    TapRemote::SwitchState getSwitch() const;
 };
 }
