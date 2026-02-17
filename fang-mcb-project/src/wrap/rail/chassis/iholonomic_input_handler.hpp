fe083f036fb812a9f5167870169417776189f8ba
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-02-17 16:22:20 -0600

    refactor: cadjust to better naming convention
    love y'all future coders ^_^

--- fang-mcb-project/src/wrap/rail/chassis/iholonomic_input_handler.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "rail/chassis/iholonomic_input_handler.hpp"
-#include "util/math/linear/vector_alias.hpp"
-
-namespace fang::chassis
-{
-    using IHolonomicInputHandler = rail::chassis::IHolonomicInputHandler<math::AbstractVector2D, double>;
-}
--- fang-mcb-project/src/wrap/rail/turret/igimbal_input_handler.hpp
+++ fang-mcb-project/src/wrap/rail/turret/igimbal_input_handler.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include "rail/turret/igimbal_input_handler.hpp"
+#include "rail/turret/igimbal_input.hpp"
 namespace fang::turret
 {
-    using IGimbalInputHandler = rail::IGimbalInputHandler<double>;
+    using IGimbalInputHandler = rail::IGimbalInput<double>;
 }
