696c3aee4d64188fe3f12e4c25a4be7cb9ab8729
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-03-10 20:11:11 -0500

    chore: add intellisense flatbuffers support

---
+++ fang-mcb-project/src/communication/cool_protocol/basic_target.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "cool_serial/byte.hpp"
+#include "cool_serial/bytes.hpp"
+#include "basic_target_generated.h"
+
+#include <cstdint>
+#include <cstring>
+
+namespace fang::communication
+{
+    /**
+     * This was developed to have a simple protocol to test with 
+     */
+    class BasicTarget
+    {
+    public:
+        static const coolSerial::Bytes kHeaderSection;
+        /**
+         * Returns the msb bytes
+         */
+        static coolSerial::Bytes serialize(float f);
+        /**
+         * Returns the associated float
+         */
+        static float deserialize(coolSerial::Bytes bytes);
+    };
+}
