99dfe382e0239dbef3ec9fe4139b667cb893822a
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-03-10 22:56:51 -0500

    fix: equality not working

--- fang-mcb-project/src/communication/cool_protocol/ibasic_target_listener.hpp
+++ fang-mcb-project/src/communication/cool_protocol/ibasic_target_listener.hpp
@@ -1,10 +1,11 @@
+#pragma once
 #include "echo/basic_target_generated.h"
 namespace fang::communication
 {
 class IBasicTargetListener
 {
 public:
-    virtual void basicTargetFound(const BasicTarget& basicTarget) = 0;
+    virtual void basicTargetFound(const BasicTargetT& basicTarget) = 0;
     ~IBasicTargetListener() = default;
 };
 }
