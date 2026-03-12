85991d3188d2ce218fd29ac222f6bcfe729317e5
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-03-12 15:29:06 -0500

    refactor: prepare to use templates due to the awful init api

--- fang-mcb-project/src/communication/cool_serial/cool_serial_uart.hpp
+++ fang-mcb-project/src/communication/cool_serial/cool_serial_uart.hpp
@@ -26,21 +26,71 @@ namespace fang::communication
         using DataHandlerRef = std::reference_wrapper<coolSerial::IDataHandler>;
         using HandlerMap = std::unordered_map<coolSerial::Byte, DataHandlerRef>;
 
-        CoolSerialUart(tap::Drivers& drivers, tap::communication::serial::Uart::UartPort port = tap::communication::serial::Uart::Uart1, int baudrate = 115200);
-        CoolSerialUart(tap::Drivers& drivers, const HandlerMap& handlerMap, tap::communication::serial::Uart::UartPort port = tap::communication::serial::Uart::Uart1, int baudrate = 115200);
+        CoolSerialUart(tap::Drivers& drivers, const HandlerMap& handlerMap, tap::communication::serial::Uart::UartPort port = tap::communication::serial::Uart::Uart1, int baudrate = 115200)
+            :
+            uart_{drivers.uart},
+            kUartPort_{port},
+            kBaudrate_{baudrate},
+            handlerMap_{handlerMap}
+        {
+        }
 
-        void initialize();
-        void update();
+        CoolSerialUart(tap::Drivers& drivers, tap::communication::serial::Uart::UartPort port = tap::communication::serial::Uart::Uart1, int baudrate = 115200)
+                :
+                uart_{drivers.uart},
+                kBaudrate_{baudrate},
+                kUartPort_{port}
+            {
+            }
+
+        void initialize()
+        {
+            uart_.init<tap::communication::serial::Uart::Uart6, 921600, tap::communication::serial::Uart::Parity::Disabled>();
+        }
+
+        void update()
+        {
+            updateByteQueue();
+            parser_.update();
+            handleMessage();
+        }
+
         /**
          * HINT: DataHandlerRef{handler} where handler inherits and implements
          * IDataHandler's functions
          */
-        void addHandler(coolSerial::Byte dataType, DataHandlerRef handler);
+        void addHandler(coolSerial::Byte dataType, DataHandlerRef handler)
+        {
+            // [] needs to be able to create a default object. DataHandlerRef does not have
+            // a default constructor
+            handlerMap_.at(dataType) = handler;
+        }
     private:
         static const int kBaudRate{115200};
-        void updateByteQueue();
-        void handleMessage();
-        void referMessage(const coolSerial::CoolMessageData& message);
+
+        void updateByteQueue()
+        {
+            // Transfer uart buffer to byte queue
+            unsigned char byte{};
+            while(uart_.read(tap::communication::serial::Uart::Uart1, &byte)) 
+            {
+                byteQueue_.push(byte);
+            }
+        }
+
+        void handleMessage()
+        {
+            if(!parser_.currentMessageProcessed())
+            {
+                referMessage(parser_.getCurrentMessage());
+            }
+        }
+        void referMessage(const coolSerial::CoolMessageData& message)
+        {
+            // [] needs to be able to create a default object. DataHandlerRef does not have
+            // a default constructor
+            handlerMap_.at(message.dataType).get().handleData(message.data);
+        }
 
         HandlerMap handlerMap_{};
         const tap::communication::serial::Uart::UartPort kUartPort_;
