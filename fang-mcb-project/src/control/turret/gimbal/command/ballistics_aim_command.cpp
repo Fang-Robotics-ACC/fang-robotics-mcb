1b2937315b4dd97452138463d232a994a03510b5
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-03-13 21:57:35 -0500

    feat: initialize blank for ballistics command

---
+++ fang-mcb-project/src/control/turret/gimbal/command/ballistics_aim_command.cpp
@@ -0,0 +1,33 @@
+#include "ballistics_aim_command.hpp"
+
+#include "tap/algorithms/math_user_utils.hpp"
+
+namespace fang::turret
+{
+    using namespace units::literals;
+    BallisticsAimCommand::BallisticsAimCommand(FieldGimbalSubsystem& gimbal, const Config& config)
+    :   gimbal_{gimbal}
+    {
+        addSubsystemRequirement(&gimbal_);
+    }
+
+    void BallisticsAimCommand::initialize()
+    {
+    }
+
+    void BallisticsAimCommand::execute()
+    {
+        gimbal_.setTargetFieldPitch(1.0_rad);
+        gimbal_.setTargetFieldYaw(1.0_rad);
+    }
+
+    void BallisticsAimCommand::end(bool interrupted)
+    {
+    }
+
+    bool BallisticsAimCommand::isFinished() const
+    {
+        //Default command must never finish
+        return false;
+    }
+}
---
+++ fang-mcb-project/src/control/turret/gimbal/command/ballistics_aim_command.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "control/turret/gimbal/field_gimbal_subsystem.hpp"
+#include "util/math/arithmetic/range.hpp"
+
+#include "wrap/rail/turret/igimbal_input.hpp"
+
+#include "tap/control/command.hpp"
+
+namespace fang::turret 
+{
+    /**
+     * Unlike aim command, it takes the raw position instead of velocity.
+     * Used for autonomous testing and as a precursor to general auto-aim commands
+     */
+    class BallisticsAimCommand : public tap::control::Command
+    {
+    public:
+        struct Config
+        {
+            Radians pitchScale;
+            Radians yawScale;
+            math::Range<Radians> pitchRange;
+        };
+        BallisticsAimCommand(FieldGimbalSubsystem& gimbal, const Config& config);
+
+        const char* getName() const override {return "Aim Command";}
+        void initialize() override;
+        void execute() override;
+        void end(bool interrupted) override;
+        bool isFinished() const override;
+
+    private:
+        FieldGimbalSubsystem& gimbal_;
+        IGimbalInput& input_;
+
+        Radians targetPitch_{0};
+        Radians targetYaw_{0};
+    };
+}
