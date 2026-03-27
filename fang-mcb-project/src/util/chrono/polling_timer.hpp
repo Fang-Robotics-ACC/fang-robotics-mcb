377d68191d3122bd1f730c3b6ffb043a71b3abfc
Rave.n Asher Raziel <raven.randomz.code@protonmail.com>
2026-03-26 22:14:10 -0500

    clean: formatting

--- fang-mcb-project/src/util/chrono/polling_timer.hpp
+++ fang-mcb-project/src/util/chrono/polling_timer.hpp
@@ -4,57 +4,57 @@
 
 namespace fang::chrono
 {
+/**
+ * Can time stuff up to 71 days within microsecond precision
+ */
+class PollingTimer
+{
+public:
     /**
-     * Can time stuff up to 71 days within microsecond precision
+     * Automatically sets current moment to that of its creation :)
+     * Sounds a bit mystical imo
      */
-    class PollingTimer
-    {
-    public:
-        /**
-         * Automatically sets current moment to that of its creation :)
-         * Sounds a bit mystical imo
-         */
-        PollingTimer();
+    PollingTimer();
 
-        /**
-         * The duration for the timer to wait.
-         * 
-         * The default duration is zero. Hopefully, this won't crash anything.
-         */
-        void setDuration(Microseconds duration);
+    /**
+     * The duration for the timer to wait.
+     * 
+     * The default duration is zero. Hopefully, this won't crash anything.
+     */
+    void setDuration(Microseconds duration);
 
-        /**
-         * Has the timer to begin timing now
-         */
-        void start();
+    /**
+     * Has the timer to begin timing now
+     */
+    void start();
 
-        /**
-         * true if the timer has elapsed
-         * >= comparison
-         */
-        bool isFinished() const;
+    /**
+     * true if the timer has elapsed
+     * >= comparison
+     */
+    bool isFinished() const;
 
-        /**
-         * This will provide a negative value if it has not finished
-         * 
-         * This will allow you to compensate.
-         */
-        Microseconds getTimePastAlarm() const;
+    /**
+     * This will provide a negative value if it has not finished
+     * 
+     * This will allow you to compensate.
+     */
+    Microseconds getTimePastAlarm() const;
 
-        /**
-         * This will provide a negative value if it has finished
-         * 
-         * This will allow you to compensate.
-         */
-        Microseconds getTimeLeft() const;
+    /**
+     * This will provide a negative value if it has finished
+     * 
+     * This will allow you to compensate.
+     */
+    Microseconds getTimeLeft() const;
 
-        /**
-         * Resets the count
-         */
-        void reset();
+    /**
+     * Resets the count
+     */
+    void reset();
 
-    private:
-        SimpleTimer simpleTimer_{};
-        Microseconds timingDuration_{};
-    };
+private:
+    SimpleTimer simpleTimer_{};
+    Microseconds timingDuration_{};
+};
 }
